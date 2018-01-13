3589f0d25e901e8486c6af88f37cd2ad2d57aa4e
Krzysztof Kaczor <chris@kaczor.io>
2018-01-13 19:17:47 +0100

    improve HMR setup

--- app/index.tsx
+++ app/index.tsx
@@ -3,16 +3,18 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./components/App";
 
-function renderApp(Component: any) {
+function renderApp(Component: React.SFC<any>) {
   const mountNode = document.getElementById("app");
   ReactDOM.render(<Component />, mountNode);
 }
 
-if ((module as any).hot) {
-  (module as any).hot.accept("./components/App", () => {
-    const { App } = require("./components/App");
-    renderApp(App);
-  });
+if (process.env.NODE_ENV === "development") {
+  if ((module as any).hot) {
+    (module as any).hot.accept("./components/App", () => {
+      const { App } = require("./components/App");
+      renderApp(App);
+    });
+  }
 }
 
 renderApp(App);

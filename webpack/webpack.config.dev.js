561069f92e8e4b6813b1e43d236265de5b6ea172
Krzysztof Kaczor <chris@kaczor.io>
2018-01-13 19:39:27 +0100

    proper support for reading NODE_ENV inside the app

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -82,5 +82,8 @@ module.exports = {
     }),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
   ],
 };

86b6e6b639f274e0d36eb9acf15f0328de59712d
Tomek Banach <tomek@banciur.org>
2018-01-16 10:17:05 +0100

    Navbar navigation menu (just for dev purposes)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,26 @@
 import * as React from "react";
-import { Col, Container, Navbar, NavbarBrand, Row } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import { Navbar } from "reactstrap";
+
+import * as styles from "./Header.module.scss";
 
 export const Header = () => (
+  // This navbar and navigation is just placeholder
   <Navbar color="primary" dark>
-    <Container>
-      <Row>
-        <Col>
-          <NavbarBrand href="/">Neufund Platform</NavbarBrand>
-        </Col>
-      </Row>
-    </Container>
+    <NavLink exact to="/" className={styles.link} activeClassName={styles.active}>
+      Neufund Platform
+    </NavLink>
+
+    <NavLink exact to="/walletselector" className={styles.link} activeClassName={styles.active}>
+      Wallet Selector
+    </NavLink>
+
+    <NavLink exact to="/kyc" className={styles.link} activeClassName={styles.active}>
+      KYC
+    </NavLink>
+
+    <NavLink exact to="/dashboard" className={styles.link} activeClassName={styles.active}>
+      Dashboard
+    </NavLink>
   </Navbar>
 );

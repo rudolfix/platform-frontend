053db78a030dccb3d08b1a2b96cc40fcf24a8b97
Tomek Banach <tomek@banciur.org>
2018-01-22 13:08:00 +0100

    Create separate describe section for AccountRow in WalletLedgerChooserComponent

--- test/components/walletSelector/WalletLedgerChooserComponent.spec.tsx
+++ test/components/walletSelector/WalletLedgerChooserComponent.spec.tsx
@@ -10,40 +10,40 @@ import {
 } from "../../../app/components/walletSelector/WalletLedgerChooserComponent";
 import { tid } from "../../testUtils";
 
-describe("<WalletLedgerChooserComponent />", () => {
-  const defaultProps = {
-    loading: false,
-    accounts: [
-      {
-        address: "0x6C1086C292a7E1FdF66C68776eA972038467A370",
-        derivationPath: "44'/60'/0'/0",
-        balance: "1.6495ETH",
-      },
-      {
-        address: "0xB2A0e2688c5A82bEEe6818F5a3D206680FdFD75d",
-        derivationPath: "44'/60'/0'/1",
-        balance: "0",
-      },
-      {
-        address: "0xa13D14DA39529761a6C45F4f556700735E0774a8",
-        derivationPath: "44'/60'/0'/2",
-        balance: "0",
-      },
-      {
-        address: "0x3cC2ef578f6Eb7ff63f9CA8f5a54cfe40339256A",
-        derivationPath: "44'/60'/0'/3",
-        balance: "1.6495ETH",
-      },
-    ],
-    handleAddressChosen: spy(),
-    hasPreviousAddress: true,
-    showPrevAddresses: spy(),
-    showNextAddresses: spy(),
-    derivationPath: "44'/60'/0'/0",
-    onDerivationPathChange: spy(),
-    invalidDerivationPath: false,
-  };
+const defaultProps = {
+  loading: false,
+  accounts: [
+    {
+      address: "0x6C1086C292a7E1FdF66C68776eA972038467A370",
+      derivationPath: "44'/60'/0'/0",
+      balance: "1.6495ETH",
+    },
+    {
+      address: "0xB2A0e2688c5A82bEEe6818F5a3D206680FdFD75d",
+      derivationPath: "44'/60'/0'/1",
+      balance: "0",
+    },
+    {
+      address: "0xa13D14DA39529761a6C45F4f556700735E0774a8",
+      derivationPath: "44'/60'/0'/2",
+      balance: "0",
+    },
+    {
+      address: "0x3cC2ef578f6Eb7ff63f9CA8f5a54cfe40339256A",
+      derivationPath: "44'/60'/0'/3",
+      balance: "1.6495ETH",
+    },
+  ],
+  handleAddressChosen: spy(),
+  hasPreviousAddress: true,
+  showPrevAddresses: spy(),
+  showNextAddresses: spy(),
+  derivationPath: "44'/60'/0'/0",
+  onDerivationPathChange: spy(),
+  invalidDerivationPath: false,
+};
 
+describe("<WalletLedgerChooserComponent />", () => {
   it("should render LoadingIndicator for loading attribute", () => {
     const props = {
       ...defaultProps,
@@ -59,7 +59,7 @@ describe("<WalletLedgerChooserComponent />", () => {
     expect(component.find(AccountRow).length).to.be.eq(defaultProps.accounts.length);
   });
 
-  it("should show correct prev / next buttons regarding hasPreviousAddress property ", () => {
+  it("should show correct prev / next buttons regarding hasPreviousAddress property", () => {
     const componentWithPrevAddr = shallow(<WalletLedgerChooserComponent {...defaultProps} />);
 
     const props = {
@@ -83,8 +83,10 @@ describe("<WalletLedgerChooserComponent />", () => {
     expect(defaultProps.showPrevAddresses).to.be.calledOnce;
     expect(defaultProps.showNextAddresses).to.be.calledOnce;
   });
+});
 
-  it("<AccountRow /> should render correct account data and handle click ", () => {
+describe("<AccountRow />", () => {
+  it("should render correct account data and handle click", () => {
     const account = defaultProps.accounts[0];
     const accountRow = shallow(
       <AccountRow ledgerAccount={account} handleAddressChosen={defaultProps.handleAddressChosen} />,

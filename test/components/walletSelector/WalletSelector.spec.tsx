9a411e99d34838379c6010dff68c32b8eb8c5628
Tomek Banach <tomek@banciur.org>
2018-01-24 17:43:39 +0100

    Add integration tests for wallet chooser for wallet in browser path

--- test/components/walletSelector/WalletSelector.spec.tsx
+++ test/components/walletSelector/WalletSelector.spec.tsx
@@ -7,7 +7,10 @@ import * as React from "react";
 import { Provider } from "react-redux";
 import { spy } from "sinon";
 
-import { WalletBrowser } from "../../../app/components/walletSelector/WalletBrowser";
+import {
+  BROWSER_WALLET_RECONNECT_INTERVAL,
+  WalletBrowser,
+} from "../../../app/components/walletSelector/WalletBrowser";
 import { WalletLedger } from "../../../app/components/walletSelector/WalletLedger";
 import { LEDGER_RECONNECT_INTERVAL } from "../../../app/components/walletSelector/WalletLedgerInitComponent";
 import { WalletLight } from "../../../app/components/walletSelector/WalletLight";
@@ -15,6 +18,11 @@ import {
   WalletSelector,
   WalletSelectorComponent,
 } from "../../../app/components/walletSelector/WalletSelector";
+import {
+  BrowserWallet,
+  BrowserWalletLockedError,
+  BrowserWalletMissingError,
+} from "../../../app/modules/web3/BrowserWallet";
 import { LedgerWallet } from "../../../app/modules/web3/LedgerWallet";
 import { Web3Adapter } from "../../../app/modules/web3/Web3Adapter";
 import { Web3Manager } from "../../../app/modules/web3/Web3Manager";
@@ -134,6 +142,62 @@ describe("<WalletSelector />", () => {
 
       expect(ledgerWalletMock.setDerivationPath).to.be.calledWithExactly("44'/60'/0'/1");
       expect(web3ManagerMock.plugPersonalWallet).to.be.calledWithExactly(ledgerWalletMock);
+    });
+
+    it("should select browser wallet", async () => {
+      const browserWalletMock = createMock(BrowserWallet, {
+        connect: async () => {
+          throw new BrowserWalletMissingError();
+        },
+      });
+      const web3ManagerMock = createMock(Web3Manager, {
+        networkId: "5",
+        internalWeb3Adapter: createMock(Web3Adapter, {
+          getBalance: async () => new BigNumber(1),
+        }),
+        plugPersonalWallet: async () => {},
+      });
+      const { store } = createIntegrationTestsSetup({ browserWalletMock, web3ManagerMock });
+
+      const mountedComponent = createMount(
+        <MuiThemeProvider>
+          <Provider store={store}>
+            <WalletSelector />
+          </Provider>
+        </MuiThemeProvider>,
+      );
+
+      // select wallet in browser tab is selected
+      clickFirstTid(mountedComponent, "wallet-selector-browser");
+      await waitForTid(mountedComponent, "browser-wallet-error-msg");
+
+      // there is no wallet in browser (BrowserWallet thrown BrowserWalletMissingError)
+      expect(mountedComponent.find(tid("browser-wallet-error-msg")).text()).to.be.eq(
+        "We did not detect any Web3 wallet.",
+      );
+
+      // wallet in browser is locked
+      browserWalletMock.reMock({
+        connect: async () => {
+          throw new BrowserWalletLockedError();
+        },
+      });
+      globalFakeClock.tick(BROWSER_WALLET_RECONNECT_INTERVAL);
+      // Here we cannot use waitForTid as error component already exists after reMock it will have different text.
+      await Promise.resolve();
+      mountedComponent.update();
+      expect(mountedComponent.find(tid("browser-wallet-error-msg")).text()).to.be.eq(
+        "Your wallet seems to be locked — we can't access any accounts.",
+      );
+
+      // connect doesn't throw which means there is web3 in browser
+      browserWalletMock.reMock({
+        connect: async () => {},
+      });
+      globalFakeClock.tick(BROWSER_WALLET_RECONNECT_INTERVAL);
+      await Promise.resolve();
+
+      expect(web3ManagerMock.plugPersonalWallet).to.be.calledWithExactly(browserWalletMock);
     });
   });
 });

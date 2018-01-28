9f46690974cd3e0ca59a69b865622ee7fcd38a9b
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-01-28 16:19:49 +0100

    changes light wallet to use routing
    seperate light wallet into components

--- app/components/walletSelector/WalletLight.tsx
+++ app/components/walletSelector/WalletLight.tsx
@@ -1,43 +1,34 @@
+import * as wallet from "eth-lightwallet";
 import * as React from "react";
 import { Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
 
 const passphrase = "test dog cat hello star toad east car food jump picnic beast";
-export const WalletLight = () => (
-  <div>
-    <Row className="justify-content-md-center">
-      <Col sm="5">
-        <Form className="align-self-end">
-          <FormGroup>
-            <Label for="exampleEmail">Email</Label>
-            <Input type="email" name="email" id="email" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleEmail">Password</Label>
-            <Input type="password" name="password" id="password" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleEmail">Repeat password</Label>
-            <Input type="password" name="repeated" id="repeated-pass" />
-          </FormGroup>
-          <Button>Create Account</Button>
-        </Form>
-      </Col>
-    </Row>
 
-    <Row className="mt-5 justify-content-md-center">
-      <Col sm="5">
-        <Form className="align-self-end">
-          <FormGroup>
-            <Label for="exampleEmail">Email</Label>
-            <fieldset disabled>
-              <Input type="textarea" name="backup" id="passphrase" placeholder={passphrase} />
-            </fieldset>
-          </FormGroup>
-          <Button>I saved it</Button>
-        </Form>
-      </Col>
-    </Row>
+interface ICreateWalletLight {
+  password: string;
+}
 
+const createWallet: any = (props:ICreateWalletLight) => {
+  return ;
+};
+
+export const SavePassPhrase: React.SFC<{}> = () => (
+  <Row className="mt-5 justify-content-md-center">
+    <Col sm="5">
+      <Form className="align-self-end">
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <fieldset disabled>
+            <Input type="textarea" name="backup" id="passphrase" placeholder={passphrase} />
+          </fieldset>
+        </FormGroup>
+        <Button>I saved it</Button>
+      </Form>
+    </Col>
+  </Row>
+);
+export const RecoverWalletLight: React.SFC<{}> = () => {
+  return (
     <Row className="mt-5 justify-content-md-center">
       <Col sm="5">
         <Form className="align-self-end">
@@ -66,5 +57,35 @@ export const WalletLight = () => (
         </Form>
       </Col>
     </Row>
+  );
+};
+export const CreateWallet: React.SFC<{}> = () => {
+  return (
+    <Row className="justify-content-md-center mt-5">
+      <Col sm="5">
+        <Form className="align-self-end">
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input type="email" name="email" id="email" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Password</Label>
+            <Input type="password" name="password" id="password" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Repeat password</Label>
+            <Input type="password" name="repeated" id="repeated-pass" />
+          </FormGroup>
+          <Button>Create Account</Button>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
+export const WalletLight: React.SFC<{}> = () => (
+  <div>
+    <CreateWallet />
+    <RecoverWalletLight />
+    <SavePassPhrase />
   </div>
 );
--- typings/modules.d.ts
+++ typings/modules.d.ts
@@ -6,3 +6,4 @@ declare module "web3-provider-engine";
 declare module "web3-provider-engine/subproviders/rpc";
 
 declare module "ledger-wallet-provider";
+declare module "eth-lightwallet";

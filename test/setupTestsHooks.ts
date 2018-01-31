47947eb6d2cf3d3f889344c34fc84e5daca56a95
Krzysztof Kaczor <chris@kaczor.io>
2018-01-31 10:52:16 +0100

    drop link to PR of custom fork

--- test/setupTestsHooks.ts
+++ test/setupTestsHooks.ts
@@ -7,7 +7,7 @@ afterEach(autoUnmountEnzymeComponentsHook);
 export let globalFakeClock: LolexClockAsync<any>;
 
 beforeEach(() => {
-  // note: we use custom fork of lolex providing us tickAsync function which should be used to await for any async actions triggered by tick
+  // note: we use custom fork of lolex providing tickAsync function which should be used to await for any async actions triggered by tick. Read more: https://github.com/sinonjs/lolex/pull/105
   globalFakeClock = lolex.install();
 });
 

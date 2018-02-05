5da92b4755f09549a3ecffe13e85432f6d564022
Krzysztof Kaczor <chris@kaczor.io>
2018-02-05 15:18:11 +0100

    split test suite to node and browser

--- test/modules/web3/lightWallet.spec.node.ts
+++ test/modules/web3/lightWallet.spec.node.ts
@@ -1,17 +1,7 @@
 import { expect } from "chai";
 
 describe("LightWallet", () => {
-  let oldWindow: any;
-  beforeEach(() => {
-    oldWindow = window;
-    // tslint:disable-next-line
-    (window as any) = undefined;
-  });
-  afterEach(() => {
-    window = oldWindow;
-  });
-
-  it.only("should create a new serialized wallet", async () => {
+  it("should create a new serialized wallet", async () => {
     const createNewLightWallet = require("../../../app/modules/web3/LightWallet")
       .createNewLightWallet;
     const serializedLightWallet = await createNewLightWallet({
@@ -28,7 +18,7 @@ describe("LightWallet", () => {
     expect(serializedLightWallet.salt).to.equal("salt");
   });
 
-  it.only("should recover a 24 word mnemonic and generate a nano compatible BIP39", async () => {
+  it("should recover a 24 word mnemonic and generate a nano compatible BIP39", async () => {
     const recoverSeed =
       "butter clean pledge exist gym busy shove pyramid cereal bird unique bar anger hazard weapon shoe clog possible spider convince object mind beef music";
     const hdPathString = "m/44'/60'/0'";
@@ -44,8 +34,8 @@ describe("LightWallet", () => {
     expect(walletInstance.addresses[0]).to.equal(nanoAddress);
   });
 
-  it.only("should recover a 12 word mnemonic and a BIP44 compatible derivation path", async () => {
-    const recoverSeed =
+  it("should recover a 12 word mnemonic and a BIP44 compatible derivation path", async () => {
+    const nanoSeed =
       "author foster awkward faint script unique letter tag meadow garment elite drip";
     const hdPathString = "m/44'/60'/0'";
     const nanoAddress = "47fde38dc660f9d935c41b4f3a2a6e62d2e823eb";
@@ -54,14 +44,14 @@ describe("LightWallet", () => {
     const lightWalletObject = await createNewLightWallet({
       password: "test",
       hdPathString,
-      recoverSeed,
+      nanoSeed,
     });
     const walletInstance = JSON.parse(lightWalletObject.walletInstance);
     expect(walletInstance.addresses[0]).to.equal(nanoAddress);
   });
 
-  it.only("should de-serialized a lightWallet instance", async () => {
-    const recoverSeed =
+  it("should de-serialized a lightWallet instance", async () => {
+    const nanoSeed =
       "author foster awkward faint script unique letter tag meadow garment elite drip";
     const hdPathString = "m/44'/60'/0'";
     const createNewLightWallet = require("../../../app/modules/web3/LightWallet")
@@ -71,7 +61,7 @@ describe("LightWallet", () => {
     const walletInstance = (await createNewLightWallet({
       password: "test",
       hdPathString,
-      recoverSeed,
+      nanoSeed,
       customSalt: "salt",
     })).walletInstance;
     const deserializedInstance = await deserializeLightWallet(walletInstance, "salt");

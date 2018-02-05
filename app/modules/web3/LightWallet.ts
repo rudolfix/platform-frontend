2c379157706354e6cb6887e98891093253c7f4a6
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-02-05 15:47:24 +0100

    Additional lightwallet tests

--- app/modules/web3/LightWallet.ts
+++ app/modules/web3/LightWallet.ts
@@ -5,33 +5,51 @@ import * as HookedWalletSubprovider from "web3-provider-engine/subproviders/hook
 // tslint:disable-next-line
 import * as RpcSubprovider from "web3-provider-engine/subproviders/rpc";
 
-export async function deserializeLightWallet(
-  serializedWallet: object,
-  salt: string,
-): Promise<void> {
-  return await LightWalletProvider.keystore.deserialize(serializedWallet, salt);
-}
-
 interface ICreateLightWallet {
   password: string;
   hdPathString: string;
-  recoverSeed: string | undefined;
-  customSalt: string | undefined;
+  recoverSeed?: string | undefined;
+  customSalt?: string | undefined;
 }
+
+interface IVaultInstance {
+  walletInstance: any;
+  salt: string;
+}
+
+interface ILightWallet {
+  addresses: Array<string>;
+  encHdRootPriv: object;
+  encPrivKeys: object;
+  encSeed: object;
+  hdIndex: number;
+  hdPathString: string;
+  salt: string;
+  version: number;
+  getSeed: any;
+  __proto__: object;
+}
+export async function deserializeLightWallet(
+  serializedWallet: object,
+  salt: string,
+): Promise<ILightWallet> {
+  return await LightWalletProvider.keystore.deserialize(serializedWallet, salt);
+}
+
 export async function createNewLightWallet({
   password,
   hdPathString,
   recoverSeed,
   customSalt,
-}: ICreateLightWallet): Promise<{ walletInstance: any; salt: string }> {
-  //if seed is provided wallet recovers same key
+}: ICreateLightWallet): Promise<IVaultInstance> {
+  const create = promisify<any, object>(LightWalletProvider.keystore.createVault);
+  //256bit strength generates a 24 word mnemonic
   const entropyStrength = 256;
   const seed = recoverSeed
     ? recoverSeed
     : LightWalletProvider.keystore.generateRandomSeed(undefined, entropyStrength);
-  //salt strength taken from https://github.com/ConsenSys/eth-lightwallet/blob/master/lib/keystore.js#L107
+  //salt strength should be 32 bit. see https://github.com/ConsenSys/eth-lightwallet/blob/master/lib/keystore.js#L107
   const salt = customSalt ? customSalt : LightWalletProvider.keystore.generateSalt(32);
-  const create = promisify<any, any>(LightWalletProvider.keystore.createVault);
   const lightWalletInstance = await create({
     password,
     seedPhrase: seed,
@@ -43,9 +61,9 @@ export async function createNewLightWallet({
   return { walletInstance: await lightWalletInstance.serialize(), salt: salt };
 }
 
-export async function getWalletKey(lightWalletInstance: any, password: string): Promise<void> {
+export async function getWalletKey(lightWalletInstance: any, password: string): Promise<object> {
   const keyFromPassword = promisify<any, string>(
     lightWalletInstance.keyFromPassword.bind(lightWalletInstance),
   );
-  return keyFromPassword(password);
+  return await keyFromPassword(password);
 }
--- test/modules/web3/lightWallet.spec.node.ts
+++ test/modules/web3/lightWallet.spec.node.ts
@@ -1,9 +1,13 @@
 import { expect } from "chai";
+import {
+  createNewLightWallet,
+  deserializeLightWallet,
+  getWalletKey,
+} from "../../../app/modules/web3/LightWallet";
+import { expectToBeRejected } from "../../testUtils";
 
 describe("LightWallet", () => {
   it("should create a new serialized wallet", async () => {
-    const createNewLightWallet = require("../../../app/modules/web3/LightWallet")
-      .createNewLightWallet;
     const serializedLightWallet = await createNewLightWallet({
       password: "serializedLightWallet",
       hdPathString: "m/44'/60'/0'",
@@ -23,8 +27,6 @@ describe("LightWallet", () => {
       "butter clean pledge exist gym busy shove pyramid cereal bird unique bar anger hazard weapon shoe clog possible spider convince object mind beef music";
     const hdPathString = "m/44'/60'/0'";
     const nanoAddress = "07dc3a64a5fd98d23776c54f02c53d0a28b61515";
-    const createNewLightWallet = require("../../../app/modules/web3/LightWallet")
-      .createNewLightWallet;
     const lightWalletObject = await createNewLightWallet({
       password: "test",
       hdPathString,
@@ -35,37 +37,83 @@ describe("LightWallet", () => {
   });
 
   it("should recover a 12 word mnemonic and a BIP44 compatible derivation path", async () => {
-    const nanoSeed =
+    const recoverSeed =
       "author foster awkward faint script unique letter tag meadow garment elite drip";
     const hdPathString = "m/44'/60'/0'";
     const nanoAddress = "47fde38dc660f9d935c41b4f3a2a6e62d2e823eb";
-    const createNewLightWallet = require("../../../app/modules/web3/LightWallet")
-      .createNewLightWallet;
     const lightWalletObject = await createNewLightWallet({
       password: "test",
       hdPathString,
-      nanoSeed,
+      recoverSeed,
     });
     const walletInstance = JSON.parse(lightWalletObject.walletInstance);
     expect(walletInstance.addresses[0]).to.equal(nanoAddress);
   });
 
   it("should de-serialized a lightWallet instance", async () => {
-    const nanoSeed =
+    const recoverSeed =
       "author foster awkward faint script unique letter tag meadow garment elite drip";
     const hdPathString = "m/44'/60'/0'";
-    const createNewLightWallet = require("../../../app/modules/web3/LightWallet")
-      .createNewLightWallet;
-    const deserializeLightWallet = require("../../../app/modules/web3/LightWallet")
-      .deserializeLightWallet;
     const walletInstance = (await createNewLightWallet({
       password: "test",
       hdPathString,
-      nanoSeed,
+      recoverSeed,
       customSalt: "salt",
     })).walletInstance;
     const deserializedInstance = await deserializeLightWallet(walletInstance, "salt");
     //TODO: change test to equal see  https://github.com/Neufund/eth-lightwallet/issues/3
     expect(deserializedInstance).to.deep.include(JSON.parse(walletInstance));
+  });
+
+  it("should unlock the lightwallet if salt and password are correct", async () => {
+    const recoverSeed =
+      "author foster awkward faint script unique letter tag meadow garment elite drip";
+    const hdPathString = "m/44'/60'/0'";
+    const walletInstance = (await createNewLightWallet({
+      password: "test",
+      hdPathString,
+      recoverSeed,
+      customSalt: "salt",
+    })).walletInstance;
+    const deserializedInstance = await deserializeLightWallet(walletInstance, "salt");
+    const obtainedSeed = deserializedInstance.getSeed(
+      await getWalletKey(deserializedInstance, "test"),
+    );
+    expect(obtainedSeed).to.equal(recoverSeed);
+  });
+
+  it("should throw if salt is not correct", async () => {
+    const recoverSeed =
+      "author foster awkward faint script unique letter tag meadow garment elite drip";
+    const hdPathString = "m/44'/60'/0'";
+    const walletInstance = (await createNewLightWallet({
+      password: "test",
+      hdPathString,
+      recoverSeed,
+      customSalt: "salt",
+    })).walletInstance;
+    const deserializedInstance = await deserializeLightWallet(walletInstance, "wrongsalt");
+    await expectToBeRejected(
+      async () => deserializedInstance.getSeed(await getWalletKey(deserializedInstance, "test")),
+      new Error("Incorrect derived key!"),
+    );
+  });
+
+  it("should throw if password is not correct", async () => {
+    const recoverSeed =
+      "author foster awkward faint script unique letter tag meadow garment elite drip";
+    const hdPathString = "m/44'/60'/0'";
+    const walletInstance = (await createNewLightWallet({
+      password: "test",
+      hdPathString,
+      recoverSeed,
+      customSalt: "salt",
+    })).walletInstance;
+    const deserializedInstance = await deserializeLightWallet(walletInstance, "salt");
+    await expectToBeRejected(
+      async () =>
+        deserializedInstance.getSeed(await getWalletKey(deserializedInstance, "wrongpassword")),
+      new Error("Incorrect derived key!"),
+    );
   });
 });

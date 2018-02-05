b0508042bdb625cd6902ff90bdda1f690149099b
Tomek Banach <tomek@banciur.org>
2018-02-05 02:43:42 +0100

    Ensure webpack.config.prod.js passes lint validation

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,4 +1,4 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const merge = require("webpack-merge");
@@ -15,7 +15,7 @@ module.exports = merge(configCommon, {
       root: paths.root,
     }),
     new ExtractTextPlugin("styles.css"), // TODO: its not working
-    new UglifyJsPlugin()
+    new UglifyJsPlugin(),
   ],
   module: {
     rules: [

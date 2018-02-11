b86e0f0f582220ecd15e5ba294393fe8726f8ecb
Tomek Banach <tomek@banciur.org>
2018-02-11 18:54:25 +0100

    Restyled wallet selector

--- app/components/walletSelector/WalletSelector.tsx
+++ app/components/walletSelector/WalletSelector.tsx
@@ -3,24 +3,10 @@ import * as React from "react";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import { Col, Row } from "reactstrap";
 
-import { HiResImage } from "../HiResImage";
 import { LayoutRegisterLogin } from "../LayoutRegisterLogin";
 import { WalletRouter } from "./WalletRouter";
 import { walletRoutes } from "./walletRoutes";
 import * as styles from "./WalletSelector.module.scss";
-
-interface IWalletTab {
-  active?: boolean;
-  onSelect: () => any;
-}
-
-const WalletTabComponent: React.SFC<IWalletTab> = ({ active, onSelect, children, ...props }) => {
-  return (
-    <div className={cn(styles.walletTab, { active })} onClick={onSelect} {...props}>
-      <div className={styles.walletTabTitle}>{children}</div>
-    </div>
-  );
-};
 
 interface IWalletTabLink extends RouteComponentProps<{}> {
   href: string;
@@ -32,14 +18,15 @@ class WalletTabLink extends React.Component<IWalletTabLink> {
   };
 
   render(): React.ReactNode {
-    const { href, history, location, match, staticContext, ...props } = this.props;
-    return (
-      <WalletTabComponent
-        onSelect={this.onClick}
-        active={location.pathname.startsWith(href)}
-        {...props}
-      />
-    );
+    const { href, location, children } = this.props;
+    const active = location.pathname.startsWith(href);
+    const child = React.Children.only(children);
+    const className = cn(child.props.className, { active });
+
+    return React.cloneElement(child, {
+      className,
+      onClick: this.onClick,
+    });
   }
 }
 
@@ -48,26 +35,23 @@ const WalletTab = withRouter<IWalletTabLink>(WalletTabLink);
 export const WalletSelector: React.SFC = () => (
   <LayoutRegisterLogin>
     <Row>
-      <Col>
-        <p>Please select your wallet.</p>
-        <div>
-          <div className={styles.walletSelector}>
-            <WalletTab href={walletRoutes.light} data-test-id="wallet-selector-light">
-              <HiResImage
-                partialPath="wallet_selector/icon_light_wallet"
-                className={styles.walletIcon}
-              />Light Wallet
-            </WalletTab>
-            <WalletTab href={walletRoutes.browser} data-test-id="wallet-selector-browser">
-              <HiResImage partialPath="wallet_selector/icon_wallet" className={styles.walletIcon} />Wallet
-              in your browser
-            </WalletTab>
-            <WalletTab href={walletRoutes.ledger} data-test-id="wallet-selector-ledger">
-              <HiResImage partialPath="wallet_selector/icon_ledger" className={styles.walletIcon} />Ledger
-              Nano Wallet
-            </WalletTab>
-          </div>
-        </div>
+      <Col
+        className={cn(
+          "d-flex flex-column flex-md-row justify-content-center",
+          styles.walletChooser,
+        )}
+      >
+        <WalletTab href={walletRoutes.light} data-test-id="wallet-selector-light">
+          <span className={styles.wallet}>use Neufund wallet</span>
+        </WalletTab>
+        <span className={styles.separator} />
+        <WalletTab href={walletRoutes.browser} data-test-id="wallet-selector-browser">
+          <span className={styles.wallet}>use existing wallet</span>
+        </WalletTab>
+        <span className={styles.separator} />
+        <WalletTab href={walletRoutes.ledger} data-test-id="wallet-selector-ledger">
+          <span className={styles.wallet}>use nano ledger</span>
+        </WalletTab>
       </Col>
     </Row>
     <Row>

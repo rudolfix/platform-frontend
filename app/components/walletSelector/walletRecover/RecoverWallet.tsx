91636b80545dab27fca6dfbc40af394210f35f5f
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-02-23 21:19:46 +0100

    Adds Recover Wallet Flow

--- app/components/walletSelector/walletRecover/RecoverWallet.tsx
+++ app/components/walletSelector/walletRecover/RecoverWallet.tsx
@@ -1,34 +1,83 @@
 import * as React from "react";
-import { Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import { compose } from "redux";
+import { appConnect } from "../../../store";
 
-export const RecoverWallet: React.SFC<{}> = () => {
-  return (
-    <Row className="mt-5 justify-content-sm-center">
-      <Col sm="5">
-        <Form className="align-self-end">
-          <FormGroup>
-            <Label for="copy-backup">Backup phrases</Label>
-            <Input
-              type="textarea"
-              name="backup"
-              id="backup-phrase"
-              placeholder="Enter your backup phrases here"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label>Email</Label>
-            <Input type="email" name="email" id="email-recover" />
-          </FormGroup>
-          <FormGroup>
-            <Label>Password</Label>
-            <Input type="password" name="password" id="recover-password" />
-          </FormGroup>
-          <FormGroup>
-            <Label>Repeat password</Label>
-            <Input type="password" name="repeated" id="recov-repeated-pass" />
-          </FormGroup>
-        </Form>
-      </Col>
-    </Row>
-  );
+import { flows } from "../../../modules/flows";
+import { RegisterWalletComponent } from "../walletLight/RegisterWallet";
+import { WalletLightSeedRecoveryComponent } from "./SeedRecovery";
+import { WalletResetHeader } from "./WalletResetHeader";
+
+interface IMainRecoveryState {
+  seed?: string;
+}
+
+interface IFormValues {
+  email?: string;
+  password?: string;
+  repeatPassword?: string;
+  seed?: string;
+}
+
+interface IProps {
+  submitForm: (values: IFormValues) => void;
+  seed: string;
+}
+
+interface IMainRecoveryProps {
+  submitForm: (values: IFormValues) => void;
+}
+export class RecoveryProcessesComponent extends React.Component<
+  IMainRecoveryProps,
+  IMainRecoveryState
+> {
+  constructor(props: IMainRecoveryProps) {
+    super(props);
+    this.state = {};
+    this.setSeed = this.setSeed.bind(this);
+  }
+
+  setSeed(words: string): any {
+    this.setState({ seed: words });
+  }
+
+  render(): React.ReactNode {
+    return this.state.seed ? (
+      <div>
+        <WalletResetHeader text="Lopsum Iprum" currentStep={6} steps={7} />
+        <RegisterWalletComponent
+          submitForm={(values: any) => {
+            this.props.submitForm({ ...values, seed: this.state.seed });
+          }}
+        />
+      </div>
+    ) : (
+      <div>
+        <WalletLightSeedRecoveryComponent
+          startingStep={0}
+          extraSteps={1}
+          sendWords={this.setSeed}
+        />
+      </div>
+    );
+  }
+}
+
+export const RecoveryProcesses: React.SFC<any> = props => {
+  return <RecoveryProcessesComponent {...props} />;
 };
+
+export const RecoverWallet = compose<React.SFC>(
+  appConnect<IProps>({
+    dispatchToProps: dispatch => ({
+      submitForm: (values: IFormValues) => {
+        dispatch(
+          flows.wallet.tryConnectingWithLightWallet(
+            values.email as string,
+            values.password as string,
+            values.seed as string,
+          ),
+        );
+      },
+    }),
+  }),
+)(RecoveryProcesses);
--- app/components/walletSelector/walletRecover/WalletRecoverMain.tsx
+++ app/components/walletSelector/walletRecover/WalletRecoverMain.tsx
@@ -1,15 +1,13 @@
-import * as cn from "classnames";
 import * as React from "react";
-import { NavLink } from "react-router-dom";
 import { Col, Row } from "reactstrap";
 import { LayoutRegisterLogin } from "../../layouts/LayoutRegisterLogin";
-import {WalletRecoverRouter} from "./WalletRecoverRouter";
+import { RecoverRouter } from "./RecoverRouter";
 
 export const WalletRecoverMain: React.SFC = () => (
   <LayoutRegisterLogin>
     <Row>
       <Col>
-        <WalletRecoverRouter />
+        <RecoverRouter />
       </Col>
     </Row>
   </LayoutRegisterLogin>

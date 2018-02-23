6e9e7873451263442cadfb9e2819d82b268dd7d7
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-02-23 21:19:30 +0100

    Refactors Recovery Success

---
+++ app/components/walletSelector/walletRecover/RecoverySuccess.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { Col, Row } from "reactstrap";
+import { compose } from "redux";
+import { actions } from "../../../modules/actions";
+import { appConnect } from "../../../store";
+import { StandardButton } from "../../shared/StandardButton";
+import { WalletResetHeader } from "./WalletResetHeader";
+
+export const RecoverySuccessComponent: React.SFC<{ submitForm: () => void }> = props => (
+  <div>
+    <Col className="mt-4 pb-5">
+      <WalletResetHeader steps={7} currentStep={7} text={"Lorem ipsum"} />
+    </Col>
+    <Col className="mt-4 mb-5 mx-auto">
+      <h5 className="text-center">
+        <i className="fa fa-check-circle mr-1" /> Great, you have a new password!
+      </h5>
+    </Col>
+    <Row className="justify-content-center mb-5 mt-5 pt-4">
+      <Col xs={6} sm={5} md={4} lg={4}>
+        <StandardButton text="GO TO DASHBOARD" onClick={props.submitForm} />
+      </Col>
+    </Row>
+    <Row className="justify-content-end mt-4 pt-4 align-bottom" noGutters>
+      <Col className="align-bottom text-end">
+        <div className="mt-5 align-bottom font-weight-bold text-right">
+          Contact for help <i className="fa fa-lg fa-angle-right ml-1" />
+        </div>
+      </Col>
+    </Row>
+  </div>
+);
+
+export const RecoverySuccess = compose<React.SFC>(
+  appConnect<void>({
+    dispatchToProps: dispatch => ({
+      //There must be a guard here if we will continue like this
+      submitForm: () => dispatch(actions.wallet.connected()),
+    }),
+  }),
+)(RecoverySuccessComponent);

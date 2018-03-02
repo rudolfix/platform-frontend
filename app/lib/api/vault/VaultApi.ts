2060783ef8549d634d98198eaf4ed02d8114aa84
Krzysztof Kaczor <chris@kaczor.io>
2018-03-02 20:37:27 +0100

    code review fixes

--- app/lib/api/vault/VaultApi.ts
+++ app/lib/api/vault/VaultApi.ts
@@ -5,12 +5,7 @@ import { ILogger } from "../../dependencies/Logger";
 import { IHttpClient } from "../client/IHttpClient";
 import { IVault, VaultValidator } from "./interfaces";
 
-export class VaultApiError extends Error {}
-export class VaultNotExisting extends VaultApiError {}
-export class VaultAlreadyExisting extends VaultApiError {}
-export class VaultCorrupt extends VaultApiError {}
-
-const USER_VAULT_ROOT = "/api/wallet";
+const VAULT_API_ROOT = "/api/wallet";
 
 @injectable()
 export class VaultApi {
@@ -20,37 +15,24 @@ export class VaultApi {
   ) {}
 
   public async store(key: string, serializedVault: string): Promise<void> {
-    this.logger.info("Storing Vault with Key: ", key);
+    this.logger.info("Storing vault with Key: ", key);
 
-    const response = await this.httpClient.post<string>({
-      baseUrl: USER_VAULT_ROOT,
+    await this.httpClient.post<string>({
+      baseUrl: VAULT_API_ROOT,
       url: `/vault/${key}`,
       responseSchema: VaultValidator,
       body: { wallet: serializedVault },
-      allowedStatusCodes: [409],
     });
-
-    if (response.statusCode === 409) {
-      throw new VaultAlreadyExisting();
-    }
-    if (response.statusCode === 413) {
-      throw new VaultCorrupt();
-    }
   }
 
   public async retrieve(key: string): Promise<string> {
-    this.logger.info("Retrieving Vault with Key: ", key);
+    this.logger.info("Retrieving vault with Key: ", key);
 
     const response = await this.httpClient.get<IVault>({
-      baseUrl: USER_VAULT_ROOT,
+      baseUrl: VAULT_API_ROOT,
       url: `/vault/${key}`,
       responseSchema: VaultValidator,
-      allowedStatusCodes: [404],
     });
-
-    if (response.statusCode === 404) {
-      throw new VaultNotExisting();
-    }
 
     return response.body.wallet;
   }

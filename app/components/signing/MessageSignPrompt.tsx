952231514554ddf7cf5e53c619807029ac95af45
Tomek Banach <tomek@banciur.org>
2018-03-02 16:16:31 +0100

    Restyle of message confirmation prompt

--- app/components/signing/MessageSignPrompt.tsx
+++ app/components/signing/MessageSignPrompt.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { Col, Row } from "reactstrap";
+
+import * as walletConfirmIcon from "../../assets/img/wallet_selector/wallet_confirm_icon.svg";
 import { selectConnectedWeb3State } from "../../modules/web3/reducer";
 import { WalletType } from "../../modules/web3/types";
 import { appConnect } from "../../store";
@@ -7,40 +10,21 @@ interface IStateProps {
   walletType: WalletType;
 }
 
-export const BrowserWalletMessageSignPrompt: React.SFC = () => (
-  <div className="text-center">
-    <h1>Confirm your wallet ownership</h1>
-    <p>Please confirm on selected existing wallet that you will be using it for Neufund platform</p>
-  </div>
-);
-
-// this get more complicated when light wallet is locked
-export const LightWalletMessageSignPrompt: React.SFC = () => (
-  <div className="text-center">
-    <h1>Confirm your wallet ownership</h1>
-    <p>Please confirm on selected existing wallet that you will be using it for Neufund platform</p>
-  </div>
-);
-
-export const LedgerWalletMessageSignPrompt: React.SFC = () => (
-  <div className="text-center">
-    <h1>Confirm you Ledger Ownership</h1>
-    <p>
-      Please confirm on your Nano Ledger that you are an owner of the device by clicking on the
-      right button
-    </p>
-  </div>
-);
-
 export const MessageSignPromptComponent: React.SFC<IStateProps> = ({ walletType }) => {
-  switch (walletType) {
-    case WalletType.BROWSER:
-      return <BrowserWalletMessageSignPrompt />;
-    case WalletType.LEDGER:
-      return <LedgerWalletMessageSignPrompt />;
-    default:
-      return <LightWalletMessageSignPrompt />;
-  }
+  const text =
+    walletType === WalletType.LEDGER
+      ? "Please confirm on your Nano Ledger that you are an owner of the device by clicking on the right button"
+      : "Please confirm on selected existing wallet that you will be using it for Neufund platform";
+
+  return (
+    <Row className="text-center">
+      <Col>
+        <h1>Confirm your wallet ownership</h1>
+        <img className="mt-5 mb-4" src={walletConfirmIcon} />
+        <p className="font-weight-bold">{text}</p>
+      </Col>
+    </Row>
+  );
 };
 
 export const MessageSignPrompt = appConnect<IStateProps>({
--- app/components/walletSelector/WalletMessageSigner.tsx
+++ app/components/walletSelector/WalletMessageSigner.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import { Col, Row } from "reactstrap";
 import { compose } from "redux";
 
 import { actions } from "../../modules/actions";
 import { appConnect } from "../../store";
 import { ButtonPrimary } from "../shared/Buttons";
 import { LoadingIndicator } from "../shared/LoadingIndicator";
+import { WarningAlert } from "../shared/WarningAlert";
 import { MessageSignPrompt } from "../signing/MessageSignPrompt";
+import * as styles from "./WalletMessageSigner.module.scss";
 
 interface IStateProps {
   errorMsg?: string;
@@ -25,8 +28,18 @@ export const MessageSignerComponent: React.SFC<IStateProps & IDispatchProps> = (
 }) => (
   <>
     <MessageSignPrompt />
-    {errorMsg ? <p>{errorMsg}</p> : <LoadingIndicator />}
-    <ButtonPrimary onClick={cancelSigning}>Cancel</ButtonPrimary>
+    {errorMsg ? (
+      <Row className="justify-content-center">
+        <WarningAlert className="my-4">{errorMsg}</WarningAlert>
+      </Row>
+    ) : (
+      <LoadingIndicator className={styles.spinner} />
+    )}
+    <Row>
+      <Col className="text-center">
+        <ButtonPrimary onClick={cancelSigning}>Cancel</ButtonPrimary>
+      </Col>
+    </Row>
   </>
 );
 

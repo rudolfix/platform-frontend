7b446650db6ca7a64477398d3a0f4860487e5930
Tomek Banach <tomek@banciur.org>
2018-03-02 17:23:25 +0100

    Fix ledger sign prompt

--- app/components/signing/MessageSignPrompt.tsx
+++ app/components/signing/MessageSignPrompt.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Col, Row } from "reactstrap";
 
+import * as ledgerConfirm from "../../assets/img/wallet_selector/ledger_confirm.svg";
 import * as walletConfirmIcon from "../../assets/img/wallet_selector/wallet_confirm_icon.svg";
 import { selectConnectedWeb3State } from "../../modules/web3/reducer";
 import { WalletType } from "../../modules/web3/types";
@@ -10,20 +11,36 @@ interface IStateProps {
   walletType: WalletType;
 }
 
-export const MessageSignPromptComponent: React.SFC<IStateProps> = ({ walletType }) => {
-  const text =
-    walletType === WalletType.LEDGER
-      ? "Please confirm on your Nano Ledger that you are an owner of the device by clicking on the right button"
-      : "Please confirm on selected existing wallet that you will be using it for Neufund platform";
+export const BrowserWalletMessageSignPrompt: React.SFC = () => (
+  <Row className="text-center">
+    <Col>
+      <h1>Confirm your wallet ownership</h1>
+      <img className="mt-5 mb-4" src={walletConfirmIcon} />
+      <p className="font-weight-bold">
+        Please confirm on selected existing wallet that you will be using it for Neufund platform
+      </p>
+    </Col>
+  </Row>
+);
 
-  return (
-    <Row className="text-center">
-      <Col>
-        <h1>Confirm your wallet ownership</h1>
-        <img className="mt-5 mb-4" src={walletConfirmIcon} />
-        <p className="font-weight-bold">{text}</p>
-      </Col>
-    </Row>
+export const LedgerWalletMessageSignPrompt: React.SFC = () => (
+  <Row className="text-center">
+    <Col>
+      <h1>Confirm you Ledger Ownership</h1>
+      <img className="mt-5 mb-4" src={ledgerConfirm} />
+      <p className="font-weight-bold">
+        Please confirm on your Nano Ledger that you are an owner of the device by clicking on the
+        right button
+      </p>
+    </Col>
+  </Row>
+);
+
+export const MessageSignPromptComponent: React.SFC<IStateProps> = ({ walletType }) => {
+  return walletType === WalletType.LEDGER ? (
+    <LedgerWalletMessageSignPrompt />
+  ) : (
+    <BrowserWalletMessageSignPrompt />
   );
 };
 

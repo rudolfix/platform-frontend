e2a6e4f000fe5f65bb2c14e332b461cb84aae386
Tomek Banach <tomek@banciur.org>
2018-03-06 12:40:06 +0100

    Add PanelDark component

---
+++ app/components/shared/PanelDark.tsx
@@ -0,0 +1,30 @@
+import * as cn from "classnames";
+import * as React from "react";
+import { Col, Container, Row } from "reactstrap";
+
+import * as styles from "./PanelDark.module.scss";
+
+interface IPanelDarkProps {
+  headerText: string;
+  rightComponent: React.ReactNode;
+}
+
+export const PanelDark: React.SFC<IPanelDarkProps & React.HTMLAttributes<HTMLDivElement>> = ({
+  headerText,
+  rightComponent,
+  className,
+  children,
+  ...props
+}) => (
+  <Container {...props} className={cn(styles.panel, className)}>
+    <Row>
+      <Col className={cn(styles.header, "d-flex justify-content-between align-items-center")}>
+        <span className={styles.headerText}>{headerText}</span>
+        {rightComponent}
+      </Col>
+    </Row>
+    <Row>
+      <Col>{children}</Col>
+    </Row>
+  </Container>
+);

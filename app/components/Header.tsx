7cc5ca7abf073622bfa591eb95c2c62f22c2c94f
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-03-06 12:15:31 +0100

    Adds Auth Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,13 +3,41 @@ import { Link } from "react-router-dom";
 import { Navbar } from "reactstrap";
 
 import * as logo from "../../app/assets/img/logo_yellow.svg";
+import { actions } from "../modules/actions";
+import { selectIsAuthorized } from "../modules/auth/reducer";
+import { appConnect } from "../store";
 import { appRoutes } from "./AppRouter";
 import * as styles from "./Header.module.scss";
+import { ButtonSecondary } from "./shared/Buttons";
 
-export const Header: React.SFC = () => (
+interface IStateProps {
+  isAuthorized: boolean;
+}
+
+interface IDispatchProps {
+  logout: () => void;
+}
+
+export const HeaderComponent: React.SFC<IStateProps & IDispatchProps> = props => (
   <Navbar dark className="bg-dark">
     <Link to={appRoutes.root} className="navbar-brand">
       <img src={logo} className={styles.logo} />
     </Link>
+    {props.isAuthorized && (
+      <ButtonSecondary className="text-white" onClick={props.logout}>
+        LOGOUT
+      </ButtonSecondary>
+    )}
   </Navbar>
 );
+
+export const Header = appConnect<IStateProps, IDispatchProps>({
+  stateToProps: s => ({
+    isAuthorized: selectIsAuthorized(s.auth),
+  }),
+  dispatchToProps: dispatch => ({
+    logout: () => {
+      dispatch(actions.auth.logout());
+    },
+  }),
+})(HeaderComponent);

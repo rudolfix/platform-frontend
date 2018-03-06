12b949a9e70139e999b75234a341e64ef62fa23e
Tomek Banach <tomek@banciur.org>
2018-03-06 16:00:21 +0100

    dashboard/Menu.tsx initial struct commit

--- app/components/dashboard/Menu.tsx
+++ app/components/dashboard/Menu.tsx
@@ -1,14 +1,10 @@
 import * as React from "react";
-
 import { LayoutAuthorized } from "../layouts/LayoutAuthorized";
 import { MessageSignModal } from "../modals/SignMessageModal";
-import { Menu } from "./Menu";
 import { UserInfo } from "./UserInfo";
 
-export const Dashboard = () => (
-  <LayoutAuthorized menu={<Menu />}>
-    <MessageSignModal />
-    <h2>Dashboard</h2>
-    <UserInfo />
-  </LayoutAuthorized>
+export const Menu = () => (
+  <div>
+    menu
+  </div>
 );
--- app/components/layouts/LayoutAuthorized.tsx
+++ app/components/layouts/LayoutAuthorized.tsx
@@ -3,10 +3,14 @@ import { Col, Row } from "reactstrap";
 
 import * as styles from "./LayoutAuthorized.module.scss";
 
-export const LayoutAuthorized: React.SFC = ({ children }) => (
+interface ILayoutAuthorizedProps {
+  menu: React.ReactNode;
+}
+
+export const LayoutAuthorized: React.SFC<ILayoutAuthorizedProps> = ({ menu, children }) => (
   <Row className="h-100">
     <Col xs={12} sm="auto">
-      <div className={styles.menu}>MENU</div>
+      <div className={styles.menu}>{menu}</div>
     </Col>
     <Col className={styles.content}>{children}</Col>
   </Row>

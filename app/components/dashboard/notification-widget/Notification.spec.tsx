29870718af1d5900a3b7ecd707e32c7dfbfc7e9a
Patryk Lizoń <patryk@hangover.studio>
2018-03-07 09:30:47 +0100

    Remove comments

--- app/components/dashboard/notification-widget/Notification.spec.tsx
+++ app/components/dashboard/notification-widget/Notification.spec.tsx
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import * as React from 'react';
 import { spy } from 'sinon';
 
@@ -17,26 +17,13 @@ const defaultProps = () => ({
 });
 
 describe('<Notification />', () => {
-	// it('should be of correct type', () => {
-	// 	const props = defaultProps();
-	// 	const componentInfo = shallow(<Notification {...props} type={NotificationType.INFO} />);
-	// 	expect(componentInfo.find(tid('notification-text'))).to.have.className(infoClassName);
-	// 	const componentWarning = shallow(<Notification {...props} type={NotificationType.WARNING} />);
-	// 	expect(componentWarning).to.have.className(warningClassName);
-	// });
+	let props: any;
 
-	// it('should correct render notification', () => {
-	// 	const props = defaultProps();
-	// 	const component = shallow(<Notification {...props} />);
-	// 	expect(component).to.have.className(infoClassName);
-	// 	const text = component.find(tid('notification-text'));
-	// 	expect(text.text()).to.be.eq(props.text);
-	// 	const link = component.find(tid('notification-link'));
-	// 	expect(link.text()).to.be.eq(props.actionLinkText);
-	// });
+	beforeEach(() => {
+		props = defaultProps();
+	});
 
 	it('should call correct click handlers for close button', () => {
-		const props = defaultProps();
 		const component = shallow(<Notification {...props} />);
 		component.find(tid('notification-close')).simulate('click');
 		expect(props.onClose).to.be.calledOnce;

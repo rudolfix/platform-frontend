f5e00cc6411427ca15716090563d4780b40e5043
Patryk Lizoń <patryk@hangover.studio>
2018-03-07 08:45:45 +0100

    Mount component to dashboard view

--- app/components/dashboard/Dashboard.tsx
+++ app/components/dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { LayoutAuthorized } from "../layouts/LayoutAuthorized";
 import { MessageSignModal } from "../modals/SignMessageModal";
+import { NotificationWidget } from "./notification-widget/NotificationWidget";
 import { UserInfo } from "./UserInfo";
 
 export const Dashboard = () => (
   <LayoutAuthorized>
+    <NotificationWidget />
     <MessageSignModal />
     <h2>Dashboard</h2>
     <UserInfo />

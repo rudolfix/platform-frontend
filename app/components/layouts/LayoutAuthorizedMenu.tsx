4dfded6f72227456aa53f63f3b5ebce529b5c0c8
Patryk Lizoń <patryk@hangover.studio>
2018-03-08 11:06:44 +0100

    Improve menu layout

--- app/components/layouts/LayoutAuthorizedMenu.tsx
+++ app/components/layouts/LayoutAuthorizedMenu.tsx
@@ -1,41 +1,50 @@
 import * as cn from "classnames";
 import * as React from "react";
 import { NavLink, NavLinkProps } from "react-router-dom";
+import { InlineIcon } from "../shared/InlineIcon";
 
 import * as styles from "./LayoutAuthorizedMenu.module.scss";
 
+import * as iconStart from "../../assets/img/icon_home_active.svg"
+import * as iconPortfolio from "../../assets/img/icon_portfolio_inactive.svg"
+import * as iconSettings from "../../assets/img/icon_settings_inactive.svg"
+import * as iconWallet from "../../assets/img/icon_wallet_inactive.svg"
+
 interface IMenuEntry {
-  iconClass: string;
+  svgString: string;
   actionRequired?: boolean;
 }
 
 const MenuEntry: React.SFC<IMenuEntry & NavLinkProps> = ({
-  iconClass,
   actionRequired,
   to,
+  svgString,
   children,
 }) => {
   return (
-    <NavLink to={to} className={cn(styles.menuItem, iconClass)} exact>
+    <NavLink to={to} className={cn(styles.menuItem, svgString)} exact>
+      <span className={styles.icon}>
+        <InlineIcon svgIcon={svgString} />
+        {actionRequired && <div className={styles.actionIndicator} />}
+      </span>
       <span className={styles.name}>{children}</span>
-      {actionRequired && <div className={styles.actionRequired} />}
     </NavLink>
   );
 };
 
 export const LayoutAuthorizedMenu = () => (
   <div className={styles.menu}>
-    <MenuEntry iconClass={styles.start} to="/">
+    <MenuEntry svgString={iconStart} to="/">
       Start
     </MenuEntry>
-    <MenuEntry iconClass={styles.portfolio} to="/dashboard">
+    <MenuEntry svgString={iconPortfolio} to="/dashboard">
       Portfolio
     </MenuEntry>
-    <MenuEntry iconClass={styles.wallet} to="/">
+    <MenuEntry svgString={iconWallet} to="/">
       Wallet
     </MenuEntry>
-    <MenuEntry iconClass={styles.settings} to="/" actionRequired>
+    <MenuEntry svgString={iconSettings} to="/" actionRequired>
       Settings
     </MenuEntry>
-  </div>
+  </div >
 );

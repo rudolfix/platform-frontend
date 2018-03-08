53dee872bb28c61d9e5d004c05ee0024a3cc9d55
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-03-08 16:34:40 +0100

    changes NeuWidget logo  alignment

--- app/components/dashboard/myPortfolio/MyNeuWidget.tsx
+++ app/components/dashboard/myPortfolio/MyNeuWidget.tsx
@@ -15,10 +15,10 @@ interface IProps {
 export const MyNeuWidget: React.SFC<IProps> = props => {
   return props.balanceNeu === "0" ? (
     <div className={cn(styles.background)}>
-      <h5 className="text-light text-left mb-3 mb-md-5 pl-3 pt-1">YOUR NEUMARK</h5>
+      <h5 className={cn(styles.title, "text-left mb-3 mb-md-5 pl-3 pt-1")}>YOUR NEUMARK</h5>
       <Row noGutters>
-        <Col md={12}>
-          <h3 className="text-light mb-4 mb-md-5 text-left ml-3 ml-md-0 text-md-center">
+        <Col xs={6} md={12} className="mb-2 mb-md-4 text-left ml-2 ml-md-0 text-md-center pt-1">
+          <h3 className={cn(styles.neu)}>
             <img src={icon} className={cn(styles.icon, "mr-2 mb-1")} data-test-id="simple-neu" />
             0.0000 NEU
           </h3>
@@ -32,25 +32,25 @@ export const MyNeuWidget: React.SFC<IProps> = props => {
     </div>
   ) : (
     <div className={cn(styles.background)}>
-      <h5 className="text-light text-left mb-3 pl-3 pt-1">YOUR NEUMARK</h5>
+      <h5 className={cn(styles.title, "text-left mb-3 pl-2 pl-md-3 pt-2")}>MY NEUMARK</h5>
       <Row noGutters>
         <Col>
-          <Row className="text-light justify-content-md-center ml-3 ml-md-0" noGutters>
+          <Row className="justify-content-md-center ml-3 ml-md-0" noGutters>
             <img src={icon} className={cn(styles.icon, "mr-2")} />
-            <h3 className="text-light" data-test-id="balance-neu">
+            <h3 className={cn("align-self-end", styles.neu)} data-test-id="balance-neu">
               <Money value={props.balanceNeu} currency="neu" doNotSeparateThousands />
             </h3>
           </Row>
-          <Col md={12} xs={10} className="text-light text-md-center pl-5 pl-md-0 ml-2">
-            <p data-test-id="balance-eur">
+          <Col md={12} xs={10} className="text-md-center pl-5 pl-md-0 ml-2">
+            <p data-test-id="balance-eur" className={cn(styles.smaller)}>
               = <Money value={props.balanceEur!} currency="eur" doNotSeparateThousands />
             </p>
           </Col>
         </Col>
-        <Col xs={5} md={12}>
-          <div className="text-light text-right pr-2 pr-md-0 text-md-center pl-3">
-            <p className="mb-1">Outstanding NEU</p>
-            <p className={cn(styles.ratio, "pr-4")} data-test-id="ratio-neu">
+        <Col xs={5} md={12} className="align-self-end">
+          <div className="text-right pr-2 pr-md-0 text-md-center pl-3">
+            <p className={cn("mb-1", styles.smaller)}>Outstanding NEU</p>
+            <p className={cn(styles.neuRatio, "pr-4")} data-test-id="ratio-neu">
               + <Money value={props.ratioNeu!} currency="neu" doNotSeparateThousands />
             </p>
           </div>

26be93994f7e7c3b09de831d10229df66ad514b9
Patryk Lizoń <patryk@hangover.studio>
2018-03-12 01:49:56 +0100

    Make White Panel more universal

--- app/components/shared/PanelWhite.tsx
+++ app/components/shared/PanelWhite.tsx
@@ -9,9 +9,7 @@ export const PanelWhite: React.SFC<React.HTMLAttributes<HTMLDivElement>> = ({
   children,
   ...props
 }) => (
-  <Container {...props} className={cn(styles.panel, className)}>
-    <Row>
-      <Col>{children}</Col>
-    </Row>
-  </Container>
-);
+    <div {...props} className={cn(styles.panel, className)}>
+      {children}
+    </div>
+  );

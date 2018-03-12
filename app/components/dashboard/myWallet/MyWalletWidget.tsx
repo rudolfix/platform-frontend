f50ecabf2880a51cb166a4fbb3a0460c369cd1bc
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-03-12 21:25:58 +0100

    makes myWalletWidget responsive

--- app/components/dashboard/myWallet/MyWalletWidget.tsx
+++ app/components/dashboard/myWallet/MyWalletWidget.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { Col, Row } from "reactstrap";
 
 import * as cn from "classnames";
-import * as ethIcom from "../../../assets/img/eth_icon.svg";
-import * as moneyIcom from "../../../assets/img/nEUR_icon.svg";
+import * as ethIcon from "../../../assets/img/eth_icon.svg";
+import * as moneyIcon from "../../../assets/img/nEUR_icon.svg";
 import { ArrowLink } from "../../shared/ArrowLink";
 import { Money, selectCurrencySymbol, TCurrency } from "../../shared/Money";
 import { PanelDark } from "../../shared/PanelDark";
@@ -12,29 +12,30 @@ import * as styles from "./MyWalletWidget.module.scss";
 interface IMoneySuiteWidgetProps {
   icon: string;
   currency: TCurrency;
-  largeNo: string;
+  largeNumber: string;
 }
 
 const MoneySuiteWidget: React.SFC<
   IMoneySuiteWidgetProps & React.HTMLAttributes<HTMLDivElement>
-> = ({ icon, currency, largeNo, children, className }) => (
-  <div className="mt-2 mb-2">
-    <Row className={cn("justify-content-center", className)} noGutters>
-      <Col className="col-auto">
+> = ({ icon, currency, largeNumber, children, className }) => (
+  <>
+    <div
+      className={cn(
+        "mt-2 pt-1 d-flex justify-content-sm-center justify-content-lg-start noGutters",
+        className,
+      )}
+    >
+      <span>
         <img className={styles.icon} src={icon} />
-      </Col>
-      <Col className="pl-2 col-sm-auto col-lg">
-        <div className={styles.largeDarkFont}>
+        <span className={cn(styles.largeDarkFont, "align-middle pl-2")}>
           {selectCurrencySymbol(currency)}{" "}
-          <Money value={largeNo} currency={currency} noCurrencySymbol />
-        </div>
-        <div>{children}</div>
-      </Col>
-      <Col xs={12} className="mt-3">
-        <div className={styles.border} />
-      </Col>
-    </Row>
-  </div>
+          <Money value={largeNumber} currency={currency} noCurrencySymbol />
+        </span>
+        <div className="ml-3 pl-4">{children}</div>
+      </span>
+    </div>
+    <div className={cn(styles.border, "mt-2")} />
+  </>
 );
 
 interface IMyWalletWidget {
@@ -66,30 +67,33 @@ export const MyWalletWidget: React.SFC<IMyWalletWidget> = ({
           Manage Wallet
         </ArrowLink>
       }
-      className={styles.background}
+      className={cn(styles.background)}
     >
-      <Row noGutters>
-        <Col xs={12} sm={6} lg={12} className="mt-2">
+      <Row>
+        <Col xs={12} sm={6} lg={12}>
           <MoneySuiteWidget
             currency="eur_token"
-            largeNo={euroTokenAmount}
-            icon={moneyIcom}
-            className={styles.borderRight}
+            largeNumber={euroTokenAmount}
+            icon={moneyIcon}
+            className={cn(styles.borderRight)}
             data-test-id="euro-widget"
           >
             = <Money value={euroTokenEuroAmount} currency="eur" />
           </MoneySuiteWidget>
         </Col>
-        <Col xs={12} sm={6} lg={12} className="mt-2">
+        <Col xs={12} sm={6} lg={12}>
           <MoneySuiteWidget
             currency="eth"
-            largeNo={ethAmount}
-            icon={ethIcom}
+            largeNumber={ethAmount}
+            icon={ethIcon}
             data-test-id="eth-widget"
           >
             <span>
-              = <Money value={ethEuroAmount} currency="eur" />
-              <span className={cn(styles.smallRedFont, "ml-2")}> ({percentage}%) </span>
+              = <Money value={ethEuroAmount} currency="eur" className="" />
+              <span className={cn(styles.smallRedFont, "pl-2 pl-sm-0 pl-md-1")}>
+                {" "}
+                ({percentage}%)
+              </span>
             </span>
           </MoneySuiteWidget>
         </Col>
@@ -97,19 +101,21 @@ export const MyWalletWidget: React.SFC<IMyWalletWidget> = ({
           <div className={styles.borderS} />
         </Col>
       </Row>
-      <Row className="mb-3 justify-content-between mt-2" noGutters data-test-id="total-widget">
-        <Col className="d-flex align-items-center">
-          <span className={cn(styles.smallFont)}>TOTAL</span>
-          <Money
-            value={totalAmount}
-            currency="eur"
-            noCurrencySymbol
-            className={cn(styles.mediumSeaweedFont, "pl-1 pl-sm-2 m-0")}
-          />
-          <span className={cn(styles.smallSeaweedFont, "pl-1")}>EUR</span>
+      <Row noGutters data-test-id="total-widget">
+        <Col>
+          <div className="mt-3 mb-3 d-flex align-items-center">
+            <span className={cn(styles.smallFont)}>TOTAL</span>
+            <Money
+              value={totalAmount}
+              currency="eur"
+              noCurrencySymbol
+              className={cn(styles.mediumSeaweedFont, "pl-1 pl-sm-2 m-0")}
+            />
+            <span className={cn(styles.smallSeaweedFont, "pl-1")}>EUR</span>
+          </div>
         </Col>
         <Col className="d-block d-sm-none text-right col-auto">
-          <ArrowLink arrowDirection="right" to="#" className={cn(styles.link, "p-0")}>
+          <ArrowLink arrowDirection="right" to="#" className={cn(styles.link, "p-0 mt-3 mb-3")}>
             Manage Wallet
           </ArrowLink>
         </Col>

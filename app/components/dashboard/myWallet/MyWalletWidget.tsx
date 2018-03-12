636c7bca8327c7a76fbfd8eaac012ba90ad126b8
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-03-12 17:54:41 +0100

    Uses Money Component in MyWalletWidget

--- app/components/dashboard/myWallet/MyWalletWidget.tsx
+++ app/components/dashboard/myWallet/MyWalletWidget.tsx
@@ -5,23 +5,28 @@ import * as cn from "classnames";
 import * as ethIcom from "../../../assets/img/eth_icon.svg";
 import * as moneyIcom from "../../../assets/img/nEUR_icon.svg";
 import { ArrowLink } from "../../shared/ArrowLink";
+import { Money, selectCurrencySymbol, TCurrency } from "../../shared/Money";
 import { PanelDark } from "../../shared/PanelDark";
 import * as styles from "./MyWalletWidget.module.scss";
 
 interface IMoneySuiteWidgetProps {
   icon: string;
+  currency: TCurrency;
   largeNo: string;
 }
 
 const MoneySuiteWidget: React.SFC<
   IMoneySuiteWidgetProps & React.HTMLAttributes<HTMLDivElement>
-> = ({ icon, largeNo, children, className }) => (
+> = ({ icon, currency, largeNo, children, className }) => (
   <Row className={cn("mt-2 mb-2 justify-content-center", className)} noGutters>
     <Col className="col-auto">
       <img className={styles.icon} src={icon} />
     </Col>
     <Col className="pl-2 col-sm-auto col-lg">
-      <div className={styles.largeDarkFont}>{largeNo}</div>
+      <div className={styles.largeDarkFont}>
+        {selectCurrencySymbol(currency)}{" "}
+        <Money value={largeNo} currency={currency} noCurrencySymbol />
+      </div>
       <div>{children}</div>
     </Col>
     <Col xs={12} className="mt-3">
@@ -46,30 +51,40 @@ export const MyWalletWidget = () => {
     >
       <Row noGutters>
         <Col xs={12} sm={6} lg={12} className="mt-2">
-          <MoneySuiteWidget largeNo="nEUR 36 490" icon={moneyIcom} className={styles.borderRight}>
-            = 36 490 EUR
+          <MoneySuiteWidget
+            currency="eur_token"
+            largeNo={"36490" + "0".repeat(18)}
+            icon={moneyIcom}
+            className={styles.borderRight}
+          >
+            = <Money value={"36490" + "0".repeat(18)} currency="eur" noCurrencySymbol />
           </MoneySuiteWidget>
         </Col>
         <Col xs={12} sm={6} lg={12} className="mt-2">
-          <MoneySuiteWidget largeNo="ETH 6.6482" icon={ethIcom}>
-            <Row className="align-self-center">
-              <div className="ml-3">= 600 490.4646 EUR</div>
-              <div className={cn(styles.smallRedFont, "ml-2")}> (-3.67%) </div>
-            </Row>
+          <MoneySuiteWidget currency="eth" largeNo={"66482" + "0".repeat(14)} icon={ethIcom}>
+            <span>
+              = <Money value={"6004904646" + "0".repeat(16)} currency="eur" noCurrencySymbol />
+              <span className={cn(styles.smallRedFont, "ml-2")}> (-3.67%) </span>
+            </span>
           </MoneySuiteWidget>
         </Col>
         <Col xs={12}>
           <div className={styles.borderS} />
         </Col>
       </Row>
-      <Row className="mb-3 ml-2 justify-content-between mt-2" noGutters>
+      <Row className="mb-3 justify-content-between mt-2" noGutters>
         <Col className="d-flex align-items-center">
           <span className={cn(styles.smallFont)}>TOTAL</span>
-          <span className={cn(styles.mediumSeaweedFont, "ml-2 m-0")}>637 238 </span>
+          <Money
+            value={"637238" + "0".repeat(18)}
+            currency="eur"
+            noCurrencySymbol
+            className={cn(styles.mediumSeaweedFont, "pl-1 pl-sm-2 m-0")}
+          />
           <span className={cn(styles.smallSeaweedFont, "pl-1")}>EUR</span>
         </Col>
-        <Col xs={6} className="d-block d-sm-none text-right">
-          <ArrowLink arrowDirection="right" to="#" className={cn(styles.link, "p-0 pb-1")}>
+        <Col className="d-block d-sm-none text-right col-auto">
+          <ArrowLink arrowDirection="right" to="#" className={cn(styles.link, "p-0")}>
             Manage Wallet
           </ArrowLink>
         </Col>

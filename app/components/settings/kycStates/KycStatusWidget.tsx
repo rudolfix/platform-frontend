9160830e7ec3903a4b2d60a5f832556e3850fa9f
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-03-13 16:42:11 +0100

    adds Mock kycStatesWidget

---
+++ app/components/settings/kycStates/KycStatusWidget.tsx
@@ -0,0 +1,61 @@
+import * as cn from "classnames";
+import * as React from "react";
+import * as styles from "./KycStatusWidget.module.scss";
+
+import * as successIcon from "../../../assets/img/notfications/Success_small.svg";
+import * as warningIcon from "../../../assets/img/notfications/warning.svg";
+
+import { Col } from "reactstrap";
+import { ArrowLink } from "../../shared/ArrowLink";
+import { PanelDark } from "../../shared/PanelDark";
+
+//This is a mock implementation
+interface IProps {
+  kycDone?: boolean;
+}
+
+export const KycStatusWidgetComponent: React.SFC<IProps> = ({ kycDone }) => {
+  return (
+    <PanelDark
+      headerText="KYC PROCESS"
+      className={styles.panel}
+      rightComponent={
+        kycDone ? (
+          <img src={successIcon} className={styles.icon} aria-hidden="true" />
+        ) : (
+          <img src={warningIcon} className={styles.icon} aria-hidden="true" />
+        )
+      }
+    >
+      {kycDone ? (
+        <div
+          data-test-id="verified-section"
+          className={cn(styles.content, "d-flex flex-wrap align-content-around")}
+        >
+          <p>Your verification is complete. </p>
+          <Col xs={12} className="d-flex justify-content-center">
+            <ArrowLink arrowDirection="right" to="#">
+              View Again
+            </ArrowLink>
+          </Col>
+        </div>
+      ) : (
+        <div
+          data-test-id="unverified-section"
+          className={cn(styles.content, "d-flex flex-wrap align-content-around")}
+        >
+          <p>
+            Lorem upsom Kyc who bla once check rep
+          </p>
+          <Col xs={12} className="d-flex justify-content-center">
+            <ArrowLink arrowDirection="right" to="#">
+              Backup phrase
+            </ArrowLink>
+          </Col>
+        </div>
+      )}
+    </PanelDark>
+  );
+};
+
+//TODO: Connect Widget With status

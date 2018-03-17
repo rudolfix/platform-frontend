30cd75af527f0298e0b2d7000c16f4c9784c174e
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-03-17 23:35:09 +0100

    decouples BackupSeedDisplay layout from component

--- app/components/settings/backupSeed/BackupSeedDisplay.tsx
+++ app/components/settings/backupSeed/BackupSeedDisplay.tsx
@@ -23,7 +23,7 @@ interface IBackupSeedDisplayState {
   pageNo: number;
 }
 
-export class BackupSeedDisplay extends React.Component<
+export class BackupSeedDisplayComponent extends React.Component<
   IBackupSeedDisplayProps,
   IBackupSeedDisplayState
 > {
@@ -57,86 +57,87 @@ export class BackupSeedDisplay extends React.Component<
     const showNextButton = endWord >= wordsNo;
 
     return (
-      <LayoutAuthorized>
-        <BreadCrumb
-          className="my-4"
-          path={["Settings", "Security settings"]}
-          view="Backup recovery phase"
+      <PanelWhite className="pt-5">
+        <HeaderProgressStepper
+          steps={4}
+          currentStep={stepNo}
+          headerText="Write down your Recovery Phrase"
+          descText="Your Recovery Phrase allows you to restore your wallet and access your funds if you forgot your password."
+          warning
         />
         <Row>
-          <Col md={12} lg={{ size: 10, offset: 1 }} xl={{ size: 8, offset: 2 }}>
-            <PanelWhite className="pt-5">
-              <HeaderProgressStepper
-                steps={4}
-                currentStep={stepNo}
-                headerText="Write down your Recovery Phrase"
-                descText="Your Recovery Phrase allows you to restore your wallet and access your funds if you forgot your password."
-                warning
-              />
-              <Row>
-                <Col xs={{ size: 10, offset: 1 }}>
-                  <Row className="no-gutters">
-                    <Col className={cn("text-right", styles.pageStatus)}>
-                      {`${(this.state.pageNo + 1) * WORDS_PER_PAGE} / ${wordsNo}`}
-                    </Col>
-                  </Row>
-                  <Row className="justify-content-around no-gutters">
-                    {this.props.words.slice(startWord, endWord).map((word, index) => (
-                      <Col
-                        className={cn(styles.word, "mt-1 p-2 text-center")}
-                        xs={12}
-                        sm={6}
-                        md={4}
-                        lg={3}
-                        xl={2}
-                        key={word}
-                        data-test-id="seed-display-word"
-                      >
-                        {`${this.state.pageNo * WORDS_PER_PAGE + index + 1}. ${word}`}
-                      </Col>
-                    ))}
-                  </Row>
-                  <Row className="my-4 justify-content-center justify-content-sm-between">
-                    <Col className="mt-2" xs="auto">
-                      <ButtonPrimary
-                        data-test-id="seed-display-prev-words"
-                        disabled={this.state.pageNo === 0}
-                        onClick={this.handlePrevPage}
-                      >
-                        {`previous ${WORDS_PER_PAGE} words`}
-                      </ButtonPrimary>
-                    </Col>
-                    <Col className="mt-2" xs="auto">
-                      {showNextButton ? (
-                        <ButtonPrimary
-                          data-test-id="seed-display-next-link"
-                          onClick={this.props.onNext}
-                        >
-                          Go to next step
-                        </ButtonPrimary>
-                      ) : (
-                        <ButtonPrimary
-                          data-test-id="seed-display-next-words"
-                          onClick={this.handleNextPage}
-                        >
-                          {`next ${WORDS_PER_PAGE} words`}
-                        </ButtonPrimary>
-                      )}
-                    </Col>
-                  </Row>
+          <Col xs={{ size: 10, offset: 1 }}>
+            <Row className="no-gutters">
+              <Col className={cn("text-right", styles.pageStatus)}>
+                {`${(this.state.pageNo + 1) * WORDS_PER_PAGE} / ${wordsNo}`}
+              </Col>
+            </Row>
+            <Row className="justify-content-around no-gutters">
+              {this.props.words.slice(startWord, endWord).map((word, index) => (
+                <Col
+                  className={cn(styles.word, "mt-1 p-2 text-center")}
+                  xs={12}
+                  sm={6}
+                  md={4}
+                  lg={3}
+                  xl={2}
+                  key={word}
+                  data-test-id="seed-display-word"
+                >
+                  {`${this.state.pageNo * WORDS_PER_PAGE + index + 1}. ${word}`}
                 </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <ArrowButton arrowDirection="left" onClick={this.props.onBack}>
-                    Back
-                  </ArrowButton>
-                </Col>
-              </Row>
-            </PanelWhite>
+              ))}
+            </Row>
+            <Row className="my-4 justify-content-center justify-content-sm-between">
+              <Col className="mt-2" xs="auto">
+                <ButtonPrimary
+                  data-test-id="seed-display-prev-words"
+                  disabled={this.state.pageNo === 0}
+                  onClick={this.handlePrevPage}
+                >
+                  {`previous ${WORDS_PER_PAGE} words`}
+                </ButtonPrimary>
+              </Col>
+              <Col className="mt-2" xs="auto">
+                {showNextButton ? (
+                  <ButtonPrimary data-test-id="seed-display-next-link" onClick={this.props.onNext}>
+                    Go to next step
+                  </ButtonPrimary>
+                ) : (
+                  <ButtonPrimary
+                    data-test-id="seed-display-next-words"
+                    onClick={this.handleNextPage}
+                  >
+                    {`next ${WORDS_PER_PAGE} words`}
+                  </ButtonPrimary>
+                )}
+              </Col>
+            </Row>
           </Col>
         </Row>
-      </LayoutAuthorized>
+        <Row>
+          <Col>
+            <ArrowButton arrowDirection="left" onClick={this.props.onBack}>
+              Back
+            </ArrowButton>
+          </Col>
+        </Row>
+      </PanelWhite>
     );
   }
 }
+
+export const BackupSeedDisplay: React.SFC<IBackupSeedDisplayProps> = props => (
+  <LayoutAuthorized>
+    <BreadCrumb
+      className="my-4"
+      path={["Settings", "Security settings"]}
+      view="Backup recovery phase"
+    />
+    <Row>
+      <Col md={12} lg={{ size: 10, offset: 1 }} xl={{ size: 8, offset: 2 }}>
+        <BackupSeedDisplayComponent {...props} />
+      </Col>
+    </Row>
+  </LayoutAuthorized>
+);

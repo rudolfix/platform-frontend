d9106be6c5fbb38a29a3e6da07eaf752ffb01fb9
Patryk Lizoń <patryk@hangover.studio>
2018-03-18 20:57:32 +0100

    Add option for modifying size @ inline icon

--- app/components/shared/InlineIcon.tsx
+++ app/components/shared/InlineIcon.tsx
@@ -3,12 +3,15 @@ import * as styles from "./InlineIcon.module.scss";
 
 interface IProps {
   svgIcon: string;
+  width?: string;
+  height?: string;
 }
 
-export const InlineIcon: React.SFC<IProps> = ({ svgIcon }) => {
+export const InlineIcon: React.SFC<IProps> = ({ svgIcon, width, height }) => {
   return (
     <span
       className={`inline-icon ${styles.inlineIcon}`}
+      style={{ width, height }}
       dangerouslySetInnerHTML={{ __html: svgIcon }}
     />
   );

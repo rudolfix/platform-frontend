ab687b70c727a7c7848221d9a3e2fa78ad93f7f0
Patryk Lizoń <patryk@hangover.studio>
2018-03-19 12:51:00 +0100

    Add missing component

--- app/components/kyc/KycPanel.tsx
+++ app/components/kyc/KycPanel.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as styles from "./KycPanel.module.scss";
 
-import { ArrowLink } from "../shared/ArrowLink";
+import { ArrowLink } from "../shared/ArrowNavigation";
 import { PanelWhite } from "../shared/PanelWhite";
 import { IProgresStepper, ProgressStepper } from "../shared/ProgressStepper";
 

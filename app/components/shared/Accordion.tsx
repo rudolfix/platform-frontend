18665b2cab1c0e633f66d9474442add11bf88169
Patryk Lizoń <patryk@hangover.studio>
2018-03-19 09:06:18 +0100

    Add Accordion component

--- app/components/shared/Accordion.tsx
+++ app/components/shared/Accordion.tsx
@@ -1,6 +1,8 @@
-import * as React from 'react';
-import * as styles from './Accordion.module.scss';
-import { InlineIcon } from './InlineIcon';
+import * as React from "react";
+import * as styles from "./Accordion.module.scss";
+import { InlineIcon } from "./InlineIcon";
+
+import * as indicatorIcon from "../../assets/img/inline_icons/accordion_arrow.svg";
 
 interface IAccordionElementProps {
   title: string;
@@ -8,38 +10,37 @@ interface IAccordionElementProps {
 }
 
 interface IAccordionElementState {
-  isOpened: boolean
+  isOpened: boolean;
 }
 
-export class AccordionElement extends React.Component<IAccordionElementProps, IAccordionElementState> {
-
+export class AccordionElement extends React.Component<
+  IAccordionElementProps,
+  IAccordionElementState
+> {
   state = {
-    isOpened: false
-  }
+    isOpened: false,
+  };
 
   toggleClose = () => {
-    this.setState(({ isOpened: !this.state.isOpened }))
-  }
+    this.setState({ isOpened: !this.state.isOpened });
+  };
 
   render(): React.ReactChild {
     const { title, children } = this.props;
     const { isOpened } = this.state;
 
     return (
-      <div className={styles.accordionElement}>
-        <h4 className={`${styles.accordionElement} ${isOpened ? 'is-opened' : ''}`} onClick={this.toggleClose}>
+      <div className={`${styles.accordionElement} ${isOpened ? "" : "is-closed"}`}>
+        <h4 className={styles.title} onClick={this.toggleClose}>
           <span>{title}</span>
-          <InlineIcon svgIcon={''} />
+          <InlineIcon width="22px" height="22px" svgIcon={indicatorIcon} />
         </h4>
-        <div>{children}</div>
+        <div className={styles.content}>{children}</div>
       </div>
-    )
+    );
   }
 }
 
-
 export const Accordion: React.SFC = ({ children }) => (
-  <div>
-    {children}
-  </div>
-)
+  <div className={styles.accordion}>{children}</div>
+);

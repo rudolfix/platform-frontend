11be44f0ffd166351e52f4f6173214c4866b2ea8
Patryk Lizoń <patryk@hangover.studio>
2018-03-19 13:21:53 +0100

    Fix linter

--- app/components/shared/MoneySuiteWidget.tsx
+++ app/components/shared/MoneySuiteWidget.tsx
@@ -12,22 +12,23 @@ interface IMoneySuiteWidgetProps {
 
 export const MoneySuiteWidget: React.SFC<
   IMoneySuiteWidgetProps & React.HTMLAttributes<HTMLDivElement>
-  > = ({ icon, currency, largeNumber, value, percentage }) => (
-    <>
-      <div className={styles.moneySuiteWidget}>
-        <img className={styles.icon} src={icon} />
-        <div>
-          <div className={styles.money}>
-            <span className={styles.currency}>
-              {selectCurrencySymbol(currency)}
-            </span>
-            <Money value={largeNumber} currency={currency} noCurrencySymbol />
-          </div>
-          <div className={styles.totalMoney}>
-            = <Money value={value} currency="eur" />
-            <span className={`${parseInt(percentage, 10) > 0 ? styles.green : styles.red}`}> ({percentage}%)</span>
-          </div>
+> = ({ icon, currency, largeNumber, value, percentage }) => (
+  <>
+    <div className={styles.moneySuiteWidget}>
+      <img className={styles.icon} src={icon} />
+      <div>
+        <div className={styles.money}>
+          <span className={styles.currency}>{selectCurrencySymbol(currency)}</span>
+          <Money value={largeNumber} currency={currency} noCurrencySymbol />
+        </div>
+        <div className={styles.totalMoney}>
+          = <Money value={value} currency="eur" />
+          <span className={`${parseInt(percentage, 10) > 0 ? styles.green : styles.red}`}>
+            {" "}
+            ({percentage}%)
+          </span>
         </div>
       </div>
-    </>
-  );
+    </div>
+  </>
+);

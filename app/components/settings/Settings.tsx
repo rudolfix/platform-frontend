3c14905f7834013a8845ca6b6983ee2b7395606c
Tomek Banach <tomek@banciur.org>
2018-03-20 13:41:45 +0100

    Conditionally show BackupSeedWidget only for LightWallet

--- app/components/settings/Settings.tsx
+++ app/components/settings/Settings.tsx
@@ -1,25 +1,39 @@
 import * as React from "react";
 import { Col, Row } from "reactstrap";
 
+import { selectIsLightWallet } from "../../modules/web3/reducer";
+import { appConnect } from "../../store";
 import { LayoutAuthorized } from "../layouts/LayoutAuthorized";
 import { MessageSignModal } from "../modals/SignMessageModal";
 import { BackupSeedWidget } from "./backupSeed/BackupSeedWidget";
 import { KycStatusWidget } from "./kycStates/KycStatusWidget";
 import { VerifyEmailWidget } from "./verifyEmail/VerifyEmailWidget";
 
-export const Settings = () => (
+interface IProps {
+  isLightWallet: boolean;
+}
+
+export const SettingsComponent: React.SFC<IProps> = ({ isLightWallet }) => (
   <LayoutAuthorized>
     <MessageSignModal />
     <Row>
       <Col lg={4} xs={12}>
         <VerifyEmailWidget />
       </Col>
-      <Col lg={4} xs={12}>
-        <BackupSeedWidget />
-      </Col>
+      {isLightWallet && (
+        <Col lg={4} xs={12}>
+          <BackupSeedWidget />
+        </Col>
+      )}
       <Col lg={4} xs={12}>
         <KycStatusWidget />
       </Col>
     </Row>
   </LayoutAuthorized>
 );
+
+export const Settings = appConnect<IProps, {}>({
+  stateToProps: s => ({
+    isLightWallet: selectIsLightWallet(s.web3State),
+  }),
+})(SettingsComponent);

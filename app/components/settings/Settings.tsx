0da0ba099513b4f8c37194a24b9e51d54dcd4086
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-03-20 18:22:02 +0100

    fixes BackupSeedWidget Visibility

--- app/components/settings/Settings.tsx
+++ app/components/settings/Settings.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Col, Row } from "reactstrap";
 
-import { selectIsLightWallet } from "../../modules/web3/reducer";
+import { isLightWalletReadyToLogin, selectIsLightWallet } from "../../modules/web3/reducer";
 import { appConnect } from "../../store";
 import { LayoutAuthorized } from "../layouts/LayoutAuthorized";
 import { MessageSignModal } from "../modals/SignMessageModal";
@@ -11,20 +11,25 @@ import { VerifyEmailWidget } from "./verifyEmail/VerifyEmailWidget";
 
 interface IProps {
   isLightWallet: boolean;
+  isLightWalletReadyToLogin: boolean;
 }
 
-export const SettingsComponent: React.SFC<IProps> = ({ isLightWallet }) => (
+export const SettingsComponent: React.SFC<IProps> = ({
+  isLightWallet,
+  isLightWalletReadyToLogin,
+}) => (
   <LayoutAuthorized>
     <MessageSignModal />
     <Row>
       <Col lg={4} xs={12}>
         <VerifyEmailWidget />
       </Col>
-      {isLightWallet && (
-        <Col lg={4} xs={12}>
-          <BackupSeedWidget />
-        </Col>
-      )}
+      {isLightWallet ||
+        (isLightWalletReadyToLogin && (
+          <Col lg={4} xs={12}>
+            <BackupSeedWidget />
+          </Col>
+        ))}
       <Col lg={4} xs={12}>
         <KycStatusWidget />
       </Col>
@@ -35,5 +40,6 @@ export const SettingsComponent: React.SFC<IProps> = ({ isLightWallet }) => (
 export const Settings = appConnect<IProps, {}>({
   stateToProps: s => ({
     isLightWallet: selectIsLightWallet(s.web3State),
+    isLightWalletReadyToLogin: isLightWalletReadyToLogin(s.web3State),
   }),
 })(SettingsComponent);

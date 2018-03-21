40e077e7877182036bbb8856d67657a1809ed710
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-03-21 16:50:39 +0100

    fixes BackupSeedDisplay tests

--- app/components/settings/backupSeed/BackupSeedDisplay.spec.tsx
+++ app/components/settings/backupSeed/BackupSeedDisplay.spec.tsx
@@ -34,7 +34,7 @@ const words = [
 ];
 
 describe("<BackupSeedDisplay />", () => {
-  it("should render all words in correct on consecutive pages", () => {
+  it("should render first half of words", () => {
     const component = shallow(
       <BackupSeedDisplay pageNo={0} onBack={noop} onNext={noop} words={words} />,
     );
@@ -43,14 +43,28 @@ describe("<BackupSeedDisplay />", () => {
     component.find(tid("seed-display-word")).forEach(node => {
       renderedWords.push(node.childAt(1).text());
     });
-    component.find(tid("seed-display-next-words")).simulate("click");
+
+    for (let index = 0; index < words.length / 2; index++) {
+      expect(
+        renderedWords[index].includes(words[index]),
+        `rendered word: ${renderedWords[index]} doesn't include: ${words[index]}`,
+      ).to.be.true;
+    }
+  });
+
+  it("should render second half of words", () => {
+    const component = shallow(
+      <BackupSeedDisplay pageNo={1} onBack={noop} onNext={noop} words={words} />,
+    );
+
+    const renderedWords: string[] = [];
     component.find(tid("seed-display-word")).forEach(node => {
       renderedWords.push(node.childAt(1).text());
     });
 
-    for (let index = 0; index < words.length; index++) {
+    for (let index = 0; index < words.length / 2; index++) {
       expect(
-        renderedWords[index].includes(words[index]),
+        renderedWords[index].includes(words[index + words.length / 2]),
         `rendered word: ${renderedWords[index]} doesn't include: ${words[index]}`,
       ).to.be.true;
     }
@@ -64,20 +78,17 @@ describe("<BackupSeedDisplay />", () => {
     expect(component.find(tid("seed-display-prev-words")).prop("disabled")).to.be.true;
   });
 
-  it("previous button should not be disabled on next page", () => {
+  it("Next button should not be disabled on next page", () => {
     const component = shallow(
-      <BackupSeedDisplay pageNo={0} onBack={noop} onNext={noop} words={words} />,
+      <BackupSeedDisplay pageNo={1} onBack={noop} onNext={noop} words={words} />,
     );
-    component.find(tid("seed-display-next-words")).simulate("click");
-
     expect(component.find(tid("seed-display-prev-words")).prop("disabled")).to.be.false;
   });
 
   it("should not render next 12 words button on last page but and show next link", () => {
     const component = shallow(
-      <BackupSeedDisplay pageNo={0} onBack={noop} onNext={noop} words={words} />,
+      <BackupSeedDisplay pageNo={1} onBack={noop} onNext={noop} words={words} />,
     );
-    component.find(tid("seed-display-next-words")).simulate("click");
 
     expect(component.find(tid("seed-display-next-words"))).to.have.length(0);
     expect(component.find(tid("seed-display-next-link"))).to.have.length(1);

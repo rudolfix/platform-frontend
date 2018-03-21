9245d9b943e91e86651d6128099399936a32de65
Patryk Lizoń <patryk@hangover.studio>
2018-03-21 01:37:57 +0100

    Refactor buttons components

--- app/components/shared/Buttons.tsx
+++ app/components/shared/Buttons.tsx
@@ -1,36 +1,52 @@
 import * as cn from "classnames";
 import * as React from "react";
 import { Link, LinkProps } from "react-router-dom";
-import { Button, ButtonProps } from "reactstrap";
 import { InlineIcon } from "./InlineIcon";
 
 import * as styles from "./Buttons.module.scss";
 
 import * as closeIcon from "../../assets/img/inline_icons/close.svg";
 
-export const ButtonPrimary: React.SFC<ButtonProps> = ({ className, children, ...props }) => (
-  <Button className={cn(styles.buttonPrimary, className)} {...props}>
-    {children}
-  </Button>
-);
 
-export const ButtonPrimaryLink: React.SFC<LinkProps> = ({ className, ...props }) => (
-  <Link className={cn(className, styles.buttonPrimary, "btn")} {...props} />
-);
+type TButtonLayout = "primary" | "secondary" | "icon-before" | "icon-after";
 
-export const ButtonSecondary: React.SFC<ButtonProps> = ({ className, children, ...props }) => (
-  <Button className={cn(styles.buttonSecondary, className)} {...props}>
-    {children}
-  </Button>
-);
+type TButtonTheme = "t-dark" | "t-white";
 
-export const ButtonSecondaryLink: React.SFC<LinkProps> = ({ className, ...props }) => (
-  <Link className={cn(className, styles.buttonSecondary, "btn")} {...props} />
-);
+interface IButtonProps {
+  layout?: TButtonLayout;
+  theme?: TButtonTheme;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<any>;
+  svgIcon?: string;
+  to?: string;
+  type?: string;
+}
+
+
+export const Button: React.SFC<IButtonProps> = ({ children, layout, theme, disabled, svgIcon, ...props }) => {
+  return (
+    <button className={cn('button', layout, theme)} disabled={ disabled } tabIndex={0} {...props}>
+      <div className={styles.content} tabIndex={-1}>
+        { layout === "icon-before" && <InlineIcon svgIcon={svgIcon || ''} />}
+        { children }
+        { layout === "icon-after" && <InlineIcon svgIcon={svgIcon || ''} />}
+      </div>
+    </button>
+  )
+}
+
+
+Button.defaultProps = {
+  layout: "primary",
+  theme: "t-dark",
+  disabled: false,
+};
+
 
 interface IButtonClose {
   handleClick?: () => void;
 }
+
 
 export const ButtonClose: React.SFC<IButtonClose> = ({ ...props }) => (
   <div className={styles.buttonClose}>

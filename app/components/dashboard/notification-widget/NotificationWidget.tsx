e7f1398fe58482c19e82b1c21d0e548649ea5a86
Tomek Banach <tomek@banciur.org>
2018-03-22 13:35:34 +0100

    NotificationWidget can display notification passed as props

--- app/components/dashboard/notification-widget/NotificationWidget.tsx
+++ app/components/dashboard/notification-widget/NotificationWidget.tsx
@@ -4,17 +4,27 @@ import { INotification } from "../../../modules/notifications/reducer";
 import { appConnect, AppDispatch } from "../../../store";
 import { Notification } from "./Notification";
 
+interface IExternalProps {
+  notifications?: INotification[];
+}
+
 interface IStateProps {
-  notifications: INotification[];
+  stateNotifications: INotification[];
 }
 
 interface IDispatchProps {
   dispatch: AppDispatch;
 }
 
-export class NotificationWidgetComponent extends React.Component<IStateProps & IDispatchProps> {
+type IProps = IStateProps & IDispatchProps & IExternalProps;
+
+export class NotificationWidgetComponent extends React.Component<IProps> {
   render(): React.ReactNode {
-    return this.props.notifications.map((notification, index) => (
+    const mergedNotifications = this.props.stateNotifications.concat(
+      this.props.notifications || [],
+    );
+
+    return mergedNotifications.map((notification, index) => (
       <Notification
         key={notification.text + index.toString(10)}
         type={notification.type}
@@ -27,7 +37,7 @@ export class NotificationWidgetComponent extends React.Component<IStateProps & I
 }
 
 export const NotificationWidget = appConnect<IStateProps, IDispatchProps>({
-  stateToProps: s => ({ notifications: s.notifications.notifications }),
+  stateToProps: s => ({ stateNotifications: s.notifications.notifications }),
   dispatchToProps: dispatch => ({
     dispatch,
   }),

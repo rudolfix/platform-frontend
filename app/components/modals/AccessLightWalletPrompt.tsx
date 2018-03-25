e9242c7ce05baa2901754aea2b05c4bf648b9db6
David Scharf <dscharf@gmx.net>
2018-03-25 23:15:00 +0200

    adds on enter submit to light wallet signing prompt

--- app/components/modals/AccessLightWalletPrompt.tsx
+++ app/components/modals/AccessLightWalletPrompt.tsx
@@ -1,10 +1,7 @@
+import { Form, FormikProps, withFormik } from "formik";
 import * as React from "react";
-import { Input } from "reactstrap";
 import { Button } from "../shared/Buttons";
-
-interface IState {
-  password: string;
-}
+import { FormField } from "../shared/forms/forms";
 
 interface IStateProps {
   isUnlocked: boolean;
@@ -17,44 +14,46 @@ interface IOwnProps {
 
 type IProps = IStateProps & IOwnProps;
 
-export class AccessLightWalletPrompt extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      password: "",
-    };
-  }
-
-  private passwordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      password: e.target.value,
-    });
-  };
-
-  render(): React.ReactNode {
-    const { onAccept, onCancel } = this.props;
-    return (
-      <div>
-        {this.props.isUnlocked ? (
-          <div />
-        ) : (
-          <div>
-            <p>Please enter your password to unlock the wallet</p>
-            <Input
-              placeholder="password"
-              type="password"
-              onChange={this.passwordChange}
-              value={this.state.password}
-            />
-          </div>
-        )}
-        <div className="mt-3">
-          <Button onClick={() => onAccept(this.state.password)}>Accept</Button>
-          <Button layout="secondary" onClick={onCancel}>
-            Reject
-          </Button>
-        </div>
-      </div>
-    );
-  }
+export interface IFormValues {
+  password: string;
 }
+
+const AccessLightWalletForm = (formikBag: FormikProps<IFormValues> & IProps) => (
+  <Form>
+    <FormField
+      type={"password"}
+      placeholder="Password"
+      touched={formikBag.touched}
+      errors={formikBag.errors}
+      name={"password"}
+    />
+    <div className="mt-3">
+      <Button type="submit" disabled={!formikBag.values.password}>
+        {" "}
+        Accept
+      </Button>
+      <Button layout="secondary" onClick={formikBag.onCancel}>
+        Reject
+      </Button>
+    </div>
+  </Form>
+);
+
+const EnhancedForm = withFormik<IProps, IFormValues>({
+  handleSubmit: (values, props) => props.props.onAccept(values.password),
+})(AccessLightWalletForm);
+
+export const AccessLightWalletPrompt: React.SFC<IProps> = props =>
+  props.isUnlocked ? (
+    <div className="mt-3">
+      <Button onClick={() => props.onAccept()}>Accept</Button>
+      <Button layout="secondary" onClick={props.onCancel}>
+        Reject
+      </Button>
+    </div>
+  ) : (
+    <div>
+      <p>Please enter your password to unlock the wallet</p>
+      <EnhancedForm {...props} />
+    </div>
+  );

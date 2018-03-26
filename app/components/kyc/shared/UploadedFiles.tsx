2f8819f587899c364d8230e5aea5ba133e9760f2
David <dscharf@gmx.net>
2018-03-26 15:55:40 +0200

    changes to uploaded files field

--- app/components/kyc/shared/UploadedFiles.tsx
+++ app/components/kyc/shared/UploadedFiles.tsx
@@ -4,9 +4,6 @@ import * as styles from "./UploadedFiles.module.scss";
 
 import * as confirmIcon from "../../../assets/img/notfications/Success_small.svg";
 
-import * as removeIcon from "../../../assets/img/inline_icons/trash.svg";
-import { InlineIcon } from "../../shared/InlineIcon";
-
 interface IProps {
   files: IKycFileInfo[];
   onRemove: () => void;
@@ -17,7 +14,7 @@ export const UploadedFiles: React.SFC<IProps> = ({ files, onRemove }) => (
     {files.map(({ fileName }) => (
       <div key={fileName} className={styles.file}>
         <span className={styles.remove} onClick={() => onRemove()}>
-          <InlineIcon svgIcon={removeIcon} />
+          {/* <InlineIcon svgIcon={removeIcon} /> */}
         </span>
         <span className={styles.text} title={fileName}>
           {fileName}

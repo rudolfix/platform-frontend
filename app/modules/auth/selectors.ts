e4c9c73831fed0847eecf6214d71229dc42865d4
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-04-09 15:56:12 +0200

    makes selectors stricter

--- app/modules/auth/selectors.ts
+++ app/modules/auth/selectors.ts
@@ -5,9 +5,9 @@ export const selectIsAuthorized = (state: IAuthState): boolean => !!(state.jwt &
 export const selectUserEmail = (state: IAuthState): string | undefined =>
   state.user && (state.user.unverifiedEmail || state.user.verifiedEmail);
 export const selectUser = (state: IAuthState): IUser | undefined => state.user;
-export const selectBackupCodesVerified = (state: IAuthState): boolean | undefined =>
-  state.user && state.user.backupCodesVerified;
-export const selectIsUserEmailVerified = (state: IAuthState): boolean | undefined =>
-  state.user && !!state.user.verifiedEmail && !state.user.unverifiedEmail;
-export const selectIsThereUnverifiedEmail = (state: IAuthState): boolean | undefined =>
-  state.user && !!state.user.unverifiedEmail;
+export const selectBackupCodesVerified = (state: IAuthState): boolean =>
+  !!state.user && !!state.user.backupCodesVerified;
+export const selectIsUserEmailVerified = (state: IAuthState): boolean =>
+  !!state.user && !!state.user.verifiedEmail;
+export const selectIsThereUnverifiedEmail = (state: IAuthState): boolean =>
+  !!state.user && !!state.user.unverifiedEmail;

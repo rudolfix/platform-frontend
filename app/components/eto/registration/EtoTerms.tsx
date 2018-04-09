0ec0e46a8cc59179a2448adae230a31506c8a423
Patryk Lizoń <patryk@hangover.studio>
2018-04-09 19:23:56 +0200

    Restore ETO Terms view

--- app/components/eto/registration/EtoTerms.tsx
+++ app/components/eto/registration/EtoTerms.tsx
@@ -14,9 +14,27 @@ import { onEnterAction } from "../../../utils/OnEnterAction";
 import { Button } from "../../shared/Buttons";
 import { EtoRegistrationPanel } from "./EtoRegistrationPanel";
 
-import { FormField } from "../../shared/forms/forms";
+import { BOOL_FALSE_KEY, BOOL_TRUE_KEY, FormField, FormSelectField, NONE_KEY } from "../../shared/forms/forms";
 
+import { Link } from "react-router-dom";
+import { Col, Row } from "reactstrap";
 import { EtoTermsSchemaRequired, IEtoTerms } from "../../../lib/api/EtoApi.interfaces";
+import { HorizontalLine } from "../../shared/HorizontalLine";
+import { SingleFileUpload } from "../../shared/SingleFileUpload";
+
+import * as iconExternalLink from "../../../assets/img/inline_icons/link_out_small.svg";
+
+const THIRD_PARTIES_DEPENDENCY_VALUES = {
+  [NONE_KEY]: "-please select-",
+  [BOOL_TRUE_KEY]: "Yes there is",
+  [BOOL_FALSE_KEY]: "No there is not",
+};
+
+const SUBJECT_OF_REGULATION_VALUES = {
+  [NONE_KEY]: "-please select-",
+  [BOOL_TRUE_KEY]: "Yes there is",
+  [BOOL_FALSE_KEY]: "No there is not",
+};
 
 interface IStateProps {
   currentValues: IEtoTerms;
@@ -31,7 +49,54 @@ type IProps = IStateProps & IDispatchProps;
 
 const EtoForm = (formikBag: FormikProps<IEtoTerms> & IProps) => (
   <Form>
-    <FormField label="First name" name="firstName" />
+    <Row className="justify-content-center">
+      <Col xs={12} lg={6}>
+        <div className="mb-4">
+          <FormField label="How many shares do you want to tokenize?" placeholder="%" name="tokenizedSharesPercent" />
+        </div>
+        <div className="mb-4">
+          <FormField label="What is the price a share?" placeholder="€" name="tokenPrice" />
+        </div>
+        <div className="mb-4">
+          <p>Please use 3 characters and check if this name is available here:</p>
+          <Link to="#0">
+            <Button svgIcon={iconExternalLink} layout="secondary" iconPosition="icon-after">Etherscan</Button>
+          </Link>
+          <Link to="#0">
+            <Button svgIcon={iconExternalLink} layout="secondary" iconPosition="icon-after">Etherscan</Button>
+          </Link>
+        </div>
+        <div className="mb-4">
+          <FormField label="Your token Name" placeholder="XXX" name="tokenName" />
+        </div>
+        <SingleFileUpload
+          files={[]}
+          fileUploading={false}
+          filesLoading={false}
+          fileFormatInformation=".jpg, .svg, .png"
+          uploadCta="Upload token symbol"
+          onDropFile={() => { }}
+        />
+      </Col>
+    </Row>
+    <HorizontalLine className="my-4" />
+    <h4 className="text-center mb-4">Risk assessment</h4>
+    <Row className="justify-content-center">
+      <Col xs={12} lg={6}>
+        <div className="mb-4">
+          <FormSelectField
+            values={THIRD_PARTIES_DEPENDENCY_VALUES}
+            label="Ist there a dependency on third parties"
+            name="hasDependencyOnThirdParties"
+          />
+          <FormSelectField
+            values={SUBJECT_OF_REGULATION_VALUES}
+            label="Ist there a dependency on third parties"
+            name="hasDependencyOnThirdParties"
+          />
+        </div>
+      </Col>
+    </Row>
     <div className="p-4 text-center">
       <Button type="submit" disabled={!formikBag.isValid || formikBag.loadingData}>
         Submit and continue
@@ -52,7 +117,7 @@ export const EtoRegistrationTermsComponent: React.SFC<IProps> = props => (
   <EtoRegistrationPanel
     steps={6}
     currentStep={6}
-    title={"Eto Terms"}
+    title={"ETO Terms"}
     hasBackButton={false}
     isMaxWidth={true}
   >
@@ -67,10 +132,10 @@ export const EtoRegistrationTerms = compose<React.SFC>(
       currentValues: {},
     }),
     dispatchToProps: _dispatch => ({
-      submitForm: () => {},
+      submitForm: () => { },
     }),
   }),
   onEnterAction({
-    actionCreator: _dispatch => {},
+    actionCreator: _dispatch => { },
   }),
 )(EtoRegistrationTermsComponent);

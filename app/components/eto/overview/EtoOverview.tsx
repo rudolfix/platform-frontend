bed5aaab5596386abd73ea5df35375b399e68faf
Patryk Lizoń <patryk@hangover.studio>
2018-04-12 15:16:59 +0200

    Add EtoOverview component

---
+++ app/components/eto/overview/EtoOverview.tsx
@@ -0,0 +1,98 @@
+import * as cn from "classnames";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
+
+import { Money, TCurrency } from "../../shared/Money";
+import { PanelWhite } from "../../shared/PanelWhite";
+import { ITag, Tag } from "../../shared/Tag";
+
+import * as styles from "./EtoOverview.module.scss";
+
+interface IProps {
+  companyName: string;
+  logoImageSrc: string;
+  description: string;
+  tags: ITag[];
+  startDate: string;
+  endDate: string;
+  tokenImageSrc: string;
+  goalValue: string;
+  goalSymbol: TCurrency;
+  currentValueationValue: string;
+  currentValueationSymbol: TCurrency;
+  tokenPriceNeu: string;
+  tokenPriceEth: string;
+  tokenSymbol: string;
+  linkToDetailedTokenInfo: string;
+}
+
+export const EtoOverview: React.SFC<IProps> = props => {
+  return (
+    <PanelWhite>
+      <Container>
+        <Row>
+          <Col>
+            <div className={styles.etoOverview}>
+              <div className={styles.aboutWrapper}>
+                <img
+                  className={styles.logoImage}
+                  src={props.logoImageSrc}
+                  alt={`${props.companyName} logo`}
+                />
+                <div>
+                  <h4>{props.companyName}</h4>
+                  <p>{props.description}</p>
+                  <div>{props.tags.map(({ text }) => <Tag text={text} key={text} />)}</div>
+                </div>
+              </div>
+              <div className={styles.detailsWrapper}>
+                <div className={styles.detailBox}>
+                  <div>
+                    <div className={styles.label}>Start</div>
+                    <div className={styles.value}>{props.startDate}</div>
+                  </div>
+                  <div>
+                    <div className={styles.label}>End</div>
+                    <div className={styles.value}>{props.endDate}</div>
+                  </div>
+                </div>
+                <div className={styles.detailBox}>
+                  <div>
+                    <div className={styles.label}>Goal</div>
+                    <div className={styles.value}>
+                      <Money currency="neu" value={props.goalValue} />
+                    </div>
+                  </div>
+                  <div>
+                    <div className={styles.label}>Current valuation</div>
+                    <div className={styles.value}>
+                      <Money currency="neu" value={props.goalValue} />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.detailBox}>
+                  <div className={styles.label}>Token price</div>
+                  <div className={cn(styles.value, styles.tokenPrice)}>
+                    <Money value={props.tokenPriceNeu} currency="neu" />
+                  </div>
+                  <div className={styles.label}>
+                    <Money value={props.tokenPriceEth} currency="eth" />
+                  </div>
+                </div>
+                <div className={styles.detailBox}>
+                  <div className={styles.label}>Company token</div>
+                  <div className={styles.token}>
+                    <img className={styles.tokenLogo} src={props.tokenImageSrc} />
+                    <div className={styles.tokenSymbol}>{props.tokenSymbol}</div>
+                  </div>
+                  <Link to={props.linkToDetailedTokenInfo}>Detailed token info ></Link>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </PanelWhite>
+  );
+};

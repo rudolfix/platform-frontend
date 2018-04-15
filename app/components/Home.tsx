dfc8993608a05866ec90f87034ea7a1e675d16ca
Krzysztof Kaczor <chris@kaczor.io>
2018-04-15 11:06:30 +0200

    modify landing page to have new set of links

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
+
 import { appRoutes } from "./AppRouter";
 import { Button } from "./shared/Buttons";
+import { SectionHeader } from "./shared/SectionHeader";
 
 export const Home: React.SFC = () => (
   <Container>
@@ -10,12 +12,31 @@ export const Home: React.SFC = () => (
       <Col>
         <h2 data-test-id="homepage-title">Home</h2>
         <p>Not logged in</p>
+      </Col>
+    </Row>
+    <Row className="mb-5">
+      <Col>
+        <SectionHeader>Investors:</SectionHeader>
         <Link to={appRoutes.login}>
           <Button>Login</Button>
         </Link>
         <br />
         <br />
         <Link to={appRoutes.register}>
+          <Button>Register</Button>
+        </Link>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col>
+        <SectionHeader>Issuers:</SectionHeader>
+        <Link to={appRoutes.etoLogin}>
+          <Button>Login</Button>
+        </Link>
+        <br />
+        <br />
+        <Link to={appRoutes.etoRegister}>
           <Button>Register</Button>
         </Link>
       </Col>

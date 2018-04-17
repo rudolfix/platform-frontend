06859e85769fceecbce11692ba3edd796a195dc3
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-04-17 15:14:55 +0200

    adds  ETO kyc constraints

--- app/components/kyc/Router.tsx
+++ app/components/kyc/Router.tsx
@@ -5,6 +5,9 @@ import { kycRoutes } from "./routes";
 import { KYCStart } from "./start/Start";
 
 // private
+import { TUserType } from "../../lib/api/users/interfaces";
+import { selectUserType } from "../../modules/auth/selectors";
+import { appConnect } from "../../store";
 import { KYCBeneficialOwners } from "./business/BeneficialOwners";
 import { KycBusinessData } from "./business/BusinessData";
 import { KycLegalRepresentative } from "./business/LegalRepresentative";
@@ -13,7 +16,11 @@ import { KycPersonalInstantId } from "./personal/InstantId";
 import { KYCPersonalStart } from "./personal/Start";
 import { KYCPersonalUpload } from "./personal/Upload";
 
-export const KycRouter: React.SFC = () => (
+interface IStateProps {
+  userType?: TUserType;
+}
+
+export const NormalKycRouter: React.SFC = () => (
   <Switch>
     <Route path={kycRoutes.start} component={KYCStart} exact />
 
@@ -31,3 +38,34 @@ export const KycRouter: React.SFC = () => (
     <Redirect to={kycRoutes.start} />
   </Switch>
 );
+
+export const EtoKycRouter: React.SFC = () => (
+  <Switch>
+    <Route path={kycRoutes.start} component={KycBusinessStart} exact />
+
+    {/* Business Only*/}
+    <Route path={kycRoutes.businessStart} component={KycBusinessStart} />
+    <Route path={kycRoutes.legalRepresentative} component={KycLegalRepresentative} />
+    <Route path={kycRoutes.businessData} component={KycBusinessData} />
+    <Route path={kycRoutes.beneficialOwners} component={KYCBeneficialOwners} />
+
+    <Redirect to={kycRoutes.businessStart} />
+  </Switch>
+);
+
+export const KycRouterComponent: React.SFC<IStateProps> = ({ userType }) => {
+  switch (userType) {
+    case "investor":
+      return <NormalKycRouter />;
+    case "issuer":
+      return <EtoKycRouter />;
+    default:
+      throw new Error("Wrong user type, user should be either issuer or investor");
+  }
+};
+
+export const KycRouter = appConnect<IStateProps, {}>({
+  stateToProps: s => ({
+    userType: selectUserType(s.auth),
+  }),
+})(KycRouterComponent);

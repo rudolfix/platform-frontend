8a87ed5574f999d4b2165c9abc7ae5c7298006f0
Patryk Lizoń <patryk@hangover.studio>
2018-04-18 16:36:24 +0200

    Add theme && size props for HorizontalLine component

--- app/components/shared/HorizontalLine.tsx
+++ app/components/shared/HorizontalLine.tsx
@@ -3,10 +3,20 @@ import * as React from "react";
 
 import * as styles from "./HorizontalLine.module.scss";
 
+type TSize = "narrow" | "wide";
+
+type TTheme = "yellow";
+
 interface IHorizontalLineProps {
   className?: string;
+  size?: TSize;
+  theme?: TTheme;
 }
 
-export const HorizontalLine: React.SFC<IHorizontalLineProps> = ({ className }) => (
-  <div className={cn(styles.horizontalLine, className)} />
+export const HorizontalLine: React.SFC<IHorizontalLineProps> = ({ className, size, theme }) => (
+  <div className={cn(styles.horizontalLine, className, size, theme)} />
 );
+
+HorizontalLine.defaultProps = {
+  size: "wide"
+};

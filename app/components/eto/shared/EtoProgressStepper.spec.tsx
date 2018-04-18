c428fc2cb603996a6c9f9f7c875eebf01ffb8b16
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-04-18 16:38:08 +0200

    fixes tests

--- app/components/eto/shared/EtoProgressStepper.spec.tsx
+++ app/components/eto/shared/EtoProgressStepper.spec.tsx
@@ -13,20 +13,21 @@ const mockData = [
   {
     name: "Test",
     isDone: false,
-    onClick: spy(),
   },
 ];
 
+const spyFunction = spy();
+
 describe("EtoProgressStepper", () => {
   it("Should show check when isDone is true", () => {
-    const component1 = shallow(<EtoProgressStepper currentStep={1} stepProps={[mockData[0]]} />);
-    const component2 = shallow(<EtoProgressStepper currentStep={1} stepProps={[mockData[1]]} />);
+    const component1 = shallow(<EtoProgressStepper currentStep={1} stepProps={[mockData[0]]} onClick={spyFunction}/>);
+    const component2 = shallow(<EtoProgressStepper currentStep={1} stepProps={[mockData[1]]} onClick={spyFunction}/>);
 
     expect(component1.find(tid("check-icon")).length).to.be.eq(1);
     expect(component2.find(tid("check-icon")).length).to.be.eq(0);
   });
   it("Should render name of step when isDone is false", () => {
-    const component = shallow(<EtoProgressStepper currentStep={1} stepProps={[mockData[1]]} />);
+    const component = shallow(<EtoProgressStepper currentStep={1} stepProps={[mockData[1]]} onClick={spyFunction}/>);
 
     expect(component.find(tid("EtoProgressStepper-header-text")).text()).to.contain(
       mockData[1].name,
@@ -34,8 +35,8 @@ describe("EtoProgressStepper", () => {
   });
 
   it("Should simulate a click when square is clicked", () => {
-    const component = shallow(<EtoProgressStepper currentStep={1} stepProps={[mockData[1]]} />);
+    const component = shallow(<EtoProgressStepper currentStep={1} stepProps={[mockData[1]]} onClick={spyFunction}/>);
     component.find(tid("EtoProgressStepper-step-button")).simulate("click");
-    expect(mockData[1].onClick).to.be.calledOnce;
+    expect(spyFunction).to.be.calledOnce;
   });
 });

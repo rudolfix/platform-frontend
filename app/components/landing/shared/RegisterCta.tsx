d4ad2e3669180e152a7ce6595c1b55e0052fc6f6
Patryk Lizoń <patryk@hangover.studio>
2018-04-18 16:34:08 +0200

    Add ctaText for Button @ RegisterCta component

--- app/components/landing/shared/RegisterCta.tsx
+++ app/components/landing/shared/RegisterCta.tsx
@@ -6,14 +6,15 @@ import * as styles from './RegisterCta.module.scss';
 
 interface ISelfProps {
   text: string;
+  ctaText: string;
   onRegister: any;
 }
 
-export const RegisterCta: React.SFC<ISelfProps> = ({ text, onRegister }) => {
+export const RegisterCta: React.SFC<ISelfProps> = ({ text, ctaText, onRegister }) => {
   return (
     <section className={styles.registerCta}>
       <h2 className={styles.ctaText}>{text}</h2>
-      <Button onClick={onRegister} size="wide" theme="t-white">Register</Button>
+      <Button onClick={onRegister} size="wide" theme="t-white">{ctaText}</Button>
     </section>
   )
 }

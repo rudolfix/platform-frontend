668dcd06205f11c2e8d9ec6fc0130dab9f01dddb
Patryk Lizoń <patryk@hangover.studio>
2018-04-19 10:57:08 +0200

    Add OffToOnChain component

--- app/components/landing/shared/OffToOnChainCompany.tsx
+++ app/components/landing/shared/OffToOnChainCompany.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+
+import { HorizontalLine } from '../../shared/HorizontalLine';
+import { InlineIcon } from '../../shared/InlineIcon';
+
+import * as arrowHead from "../../../assets/img/inline_icons/arrowhead.svg";
 import * as styles from './OffToOnChainCompany.module.scss'
 
 interface IStepProps {
-  title: string;
-  __html: string | ;
+  htmlTitle: string;
+  htmlDescription: string;
 }
 
 interface IProps {
@@ -12,20 +17,25 @@ interface IProps {
 
 export const OffToOnCompany: React.SFC<IProps> = ({ steps }) => {
   return (
-    <div className={styles.offToOnCompany}>
-      <h3><strong>OFF-CHAIN</strong> company</h3>
-      {
-        steps.map(({ title, __html }) => (
-          <div className={styles.steps}>
-            <div className={styles.step}>
-              <h4 className={styles.stepName}>Register</h4>
-              <span className={styles.stepCount}>1</span>
-              <p className={styles.stepDescription} dangerouslySetInnerHTML={{ __html }} />
+    <div className={styles.offToOnChainCompany}>
+      <h3 className={styles.header}><strong>OFF-CHAIN</strong> company</h3>
+      <HorizontalLine theme="yellow" size="narrow" />
+      <div className={styles.steps}>
+        {
+          steps.map(({ htmlTitle, htmlDescription }, index) => (
+            <div className={styles.step} key={index}>
+              <h4 className={styles.stepName} dangerouslySetInnerHTML={{ __html: htmlTitle }} />
+              <div className={styles.stepCount}>
+                <div className={styles.counter}>{index + 1}</div>
+              </div>
+              <p className={styles.stepDescription} dangerouslySetInnerHTML={{ __html: htmlDescription }} />
             </div>
-          </div>
-        ))
-      }
-      <h3><strong>on-CHAIN</strong> company</h3>
+          ))
+        }
+        <InlineIcon svgIcon={arrowHead} />
+      </div>
+      <h3 className={styles.header}><strong>ON-CHAIN</strong> company</h3>
+      <HorizontalLine theme="yellow" size="narrow" />
     </div>
   )
 }

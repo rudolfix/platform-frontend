a5cdc03b251b8dd1597884297493e3fa6c2dffe1
Patryk Lizoń <patryk@hangover.studio>
2018-04-19 10:57:24 +0200

    Improve RegisterCta component

--- app/components/landing/shared/RegisterCta.tsx
+++ app/components/landing/shared/RegisterCta.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Button } from '../../shared/Buttons';
 
+import * as logo from '../../../assets/img/logo_capitalized.svg';
 import * as styles from './RegisterCta.module.scss';
 
 interface ISelfProps {
   text: string;
   ctaText: string;
-  onRegister: any;
+  ctaLink: string;
 }
 
-export const RegisterCta: React.SFC<ISelfProps> = ({ text, ctaText, onRegister }) => {
+export const RegisterCta: React.SFC<ISelfProps> = ({ text, ctaText, ctaLink }) => {
   return (
     <section className={styles.registerCta}>
+      <img className={styles.image} src={logo} alt="Neufund logo" />
       <h2 className={styles.ctaText}>{text}</h2>
-      <Button onClick={onRegister} size="wide" theme="t-white">{ctaText}</Button>
+      <Link to={ctaLink}>
+        <Button size="wide" theme="t-white">{ctaText}</Button>
+      </Link>
     </section>
   )
 }

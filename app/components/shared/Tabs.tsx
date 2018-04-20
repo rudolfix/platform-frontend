06cfd12da86623315e6a893c5a340c52206fc6f3
Patryk Lizoń <patryk@hangover.studio>
2018-04-20 12:06:19 +0200

    Improve Tabs component

--- app/components/shared/Tabs.tsx
+++ app/components/shared/Tabs.tsx
@@ -7,10 +7,11 @@ import * as styles from "./Tabs.module.scss";
 type TTheme = "dark" | "light";
 
 interface ITab {
-  path: string;
+  path?: string;
   text: string;
   handleClick?: () => void;
   dataTestId?: string;
+  isActive?: boolean;
 }
 
 interface IProps {
@@ -22,18 +23,23 @@ interface IProps {
 
 export const Tabs: React.SFC<IProps> = ({ tabs, theme, className, ...props }) => (
   <div className={cn(styles.tabs, className)} {...props}>
-    {tabs.map(({ path, text, handleClick, dataTestId }, index) => (
-      <NavLinkConnected
-        to={{ pathname: path, search: window.location.search }} // we pass all query string arguments. It's needed to make redirection back to authorized route work
-        className={cn(styles.tab, theme)}
-        data-test-id={dataTestId}
-        key={index}
-      >
-        <div key={text} onClick={handleClick}>
-          {text}
-        </div>
-      </NavLinkConnected>
-    ))}
+    {tabs.map(
+      ({ path, text, handleClick, dataTestId, isActive }, index) =>
+        path ? (
+          <NavLinkConnected
+            to={{ pathname: path, search: window.location.search }} // we pass all query string arguments. It's needed to make redirection back to authorized route work
+            className={cn(styles.tab, theme)}
+            data-test-id={dataTestId}
+            key={index}
+          >
+            <div>{text}</div>
+          </NavLinkConnected>
+        ) : (
+          <div onClick={handleClick} className={cn(styles.tab, isActive && "active")} key={index}>
+            {text}
+          </div>
+        ),
+    )}
   </div>
 );
 

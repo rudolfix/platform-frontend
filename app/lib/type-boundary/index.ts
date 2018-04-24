88ac0ffd1f35b0fdc026dd3b166ca0d321417212
Krzysztof Kaczor <chris@kaczor.io>
2018-04-24 10:30:00 +0200

    docs about type bundaries

--- app/lib/type-boundary/index.ts
+++ app/lib/type-boundary/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Set of this runtype types + io-ts should be used to define and validate type-boundaries in our system (i.e. form data, api responses).
+ * It deprecates yup validators.
+ */
+
 export { numberType, nonEmptyStringType } from "./primitives";
 export { IntegerFromString } from "./IntegerFromString";
 export { NumberFromString } from "./NumberFromString";

27cd31b1e2fb2e2d05fdda901fabda42fb0fc148
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-04-25 17:59:56 +0200

    adds littirals to MyNeuWidget

--- app/components/dashboard/myPortfolio/MyNeuWidget.tsx
+++ app/components/dashboard/myPortfolio/MyNeuWidget.tsx
@@ -5,6 +5,7 @@ import { Col, Row } from "reactstrap";
 import { Button } from "../../shared/Buttons";
 import { Money } from "../../shared/Money";
 
+import { FormattedMessage } from "react-intl";
 import * as arrowRight from "../../../assets/img/inline_icons/arrow_right.svg";
 import * as icon from "../../../assets/img/neu_icon.svg";
 import * as styles from "./MyNeuWidget.module.scss";
@@ -17,7 +18,9 @@ interface IProps {
 export const MyNeuWidget: React.SFC<IProps> = props => {
   return props.balanceNeu === "0" ? (
     <div className={cn(styles.background, "h-100")}>
-      <h5 className={cn(styles.title, "text-left mb-3 mb-md-5 pl-3 pt-1")}>YOUR NEUMARK</h5>
+      <h5 className={cn(styles.title, "text-left mb-3 mb-md-5 pl-3 pt-1")}>
+        <FormattedMessage id="dashboard.my-neu-widget.your-neumark" />
+      </h5>
       <Row noGutters>
         <Col xs={6} md={12} className="mb-2 mb-md-4 text-left ml-2 ml-md-0 text-md-center pt-1">
           <h3 className={cn(styles.neu)}>
@@ -27,14 +30,16 @@ export const MyNeuWidget: React.SFC<IProps> = props => {
         </Col>
         <Col md={12} className="text-right text-md-center" data-test-id="arrow-neu">
           <Button layout="secondary" iconPosition="icon-after" theme="t-white" svgIcon={arrowRight}>
-            About NEU
+            <FormattedMessage id="dashboard.my-neu-widget.about" />
           </Button>
         </Col>
       </Row>
     </div>
   ) : (
     <div className={cn(styles.background, "h-100")}>
-      <h5 className={cn(styles.title, "text-left mb-3 pl-2 pl-md-3 pt-2")}>MY NEUMARK</h5>
+      <h5 className={cn(styles.title, "text-left mb-3 pl-2 pl-md-3 pt-2")}>
+        <FormattedMessage id="dashboard.my-neu-widget.my-neu" />
+      </h5>
       <Row noGutters>
         <Col>
           <Row className="justify-content-md-center ml-3 ml-md-0" noGutters>

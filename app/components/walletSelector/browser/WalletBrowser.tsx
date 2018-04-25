32cd85f37dc52c5fb30b4730d2b55d3faf7819fa
Krzysztof Kaczor <chris@kaczor.io>
2018-04-25 23:52:34 +0200

    browser selector translations

--- app/components/walletSelector/browser/WalletBrowser.tsx
+++ app/components/walletSelector/browser/WalletBrowser.tsx
@@ -16,7 +16,9 @@ import * as browserIcon from "../../../assets/img/wallet_selector/browser_icon.s
 import * as lockIcon from "../../../assets/img/wallet_selector/lock_icon.svg";
 import * as walletIcon from "../../../assets/img/wallet_selector/wallet_icon.svg";
 
+import { FormattedMessage } from "react-intl";
 import { selectIsLoginRoute } from "../../../modules/wallet-selector/selectors";
+import { IIntlProps, injectIntlHelpers } from "../../../utils/injectIntlHelpers";
 import * as styles from "./WalletBrowser.module.scss";
 
 export const BROWSER_WALLET_RECONNECT_INTERVAL = 1000;
@@ -27,14 +29,19 @@ interface IWalletBrowserProps {
   isLoginRoute: boolean;
 }
 
-export const WalletBrowserComponent: React.SFC<IWalletBrowserProps> = ({
+export const WalletBrowserComponent: React.SFC<IWalletBrowserProps & IIntlProps> = ({
   errorMessage,
   isLoading,
   isLoginRoute,
+  intl: { formatIntlMessage },
 }) => (
   <div>
     <h1 className="text-center mb-3">
-      {isLoginRoute ? "Login with existing wallet" : "Register your existing wallet"}
+      {isLoginRoute ? (
+        <FormattedMessage id="wallet-selector.browser.login-prompt" />
+      ) : (
+        <FormattedMessage id="wallet-selector.browser.register-prompt" />
+      )}
     </h1>
 
     {isLoading ? (
@@ -48,16 +55,18 @@ export const WalletBrowserComponent: React.SFC<IWalletBrowserProps> = ({
         </Row>
 
         <Row className="mb-4 text-center">
-          <StepCard img={walletIcon} text="1. Choose existing wallet" />
-          <StepCard img={browserIcon} text="2. Turn on your browser plugin" />
-          <StepCard img={lockIcon} text="3. Unlock your wallet to register" />
+          <StepCard img={walletIcon} text={formatIntlMessage("wallet-selector.browser.steps.1")} />
+          <StepCard img={browserIcon} text={formatIntlMessage("wallet-selector.browser.steps.2")} />
+          <StepCard img={lockIcon} text={formatIntlMessage("wallet-selector.browser.steps.3")} />
         </Row>
 
         <HorizontalLine className="mb-4" />
 
         <Row className="text-center mb-4">
           <Col>
-            <span className="font-weight-bold">NEUFUND supports</span>
+            <span className="font-weight-bold">
+              <FormattedMessage id="wallet-selector.browser.supports" />
+            </span>
           </Col>
         </Row>
         <Row className={cn("justify-content-center text-center", styles.walletLogos)}>
@@ -92,4 +101,5 @@ export const WalletBrowser = compose<React.SFC>(
     actionCreator: dispatch => dispatch(walletFlows.tryConnectingWithBrowserWallet),
     interval: BROWSER_WALLET_RECONNECT_INTERVAL,
   }),
+  injectIntlHelpers,
 )(WalletBrowserComponent);

bae67c09699899537a3417ddc4858417c767ab55
Patryk Lizoń <patryk@hangover.studio>
2018-04-27 15:56:43 +0200

    Remove redundant prop

--- app/components/kyc/start/Start.tsx
+++ app/components/kyc/start/Start.tsx
@@ -21,7 +21,6 @@ export const KYCStartComponent = injectIntlHelpers<IProps>(
       steps={4}
       currentStep={1}
       title={formatIntlMessage("kyc.start.title")}
-      backLink={""}
       isMaxWidth={true}
     >
       <Panels

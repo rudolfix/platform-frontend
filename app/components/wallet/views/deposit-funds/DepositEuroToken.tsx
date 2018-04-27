cce8d196300e01131c9c17183b9142a79546e9b0
Krzysztof Kaczor <chris@kaczor.io>
2018-04-27 15:13:24 +0200

    extract translations from DepositEuroToken module

--- app/components/wallet/views/deposit-funds/DepositEuroToken.tsx
+++ app/components/wallet/views/deposit-funds/DepositEuroToken.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as styles from "./DepositEuroToken.module.scss";
 import { DepositFunds } from "./DepositFunds";
 
+import { FormattedMessage } from "react-intl";
 import * as icon from "../../../../assets/img/nEUR_icon.svg";
 
 interface IProps {
@@ -13,26 +14,39 @@ export const DepositEuroToken: React.SFC<IProps> = ({ path }) => {
     <DepositFunds path={path}>
       <div className={styles.methodEuroToken}>
         <h2 className={styles.title}>
-          Deposit <img src={icon} className={styles.icon} alt="nEUR icon" /> nEUR to your wallet
+          <FormattedMessage
+            id="wallet.deposit-funds.eur.header"
+            values={{ icon: <img src={icon} className={styles.icon} alt="nEUR icon" /> }}
+          />
         </h2>
         <p className={styles.description}>
-          Transfer the amount you wish to deposit to the bank account below, using the exact
-          reference.<br />You will receive an e-mail notification once your funds have been
-          successfully deposited!
+          <FormattedMessage id="wallet.deposit-funds.eur.description" />
+          <br />
+          <FormattedMessage id="wallet.deposit-funds.eur.email-notification" />
         </p>
         <div className={styles.background}>
           <div className={styles.details}>
-            <div className={styles.label}>Beneficiary</div>
+            <div className={styles.label}>
+              <FormattedMessage id="wallet.deposit-funds.eur.beneficiary" />
+            </div>
             <div className={styles.value}>-</div>
-            <div className={styles.label}>IBAN</div>
+            <div className={styles.label}>
+              <FormattedMessage id="wallet.deposit-funds.eur.iban" />
+            </div>
             <div className={styles.value}>-</div>
-            <div className={styles.label}>BIC</div>
+            <div className={styles.label}>
+              <FormattedMessage id="wallet.deposit-funds.eur.bic" />
+            </div>
             <div className={styles.value}>-</div>
-            <div className={styles.label}>Reference</div>
+            <div className={styles.label}>
+              <FormattedMessage id="wallet.deposit-funds.eur.reference" />
+            </div>
             <div className={styles.value}>-</div>
           </div>
           <div className={styles.currencyDetails}>
-            <div className={styles.label}>nEUR VALUE</div>
+            <div className={styles.label}>
+              <FormattedMessage id="wallet.deposit-funds.eur.eur-value" />
+            </div>
             <div className={styles.exchangeRate}>1nEUR = 1€</div>
           </div>
         </div>

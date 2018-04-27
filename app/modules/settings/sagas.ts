9fd5ab4bf24fcb9072b6dfc791093bb628ad02ea
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-04-27 11:54:05 +0200

    loadsUser when backend returns an exception

--- app/modules/settings/sagas.ts
+++ app/modules/settings/sagas.ts
@@ -5,7 +5,7 @@ import { TGlobalDependencies } from "../../di/setupBindings";
 import { IAppState } from "../../store";
 import { accessWalletAndRunEffect } from "../accessWallet/sagas";
 import { TAction } from "../actions";
-import { ensurePermissionsArePresent, updateUser } from "../auth/sagas";
+import { ensurePermissionsArePresent, loadUser, updateUser } from '../auth/sagas';
 import { selectUser } from "../auth/selectors";
 import { neuCall, neuTakeEvery } from "../sagas";
 import { actions } from "./../actions";
@@ -30,7 +30,8 @@ export function* addNewEmail(
     yield effects.call(updateUser, { ...user, new_email: email });
     notificationCenter.info("New Email added");
   } catch {
-    notificationCenter.error("Failed to change email");
+    yield effects.call(loadUser);
+    notificationCenter.error("Failed to send email");
   }
 }
 

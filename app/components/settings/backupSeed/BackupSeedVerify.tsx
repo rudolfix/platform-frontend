799404208124d807849aad80f8511676849c65fa
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-05-02 22:30:56 +0200

    fixes tests

--- app/components/settings/backupSeed/BackupSeedVerify.tsx
+++ app/components/settings/backupSeed/BackupSeedVerify.tsx
@@ -46,138 +46,141 @@ const getRandomNumbers = (numbers: number, range: number): number[] => {
   return arr.sort((a, b) => a - b);
 };
 
-export const BackupSeedVerify = injectIntlHelpers(
-  class extends React.Component<IBackupSeedVerifyProps & IIntlProps, IBackupSeedVerifyState> {
-    displayName = "BackupSeedVerify";
+export const BackupSeedVerifyComponent = class extends React.Component<
+  IBackupSeedVerifyProps & IIntlProps,
+  IBackupSeedVerifyState
+> {
+  displayName = "BackupSeedVerify";
 
-    constructor(props: IBackupSeedVerifyProps & IIntlProps) {
-      super(props);
-      const wordsToCheck = getRandomNumbers(WORDS_TO_VERIFY, props.words.length);
+  constructor(props: IBackupSeedVerifyProps & IIntlProps) {
+    super(props);
+    const wordsToCheck = getRandomNumbers(WORDS_TO_VERIFY, props.words.length);
 
-      this.state = {
-        verificationWords: wordsToCheck.map(number => ({ number: number, word: "" })),
-      };
+    this.state = {
+      verificationWords: wordsToCheck.map(number => ({ number: number, word: "" })),
+    };
+  }
+  private formatIntlMessage = this.props.intl.formatIntlMessage;
+  updateValueFactory = (wordOnPageNumber: number) => (newValue: any): void => {
+    const verificationWords = this.state.verificationWords;
+    verificationWords[wordOnPageNumber].word = newValue;
+    this.setState({
+      verificationWords,
+    });
+  };
+
+  validateWord = (wordOnPageNumber: number): boolean | undefined => {
+    const wordNumber = this.state.verificationWords[wordOnPageNumber].number;
+    const word = this.state.verificationWords[wordOnPageNumber].word;
+    if (word) {
+      return word === this.props.words[wordNumber];
+    } else {
+      return undefined;
     }
-    private formatIntlMessage = this.props.intl.formatIntlMessage;
-    updateValueFactory = (wordOnPageNumber: number) => (newValue: any): void => {
-      const verificationWords = this.state.verificationWords;
-      verificationWords[wordOnPageNumber].word = newValue;
-      this.setState({
-        verificationWords,
-      });
-    };
+  };
 
-    validateWord = (wordOnPageNumber: number): boolean | undefined => {
-      const wordNumber = this.state.verificationWords[wordOnPageNumber].number;
-      const word = this.state.verificationWords[wordOnPageNumber].word;
-      if (word) {
-        return word === this.props.words[wordNumber];
-      } else {
-        return undefined;
-      }
-    };
+  getValidationStyle = (wordOnPageNumber: number): string => {
+    const validationResult = this.validateWord(wordOnPageNumber);
+    if (validationResult !== undefined) {
+      return validationResult ? styles.valid : styles.invalid;
+    } else {
+      return "";
+    }
+  };
 
-    getValidationStyle = (wordOnPageNumber: number): string => {
-      const validationResult = this.validateWord(wordOnPageNumber);
-      if (validationResult !== undefined) {
-        return validationResult ? styles.valid : styles.invalid;
-      } else {
-        return "";
-      }
-    };
+  showInvalidMsg = (): boolean => {
+    let show = false;
+    for (let i = 0; i < WORDS_TO_VERIFY; i++) {
+      show = show || this.validateWord(i) === false;
+    }
+    return show;
+  };
 
-    showInvalidMsg = (): boolean => {
-      let show = false;
-      for (let i = 0; i < WORDS_TO_VERIFY; i++) {
-        show = show || this.validateWord(i) === false;
-      }
-      return show;
-    };
+  allWordsValid = (): boolean => {
+    let valid = true;
+    for (let i = 0; i < WORDS_TO_VERIFY; i++) {
+      valid = valid && this.validateWord(i) === true;
+    }
+    return valid;
+  };
 
-    allWordsValid = (): boolean => {
-      let valid = true;
-      for (let i = 0; i < WORDS_TO_VERIFY; i++) {
-        valid = valid && this.validateWord(i) === true;
-      }
-      return valid;
-    };
+  generateSelect = (wordOnPageNumber: number): React.ReactNode => (
+    <Select
+      options={wordsOptions}
+      simpleValue
+      matchPos={this.formatIntlMessage("settings.backup-seed-verify.start")}
+      matchProp={this.formatIntlMessage("settings.backup-seed-verify.value")}
+      value={this.state.verificationWords[wordOnPageNumber].word}
+      onChange={this.updateValueFactory(wordOnPageNumber)}
+      placeholder={this.formatIntlMessage("settings.backup-seed-verify.enter-word")}
+      noResultsText={this.formatIntlMessage("settings.backup-seed-verify.no-matching-words")}
+      className={this.getValidationStyle(wordOnPageNumber)}
+    />
+  );
 
-    generateSelect = (wordOnPageNumber: number): React.ReactNode => (
-      <Select
-        options={wordsOptions}
-        simpleValue
-        matchPos={this.formatIntlMessage("settings.backup-seed-verify.start")}
-        matchProp={this.formatIntlMessage("settings.backup-seed-verify.value")}
-        value={this.state.verificationWords[wordOnPageNumber].word}
-        onChange={this.updateValueFactory(wordOnPageNumber)}
-        placeholder={this.formatIntlMessage("settings.backup-seed-verify.enter-word")}
-        noResultsText={this.formatIntlMessage("settings.backup-seed-verify.no-matching-words")}
-        className={this.getValidationStyle(wordOnPageNumber)}
-      />
-    );
-
-    render(): React.ReactNode {
-      return (
-        <>
-          <Row>
-            <Col xs={{ size: 10, offset: 1 }}>
-              <Row>
-                {range(0, WORDS_TO_VERIFY).map((num, wordPageNumber) => {
-                  const wordNumber = this.state.verificationWords[wordPageNumber].number;
-                  return (
-                    <Col
-                      xs={{ size: 6, offset: 3 }}
-                      sm={{ size: 3, offset: 0 }}
-                      key={num}
-                      className="my-4"
-                    >
-                      <div data-test-id={`seed-verify-label`}>{`word ${wordNumber + 1}`}</div>
-                      {this.generateSelect(num)}
-                    </Col>
-                  );
-                })}
-              </Row>
-            </Col>
+  render(): React.ReactNode {
+    return (
+      <>
+        <Row>
+          <Col xs={{ size: 10, offset: 1 }}>
+            <Row>
+              {range(0, WORDS_TO_VERIFY).map((num, wordPageNumber) => {
+                const wordNumber = this.state.verificationWords[wordPageNumber].number;
+                return (
+                  <Col
+                    xs={{ size: 6, offset: 3 }}
+                    sm={{ size: 3, offset: 0 }}
+                    key={num}
+                    className="my-4"
+                  >
+                    <div data-test-id={`seed-verify-label`}>{`word ${wordNumber + 1}`}</div>
+                    {this.generateSelect(num)}
+                  </Col>
+                );
+              })}
+            </Row>
+          </Col>
+        </Row>
+        {this.showInvalidMsg() && (
+          <Row className="my-4 justify-content-center">
+            <WarningAlert
+              data-test-id="seed-verify-invalid-msg"
+              className={styles.placeholderHeight}
+            >
+              <FormattedMessage id="settings.backup-seed-verify.recheck-words-message" />
+            </WarningAlert>
           </Row>
-          {this.showInvalidMsg() && (
-            <Row className="my-4 justify-content-center">
-              <WarningAlert
-                data-test-id="seed-verify-invalid-msg"
-                className={styles.placeholderHeight}
-              >
-                <FormattedMessage id="settings.backup-seed-verify.recheck-words-message" />
-              </WarningAlert>
-            </Row>
-          )}
-          {this.allWordsValid() && (
-            <Row className="my-4 text-center">
-              <Col className={styles.placeholderHeight}>
-                <Button data-test-id="seed-verify-button-next" onClick={this.props.onNext}>
-                  <FormattedMessage id="form.button.continue" />
-                </Button>
-              </Col>
-            </Row>
-          )}
-          {!this.showInvalidMsg() &&
-            !this.allWordsValid() && (
-              <Row className="my-4 text-center">
-                <Col className={styles.placeholderHeight} />
-              </Row>
-            )}
-          <Row>
-            <Col>
-              <Button
-                layout="secondary"
-                iconPosition="icon-before"
-                svgIcon={arrowLeft}
-                onClick={this.props.onBack}
-              >
-                <FormattedMessage id="form.button.back" />
+        )}
+        {this.allWordsValid() && (
+          <Row className="my-4 text-center">
+            <Col className={styles.placeholderHeight}>
+              <Button data-test-id="seed-verify-button-next" onClick={this.props.onNext}>
+                <FormattedMessage id="form.button.continue" />
               </Button>
             </Col>
           </Row>
-        </>
-      );
-    }
-  },
-);
+        )}
+        {!this.showInvalidMsg() &&
+          !this.allWordsValid() && (
+            <Row className="my-4 text-center">
+              <Col className={styles.placeholderHeight} />
+            </Row>
+          )}
+        <Row>
+          <Col>
+            <Button
+              layout="secondary"
+              iconPosition="icon-before"
+              svgIcon={arrowLeft}
+              onClick={this.props.onBack}
+            >
+              <FormattedMessage id="form.button.back" />
+            </Button>
+          </Col>
+        </Row>
+      </>
+    );
+  }
+};
+
+export const BackupSeedVerify = injectIntlHelpers(BackupSeedVerifyComponent);

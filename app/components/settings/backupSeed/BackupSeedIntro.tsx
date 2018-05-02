130b8610c690f16521b3de4ae0ec74fd36db249c
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-05-02 16:46:23 +0200

    adds translation to BackupSeedIntro

--- app/components/settings/backupSeed/BackupSeedIntro.tsx
+++ app/components/settings/backupSeed/BackupSeedIntro.tsx
@@ -8,36 +8,48 @@ import * as noComputer from "../../../assets/img/seed_backup/no_computer.svg";
 import * as safe from "../../../assets/img/seed_backup/safe.svg";
 import * as write from "../../../assets/img/seed_backup/write.svg";
 
+import { FormattedMessage } from "react-intl";
 import * as arrowLeft from "../../../assets/img/inline_icons/arrow_left.svg";
+import { injectIntlHelpers } from "../../../utils/injectIntlHelpers";
 
 interface IBackupSeedIntroProps {
   onNext: () => void;
   onBack: string;
 }
 
-export const BackupSeedIntro: React.SFC<IBackupSeedIntroProps> = ({ onBack, onNext }) => (
-  <>
-    <Row className="mb-4 text-center">
-      <StepCard img={write} text={"Write all words on a piece of paper"} />
-      <StepCard img={noComputer} text={"Do not store the words on your computer"} />
-      <StepCard img={safe} text={"Store the phrase very safely"} />
-    </Row>
-    <Row className="my-5">
-      <Col className="text-center">
-        <Button onClick={onNext}>I have read instructions</Button>
-      </Col>
-    </Row>
-    <Row>
-      <Col className="col-auto">
-        <Button
-          layout="secondary"
-          iconPosition="icon-before"
-          svgIcon={arrowLeft}
-          onClick={() => onBack}
-        >
-          Back
-        </Button>
-      </Col>
-    </Row>
-  </>
+export const BackupSeedIntro = injectIntlHelpers<IBackupSeedIntroProps>(
+  ({ intl: { formatIntlMessage }, onBack, onNext }) => (
+    <>
+      <Row className="mb-4 text-center">
+        <StepCard
+          img={write}
+          text={formatIntlMessage("settings.backup-seed-intro.write-all-words")}
+        />
+        <StepCard
+          img={noComputer}
+          text={formatIntlMessage("settings.backup-seed-intro.words-warning")}
+        />
+        <StepCard img={safe} text={formatIntlMessage("settings.backup-seed-intro.store-safely")} />
+      </Row>
+      <Row className="my-5">
+        <Col className="text-center">
+          <Button onClick={onNext}>
+            <FormattedMessage id="settings.backup-seed-intro.read-instructions" />
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="col-auto">
+          <Button
+            layout="secondary"
+            iconPosition="icon-before"
+            svgIcon={arrowLeft}
+            onClick={() => onBack}
+          >
+            <FormattedMessage id="form.button.back" />
+          </Button>
+        </Col>
+      </Row>
+    </>
+  ),
 );

7adeeb5866101fe7ddaa8dd1e0a7edb3f2db97eb
Krzysztof Kaczor <chris@kaczor.io>
2018-05-02 17:29:27 +0200

    add more light wallet related translations

--- app/components/walletSelector/light/RegisterLightWallet.tsx
+++ app/components/walletSelector/light/RegisterLightWallet.tsx
@@ -1,13 +1,14 @@
 import { Form, FormikProps, withFormik } from "formik";
 import * as React from "react";
+import { FormattedMessage } from "react-intl";
 import { Col, Row } from "reactstrap";
 import { compose } from "redux";
+import * as Yup from "yup";
 
 import { FormField } from "../../../components/shared/forms/forms";
 import { appConnect } from "../../../store";
 import { Button } from "../../shared/Buttons";
 
-import * as Yup from "yup";
 import { flows } from "../../../modules/flows";
 
 const EMAIL = "email";
@@ -44,7 +45,7 @@ const RegisterLightWalletForm = (formikBag: FormikProps<IFormValues>) => (
     <FormField type="password" placeholder="Repeat Password" name={REPEAT_PASSWORD} />
     <div className="text-center">
       <Button type="submit" disabled={!formikBag.isValid}>
-        REGISTER
+        <FormattedMessage id="wallet-selector.neuwallet.register" />
       </Button>
     </div>
   </Form>
@@ -60,7 +61,9 @@ export const RegisterWalletComponent: React.SFC<IProps> = props => {
   return (
     <Row className="justify-content-sm-center mt-3">
       <Col className="align-self-end col-sm-auto col-xs-12">
-        <h1 className="mb-4">Create your Neufund wallet</h1>
+        <h1 className="mb-4">
+          <FormattedMessage id="wallet-selector.neuwallet.register-prompt" />
+        </h1>
         <RegisterEnhancedLightWalletForm {...props} />
       </Col>
     </Row>

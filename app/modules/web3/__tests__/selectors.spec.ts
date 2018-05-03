f6633c8732689b4b808797a8091b8acd92a837e7
Patryk Lizoń <patryk@hangover.studio>
2018-05-03 16:10:02 +0200

    Add tests for selectors

--- app/modules/web3/__tests__/selectors.spec.ts
+++ app/modules/web3/__tests__/selectors.spec.ts
@@ -1,11 +1,23 @@
 import { expect } from "chai";
+import { RouterState } from "react-router-redux";
 import { dummyEthereumAddress } from "../../../../test/fixtures";
 import { IWeb3State } from "../reducer";
-import { selectIsLightWallet } from "../selectors";
+import {
+  selectActivationCodeFromQueryString,
+  selectIsLightWallet,
+  selectLightWalletEmailFromQueryString,
+  selectLightWalletFromQueryString,
+} from "../selectors";
 import { WalletType } from "../types";
 import { getDummyLightWalletMetadata } from "./fixtures";
 
 describe("web3 > selectors", () => {
+  const salt = "salt";
+  const code = "code";
+  const vault = "vault";
+  const email = "test@example.com";
+  const encodedEmail = email.replace("@", "%2540");
+
   describe("selectIsLightWallet", () => {
     it("should work with connected wallet", () => {
       const state: IWeb3State = {
@@ -25,15 +37,71 @@ describe("web3 > selectors", () => {
         previousConnectedWallet: {
           walletType: WalletType.LIGHT,
           address: dummyEthereumAddress,
-          vault: "vault",
+          vault,
           email: "test@example.com",
-          salt: "salt",
+          salt,
         },
       };
 
       const isLightWallet = selectIsLightWallet(state);
 
       expect(isLightWallet).to.be.true;
+    });
+  });
+
+  describe("selectLightWalletEmailFromQueryString", () => {
+    it("should work with activation link", () => {
+      const state: RouterState = {
+        location: {
+          pathname: "/login/light",
+          state: undefined,
+          hash: "",
+          search: `?redirect=%2F%26email%3D${encodedEmail}`,
+        },
+      };
+
+      expect(selectLightWalletEmailFromQueryString(state) === email).to.be.true;
+    });
+  });
+
+  describe("selectActivationCodeFromQueryString", () => {
+    it("should work with activation link", () => {
+      const state: RouterState = {
+        location: {
+          pathname: "/login/light",
+          state: undefined,
+          hash: "",
+          search: `?redirect=%2F%26code%3D${code}`,
+        },
+      };
+
+      const result: any = { ...selectActivationCodeFromQueryString(state) };
+
+      expect(result.verificationCode === code).to.be.true;
+    });
+  });
+
+  describe("selectLightWalletFromQueryString", () => {
+    it("should work with activation link", () => {
+      const state: any = {
+        location: {
+          pathname: "/login/light",
+          state: undefined,
+          hash: "",
+          search: `?redirect=%2F%26email%3D${encodedEmail}%26salt%3D${salt}`,
+        },
+        previousConnectedWallet: {
+          walletType: WalletType.LIGHT,
+          address: dummyEthereumAddress,
+          vault,
+          email: "test@example.com",
+          salt,
+        },
+      };
+
+      const result: any = { ...selectLightWalletFromQueryString(state) };
+
+      expect(result.email === email && result.salt === salt).to.be.true;
     });
   });
 });

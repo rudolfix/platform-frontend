1bf8c594ecceaabf81eca815b8dfc2ab1327e944
Krzysztof Kaczor <chris@kaczor.io>
2018-05-03 18:51:26 +0200

    run e2e tests during pipeline

--- e2e/setup-e2e.sh
+++ e2e/setup-e2e.sh
@@ -3,16 +3,14 @@ set -e
 cd "$(dirname "$0")"
 
 echo "Cloning backend repo..."
-# git clone git@github.com:Neufund/platform-backend.git 2> /dev/null
-
-cd ./platform-backend
-git pull
+echo "$BACKEND_DEPLOYMENT_KEY" > "key"
+GIT_SSH_COMMAND='ssh -i ./key' git clone git@github.com:Neufund/platform-backend.git 2> /dev/null
 
 echo "Running backend"
-# make run 2> /dev/null
+make run 2> /dev/null
 
 echo "Building production build of frontend"
 yarn build:prod
 
 echo "Starting up local http server"
-../../scripts/prod-like-srv.sh
+../../scripts/prod-like-srv.sh&

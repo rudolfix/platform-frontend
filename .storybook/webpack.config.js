71dcbf5b37b63a9538cac5cbb003ce911cfa9c24
Patryk Lizoń <patryk@hangover.studio>
2018-05-07 17:10:26 +0200

    Improve storybook setup

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,50 +1,15 @@
 const path = require("path");
 const paths = require("../webpack/paths");
+const devConfig = require("../webpack/webpack.config.dev");
 
 module.exports = (baseConfig, env, config) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve("awesome-typescript-loader"),
-  });
+  const pathToStyleLoader = path.join(__dirname, "./setup-styles.ts");
 
-  config.module.rules.push({
-    test: /\.module.scss$/,
-    use: [
-      {
-        loader: require.resolve("style-loader"),
-      },
-      {
-        loader: require.resolve("css-loader"),
-        options: {
-          importLoaders: 1,
-          modules: true,
-          localIdentName: "[name]__[local]___[hash:base64:5]",
-          camelCase: "dashesOnly",
-        },
-      },
-      {
-        loader: require.resolve("sass-loader"),
-      },
-    ],
-  });
+  config.entry.preview.push(pathToStyleLoader);
 
-  config.module.rules.push({
-    test: /\.(jpg|png|svg)$/,
-    loader: "url-loader",
-    exclude: paths.inlineIcons,
-    options: {
-      limit: 25000,
-      publicPath: "/",
-    },
-  });
+  config.module.rules = [...devConfig.module.rules];
 
-  config.module.rules.push({
-    test: /\.(svg)$/,
-    loader: "raw-loader",
-    include: paths.inlineIcons,
-  });
-
-  config.resolve.extensions.push(".ts", ".tsx", ".scss");
+  config.resolve.extensions = devConfig.resolve.extensions;
 
   return config;
 };

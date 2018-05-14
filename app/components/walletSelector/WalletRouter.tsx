92383d54e71b08032aa76e9d2d55f8f5c716f12f
Patryk Lizoń <patryk@hangover.studio>
2018-05-14 15:56:48 +0200

    Change redirection

--- app/components/walletSelector/WalletRouter.tsx
+++ app/components/walletSelector/WalletRouter.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Redirect, Route } from "react-router-dom";
 
+import { appRoutes } from "../appRoutes";
+
 import { SwitchConnected } from "../shared/connectedRouting";
 import { WalletBrowser } from "./browser/WalletBrowser";
 import { WalletLedger } from "./ledger/WalletLedger";
@@ -15,6 +17,11 @@ export const WalletRouter: React.SFC<IProps> = ({ rootPath }) => (
     <Route path={`${rootPath}/light`} component={WalletLight} />
     <Route path={`${rootPath}/browser`} component={WalletBrowser} exact />
     <Route path={`${rootPath}/ledger`} component={WalletLedger} exact />
+    <Route
+      path={`${appRoutes.loginEto}`}
+      render={() => <Redirect to={`${rootPath}/ledger`} />}
+      exact
+    />
     <Redirect to={`${rootPath}/light`} />
   </SwitchConnected>
 );

b201ee6abe84d7731ce05bed685e56e7abd7305d
Krzysztof Kaczor <chris@kaczor.io>
2018-05-15 09:59:01 +0200

    fix bug in header navigation

--- app/components/walletSelector/WalletRouter.tsx
+++ app/components/walletSelector/WalletRouter.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Redirect, Route } from "react-router-dom";
 
 import { SwitchConnected } from "../shared/connectedRouting";
-
 import { WalletBrowser } from "./browser/WalletBrowser";
 import { WalletLedger } from "./ledger/WalletLedger";
 import { WalletLight } from "./light/WalletLight";

545ea955fad3175053ec5ae105d991d3f234ee38
Patryk Lizoń <patryk@hangover.studio>
2018-05-24 16:02:28 +0200

    Improve code

--- app/modules/web3/__tests__/fixtures.ts
+++ app/modules/web3/__tests__/fixtures.ts
@@ -1,11 +1,37 @@
 import { dummyEthereumAddress } from "../../../../test/fixtures";
-import { ILightWalletMetadata } from "../../../lib/persistence/WalletMetadataObjectStorage";
+import {
+  ICommonWalletMetadata,
+  ILedgerWalletMetadata,
+  ILightWalletMetadata,
+} from "../../../lib/persistence/WalletMetadataObjectStorage";
 import { WalletType } from "../types";
 
-export const getDummyLightWalletMetadata = (): ILightWalletMetadata => ({
-  walletType: WalletType.LIGHT,
+const walletsCommon = {
   address: dummyEthereumAddress,
   vault: "vault",
   email: "test@example.com",
   salt: "salt",
+};
+
+interface IBrowserWalletMetadata extends ICommonWalletMetadata {
+  walletType: WalletType.BROWSER;
+  vault: string;
+  salt: string;
+  email: string;
+}
+
+export const getDummyLightWalletMetadata = (): ILightWalletMetadata => ({
+  walletType: WalletType.LIGHT,
+  ...walletsCommon,
+});
+
+export const getDummyBrowserWalletMetadata = (): IBrowserWalletMetadata => ({
+  walletType: WalletType.BROWSER,
+  ...walletsCommon,
+});
+
+export const getDummyLedgerWalletMetadata = (): ILedgerWalletMetadata => ({
+  walletType: WalletType.LEDGER,
+  derivationPath: "derivationPath",
+  address: dummyEthereumAddress,
 });
--- app/modules/web3/__tests__/flows.spec.ts
+++ app/modules/web3/__tests__/flows.spec.ts
@@ -1,25 +1,25 @@
 import { expect } from "chai";
 import { spy } from "sinon";
 
-import { dummyEthereumAddress } from "../../../../test/fixtures";
 import { createMock } from "../../../../test/testUtils";
 import { NotificationCenter } from "../../../lib/dependencies/NotificationCenter";
+import { IAppState } from "../../../store";
 import { web3Flows } from "../flows";
-import { WalletType } from "../types";
 import { dummyIntl } from "./../../../utils/injectIntlHelpers.fixtures";
-import { getDummyLightWalletMetadata } from "./fixtures";
+import {
+  getDummyBrowserWalletMetadata,
+  getDummyLedgerWalletMetadata,
+  getDummyLightWalletMetadata,
+} from "./fixtures";
 
 describe("web3 > flows", () => {
   describe("personalWalletDisconnected", () => {
     it("should send notification if it's ledger wallet", () => {
       const dummyDispatch = spy();
-      const state: Partial<any> = {
+      const state: Partial<IAppState> = {
         web3: {
           connected: false,
-          previousConnectedWallet: {
-            walletType: WalletType.LEDGER,
-            address: dummyEthereumAddress,
-          },
+          previousConnectedWallet: getDummyLedgerWalletMetadata(),
         },
       };
 
@@ -39,13 +39,10 @@ describe("web3 > flows", () => {
 
     it("should send notification if it's browser wallet", () => {
       const dummyDispatch = spy();
-      const state: Partial<any> = {
+      const state: Partial<IAppState> = {
         web3: {
           connected: false,
-          previousConnectedWallet: {
-            walletType: WalletType.LEDGER,
-            address: dummyEthereumAddress,
-          },
+          previousConnectedWallet: getDummyBrowserWalletMetadata(),
         },
       };
 
@@ -65,11 +62,10 @@ describe("web3 > flows", () => {
 
     it("should not send notification if it's light wallet", () => {
       const dummyDispatch = spy();
-      const state: Partial<any> = {
+      const state: Partial<IAppState> = {
         web3: {
           connected: false,
           previousConnectedWallet: getDummyLightWalletMetadata(),
-          walletType: WalletType.LIGHT,
         },
       };
       const dummyNotificationCenter = createMock(NotificationCenter, {

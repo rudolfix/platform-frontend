3556e58abb0e21ffc29adb4d4bef976a95a1ada6
Krzysztof Kaczor <chris@kaczor.io>
2018-05-24 16:49:01 +0200

    simplify fixtures

--- app/modules/web3/__tests__/fixtures.ts
+++ app/modules/web3/__tests__/fixtures.ts
@@ -1,33 +1,22 @@
 import { dummyEthereumAddress } from "../../../../test/fixtures";
 import {
-  ICommonWalletMetadata,
+  IBrowserWalletMetadata,
   ILedgerWalletMetadata,
   ILightWalletMetadata,
 } from "../../../lib/persistence/WalletMetadataObjectStorage";
 import { WalletType } from "../types";
 
-const walletsCommon = {
+export const getDummyLightWalletMetadata = (): ILightWalletMetadata => ({
+  walletType: WalletType.LIGHT,
   address: dummyEthereumAddress,
   vault: "vault",
   email: "test@example.com",
   salt: "salt",
-};
-
-interface IBrowserWalletMetadata extends ICommonWalletMetadata {
-  walletType: WalletType.BROWSER;
-  vault: string;
-  salt: string;
-  email: string;
-}
-
-export const getDummyLightWalletMetadata = (): ILightWalletMetadata => ({
-  walletType: WalletType.LIGHT,
-  ...walletsCommon,
 });
 
 export const getDummyBrowserWalletMetadata = (): IBrowserWalletMetadata => ({
   walletType: WalletType.BROWSER,
-  ...walletsCommon,
+  address: dummyEthereumAddress,
 });
 
 export const getDummyLedgerWalletMetadata = (): ILedgerWalletMetadata => ({

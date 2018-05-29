d7765432838c7b7458bece66c7fd4bfe63c7e135
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-05-29 17:31:12 +0200

    adds e2e tests

---
+++ app/components/walletSelector/light/__tests__/LightWalletETOLogin.spec.e2e.ts
@@ -0,0 +1,13 @@
+import { tid } from "../../../../../test/testUtils";
+import { registerWithLightWalletETO } from "./LightWalletETORegister.spec.e2e";
+const testEmail = "moe@test.com";
+describe("Light Wallet Login", () => {
+  it("should load user meta only from issuer path", () => {
+    registerWithLightWalletETO(testEmail, "strongpassword");
+    cy.get(tid("Header-logout")).click();
+    cy.wait(10000);
+    cy.get(tid("Header-login-eto")).click();
+    cy.get(tid("wallet-selector-light")).click();
+    cy.contains(tid("wallet-selector-nuewallet.login-email"), testEmail);
+  });
+});
--- app/components/walletSelector/light/__tests__/LightWalletETORegister.spec.e2e.ts
+++ app/components/walletSelector/light/__tests__/LightWalletETORegister.spec.e2e.ts
@@ -1,8 +1,8 @@
 import { tid } from "../../../../../test/testUtils";
 
-export const registerWithLightWallet = (email: string, password: string) => {
+export const registerWithLightWalletETO = (email: string, password: string) => {
   cy.visit("eto/register/light");
-
+  
   cy.get(tid("wallet-selector-register-email")).type(email);
   cy.get(tid("wallet-selector-register-password")).type(password);
   cy.get(tid("wallet-selector-register-confirm-password")).type(password);
@@ -13,6 +13,6 @@ export const registerWithLightWallet = (email: string, password: string) => {
 
 describe("Wallet backup recovery phrase", () => {
   it("should register user with light-wallet", () => {
-    registerWithLightWallet("moe@test.com", "strongpassword");
+    registerWithLightWalletETO("moe@test.com", "strongpassword");
   });
 });

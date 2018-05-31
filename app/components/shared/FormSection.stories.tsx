438c6d1bdd13ae2c680d89708d3c5a59b318868e
Patryk Lizoń <patryk@hangover.studio>
2018-05-31 12:14:32 +0200

    Add Form section component

---
+++ app/components/shared/FormSection.stories.tsx
@@ -0,0 +1,23 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { FormSection } from "./FormSection";
+
+storiesOf("FormSection", module).add("default", () => (
+  <FormSection title="Example section title">
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequatur incidunt aliquam
+      autem, quae, odit, nam animi facere itaque voluptates pariatur aut cum at illo similique
+      minima harum soluta dolores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+      consequatur incidunt aliquam autem, quae, odit, nam animi facere itaque voluptates pariatur
+      aut cum at illo similique minima harum soluta dolores! Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Rerum consequatur incidunt aliquam autem, quae, odit, nam animi facere
+      itaque voluptates pariatur aut cum at illo similique minima harum soluta dolores! Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Rerum consequatur incidunt aliquam autem, quae,
+      odit, nam animi facere itaque voluptates pariatur aut cum at illo similique minima harum
+      soluta dolores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequatur
+      incidunt aliquam autem, quae, odit, nam animi facere itaque voluptates pariatur aut cum at
+      illo similique minima harum soluta dolores!
+    </div>
+  </FormSection>
+));

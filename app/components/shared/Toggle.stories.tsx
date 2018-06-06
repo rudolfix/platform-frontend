635b91c7f8c60450c315f41b165a17dd2a7c7b76
Patryk Lizoń <patryk@hangover.studio>
2018-06-06 12:08:47 +0200

    Add story

---
+++ app/components/shared/Toggle.stories.tsx
@@ -0,0 +1,8 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { Toggle } from "./Toggle";
+
+storiesOf("Toggle", module).add("default", () => (
+  <Toggle enabledLabel="on" disabledLabel="off" checked={true} onClick={() => {}} />
+));

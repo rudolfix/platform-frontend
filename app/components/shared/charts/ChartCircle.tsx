2b1f80da3ef1e042f6777c363f2d658c928560bd
Patryk Lizoń <patryk@hangover.studio>
2018-06-12 09:46:27 +0200

    Add ChartCircle component

---
+++ app/components/shared/charts/ChartCircle.tsx
@@ -0,0 +1,40 @@
+import * as cn from "classnames";
+import * as React from "react";
+
+import * as styles from "./ChartCircle.module.scss";
+
+interface IProps {
+  progress: number;
+  name: string | React.ReactNode;
+}
+
+export class ChartCircle extends React.Component<IProps> {
+  render(): React.ReactNode {
+    const { progress, name } = this.props;
+    const viewBoxSize = 210;
+    const radius = viewBoxSize / 2;
+    const indicatorRadius = radius * 0.8;
+    const indicatorCircumference = indicatorRadius * 2 * Math.PI;
+    const svgCenter = viewBoxSize / 2;
+
+    return (
+      <div className={cn(styles.chartCircle, progress === 1 && "is-completed")}>
+        <svg viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}>
+          <circle className={styles.border} r={svgCenter - 2} cx={svgCenter} cy={svgCenter} />
+          <circle className={styles.indicator} r={indicatorRadius} cx={svgCenter} cy={svgCenter} />
+          <path
+            className={styles.progress}
+            d={`M${svgCenter} ${radius - indicatorRadius}
+            a ${indicatorRadius} ${indicatorRadius} 0 0 1 0 ${indicatorRadius * 2}
+            a ${indicatorRadius} ${indicatorRadius} 0 0 1 0 ${indicatorRadius * -2}`}
+            strokeDasharray={`${progress * indicatorCircumference}, ${indicatorCircumference}`}
+          />
+        </svg>
+        <div className={styles.labels}>
+          <div className={styles.percents}>{`${progress * 100}%`}</div>
+          <div className={styles.name}>{name}</div>
+        </div>
+      </div>
+    );
+  }
+}

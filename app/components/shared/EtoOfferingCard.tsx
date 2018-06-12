83f0752fe1d76a75a521f4c0ee1eb57e6a937817
Patryk Lizoń <patryk@hangover.studio>
2018-06-12 15:03:49 +0200

    Improve styles

--- app/components/shared/EtoOfferingCard.tsx
+++ app/components/shared/EtoOfferingCard.tsx
@@ -55,13 +55,21 @@ export const EtoOfferingCard: React.SFC<IEtoOfferingProps> = props => {
   const Wrapper: React.SFC = ({ children }) => {
     if (props.to) {
       return (
-        <a href={props.to} target="_blank" className={cn(styles.card, props.className, props.teaser && styles.teaser)}>
+        <a
+          href={props.to}
+          target="_blank"
+          className={cn(styles.card, props.className, props.teaser && styles.teaser)}
+        >
           {children}
         </a>
       );
     }
 
-    return <div className={cn(styles.card, props.className, props.teaser && styles.teaser)}>{children}</div>;
+    return (
+      <div className={cn(styles.card, props.className, props.teaser && styles.teaser)}>
+        {children}
+      </div>
+    );
   };
 
   return (

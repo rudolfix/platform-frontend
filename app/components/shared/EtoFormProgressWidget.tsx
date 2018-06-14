e56d93cfab76665062a418f2b28dcb44ab428906
Patryk Lizoń <patryk@hangover.studio>
2018-06-15 01:09:00 +0200

    Hardcode component's size && remove light arrow icon

--- app/components/shared/EtoFormProgressWidget.tsx
+++ app/components/shared/EtoFormProgressWidget.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import { FormattedMessage } from "react-intl-phraseapp";
-
 import { Link } from "react-router-dom";
-import { ChartCircle, IChartCircleProps } from "./charts/ChartCircle";
-import { InlineIcon } from "./InlineIcon";
-import { Proportion } from "./Proportion";
 
-import * as arrowRightIcon from "../../assets/img/inline_icons/arrow_right_light.svg";
+import { Button } from "./Buttons";
+import { ChartCircle, IChartCircleProps } from "./charts/ChartCircle";
+
+import * as arrowRightIcon from "../../assets/img/inline_icons/arrow_right.svg";
 import * as styles from "./EtoFormProgressWidget.module.scss";
 
 interface IProps {
@@ -19,16 +18,22 @@ export const EtoFormProgressWidget: React.SFC<IProps & IChartCircleProps> = ({
   name,
 }) => {
   return (
-    <Proportion className={styles.etoFormProgressWidget}>
+    <div className={styles.etoFormProgressWidget}>
       <ChartCircle progress={progress} name={name} />
       <Link to={to} className={styles.linkWrapper}>
-        {progress < 1 ? (
-          <FormattedMessage id="shared-component.eto-form-progress-widget.complete" />
-        ) : (
-          <FormattedMessage id="shared-component.eto-form-progress-widget.edit" />
-        )}
-        <InlineIcon svgIcon={arrowRightIcon} width="12px" height="12px" />
+        <Button
+          theme="silver"
+          layout="secondary"
+          iconPosition="icon-after"
+          svgIcon={arrowRightIcon}
+        >
+          {progress < 1 ? (
+            <FormattedMessage id="shared-component.eto-form-progress-widget.complete" />
+          ) : (
+            <FormattedMessage id="shared-component.eto-form-progress-widget.edit" />
+          )}
+        </Button>
       </Link>
-    </Proportion>
+    </div>
   );
 };

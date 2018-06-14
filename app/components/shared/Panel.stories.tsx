cbf982891ab47475e9932fa749479166ae1faf3b
Patryk Lizoń <patryk@hangover.studio>
2018-06-14 10:24:10 +0200

    Unify Panels components

---
+++ app/components/shared/Panel.stories.tsx
@@ -0,0 +1,16 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { Panel } from "./Panel";
+
+import * as icon from "../../assets/img/notifications/warning.svg";
+
+const lorem =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, eveniet reiciendis minus aperiam numquam vero at placeat, officia, porro similique voluptatem dolores provident labore dolorem temporibus veniam sapiente nihil quibusdam.";
+
+
+storiesOf("Panel", module)
+  .add("default", () => <Panel>{lorem}</Panel>)
+  .add("with header text", () => <Panel headerText="sample header text">{lorem}</Panel>)
+  .add("with icon", () => <Panel icon={icon}>{lorem}</Panel>)
+  .add("with header text and icon", () => <Panel headerText="sample header text" icon={icon}>{lorem}</Panel>);
---
+++ app/components/shared/Panel.tsx
@@ -0,0 +1,44 @@
+import * as cn from "classnames";
+import * as React from "react";
+
+import { CommonHtmlProps } from "../../types";
+
+import * as styles from "./Panel.module.scss";
+
+export interface IPanelProps {
+  headerText?: string | React.ReactNode;
+  rightComponent?: React.ReactNode;
+  icon?: string;
+}
+
+export const Panel: React.SFC<IPanelProps & CommonHtmlProps> = ({
+  headerText,
+  rightComponent,
+  icon,
+  className,
+  children,
+  ...props
+}) => {
+  const hasHeader = !!(headerText || rightComponent || icon);
+
+  return (
+    <div {...props} className={cn(styles.panel, className)}>
+      {
+        hasHeader
+          && <header className={styles.header}>
+            {headerText && <div className={styles.left}>{headerText}</div>}
+            {
+              (rightComponent || icon)
+                && (
+                  <div className={styles.right}>
+                    {rightComponent}
+                    {icon && <img src={icon} className={styles.icon} />}
+                  </div>
+                )
+            }
+          </header>
+      }
+      {children}
+    </div>
+  )
+};

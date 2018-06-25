d80c60fef473c78805aa7a21b2e90b7e0954126b
Patryk Lizoń <patryk@hangover.studio>
2018-06-25 10:54:51 +0200

    Remove redundant code

--- app/components/shared/Proportion.tsx
+++ app/components/shared/Proportion.tsx
@@ -7,19 +7,12 @@ interface IProps {
   height?: number;
   width?: number;
   className?: string;
-  disabledOnMobile?: boolean;
 }
 
-export const Proportion: React.SFC<IProps> = ({
-  width = 1,
-  height = 1,
-  disabledOnMobile,
-  children,
-  className,
-}) => {
+export const Proportion: React.SFC<IProps> = ({ width = 1, height = 1, children, className }) => {
   return (
     <div
-      className={cn(styles.proportion, className, disabledOnMobile && styles.disabledOnMobile)}
+      className={cn(styles.proportion, className)}
       style={{ paddingTop: `${height / width * 100}%` }}
     >
       <div className={styles.content}>{children}</div>

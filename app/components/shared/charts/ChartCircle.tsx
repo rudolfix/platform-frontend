2778d9f2cdb50c206ed3237556adbd2e43deec30
Patryk Lizoń <patryk@hangover.studio>
2018-06-27 12:33:36 +0200

    Fix percent indicator

--- app/components/shared/charts/ChartCircle.tsx
+++ app/components/shared/charts/ChartCircle.tsx
@@ -29,7 +29,7 @@ export const ChartCircle: React.SFC<IChartCircleProps> = ({ progress, name }) =>
         />
       </svg>
       <div className={styles.labels}>
-        <div className={styles.percents}>{`${progress * 100}%`}</div>
+        <div className={styles.percents}>{`${Math.round(progress* 100)}%`}</div>
         <div className={styles.name}>{name}</div>
       </div>
     </div>

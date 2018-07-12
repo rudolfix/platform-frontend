fb8dcd246744beed7b4af3fed06ec0fc7107f614
trival <thomas.gorny@mail.com>
2018-07-12 19:13:38 +0200

    first simple alert component

---
+++ app/components/shared/Alerts.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Alert } from 'reactstrap';
+import * as styles from './Alerts.module.scss'
+
+export const InfoAlert: React.SFC = ({ children }) => {
+  return (
+    <Alert className={styles.alert}>{children}</Alert>
+  )
+}

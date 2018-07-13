8a205ac832ca83107a160c933d62c1fcd6a0c800
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-07-13 12:47:05 +0200

    refactors ETO dashboard refactors ETOProgressSection

--- app/components/eto/EtoDashboard.tsx
+++ app/components/eto/EtoDashboard.tsx
@@ -1,61 +1,178 @@
 import * as React from "react";
-import { Row } from "reactstrap";
+import { Col, Row } from "reactstrap";
 
+import { compose } from "redux";
+import {
+  EtoCompanyInformationType,
+  EtoState,
+  TGeneralEtoDataType,
+} from "../../lib/api/EtoApi.interfaces";
+import {
+  EtoKeyIndividualsType,
+  EtoLegalInformationType,
+  EtoMediaType,
+  EtoProductVisionType,
+  EtoTermsType,
+} from "../../lib/api/EtoApi.interfaces";
 import { TRequestStatus } from "../../lib/api/KycApi.interfaces";
-import { selectBackupCodesVerified, selectVerifiedUserEmail } from "../../modules/auth/selectors";
-import { selectKycRequestStatus } from "../../modules/kyc/selectors";
+import { actions } from "../../modules/actions";
+import {
+  selectBackupCodesVerified,
+  selectIsUserEmailVerified,
+  selectVerifiedUserEmail,
+} from "../../modules/auth/selectors";
+import { etoFlowInitialState } from "../../modules/eto-flow/reducer";
+import { selectEtoState, selectFormFractionDone } from "../../modules/eto-flow/selectors";
+import { selectKycRequestStatus, selectWidgetLoading } from "../../modules/kyc/selectors";
 import { selectIsLightWallet } from "../../modules/web3/selectors";
 import { appConnect } from "../../store";
 import { LayoutAuthorized } from "../layouts/LayoutAuthorized";
 import { SettingsWidgets } from "../settings/SettingsWidgets";
+import { SubmitProposalWidget } from "../settings/submitProposalWidget/SubmitProposalWidget";
 import { ETOFormsProgressSection } from "./dashboard/ETOFormsProgressSection";
 import { DashboardSection } from "./shared/DashboardSection";
+
+const SUBMIT_PROPOSAL_THRESHOLD = 0.9;
 
 interface IStateProps {
   isLightWallet: boolean;
   verifiedEmail?: string;
   backupCodesVerified?: boolean;
   requestStatus?: TRequestStatus;
+  etoFormProgress?: number;
+  isEmailVerified?: boolean;
+  etoState?: EtoState;
+}
+
+interface IDispatchProps {
+  loadDataStart: () => void;
 }
 
 type IProps = IStateProps;
 
-export const EtoDashboardComponent: React.SFC<IProps> = props => {
-  const { verifiedEmail, backupCodesVerified, requestStatus } = props;
-  const isVerificationSectionDone = !!(
-    verifiedEmail &&
-    backupCodesVerified &&
-    requestStatus === "Accepted"
-  );
-  return (
-    <LayoutAuthorized>
-      <Row className="row-gutter-top">
-        {!isVerificationSectionDone && (
-          <>
-            <DashboardSection
-              step={1}
-              title="VERIFICATION"
-              data-test-id="eto-dashboard-verification"
-            />
-            <SettingsWidgets isDynamic={true} {...props} />
-          </>
-        )}
-        <DashboardSection
-          step={2}
-          title="ETO APPLICATION"
-          data-test-id="eto-dashboard-application"
-        />
-        <ETOFormsProgressSection />
-      </Row>
-    </LayoutAuthorized>
-  );
-};
+class EtoDashboardComponent extends React.Component<any> {
+  // TODO: REMOVE ANY
+  componentDidMount(): void {
+    const { kycStatus, isEmailVerified, loadDataStart } = this.props;
 
-export const EtoDashboard = appConnect<IStateProps>({
-  stateToProps: s => ({
-    isLightWallet: selectIsLightWallet(s.web3),
-    verifiedEmail: selectVerifiedUserEmail(s.auth),
-    backupCodesVerified: selectBackupCodesVerified(s.auth),
-    requestStatus: selectKycRequestStatus(s.kyc),
+    const shouldEtoDataLoad = kycStatus === "Accepted" && isEmailVerified;
+    if (shouldEtoDataLoad) {
+      loadDataStart();
+    }
+  }
+
+  render(): React.ReactNode {
+    const {
+      verifiedEmail,
+      backupCodesVerified,
+      requestStatus,
+      etoFormProgress,
+      loadDataStart,
+      kycStatus,
+      isEmailVerified,
+      etoState,
+    } = this.props;
+
+    const isVerificationSectionDone = !!(
+      verifiedEmail &&
+      backupCodesVerified &&
+      requestStatus === "Accepted"
+    );
+
+    const shouldEtoDataLoad = kycStatus === "Accepted" && isEmailVerified;
+
+    return (
+      <LayoutAuthorized>
+        <Row className="row-gutter-top">
+          {!isVerificationSectionDone && (
+            <>
+              <DashboardSection
+                step={1}
+                title="VERIFICATION"
+                data-test-id="eto-dashboard-verification"
+              />
+              <SettingsWidgets isDynamic={true} {...this.props} />
+            </>
+          )}
+          {etoFormProgress > SUBMIT_PROPOSAL_THRESHOLD &&
+            etoState === "preview" && (
+              <>
+                <DashboardSection
+                  step={3}
+                  title="SUBMIT PROPOSAL"
+                  data-test-id="eto-dashboard-verification"
+                />
+                <Col lg={4} xs={12}>
+                  <SubmitProposalWidget />
+                </Col>
+              </>
+            )}
+          {etoState === "preview" && (
+            <>
+              <DashboardSection
+                step={2}
+                title="ETO APPLICATION"
+                data-test-id="eto-dashboard-application"
+              />
+              <ETOFormsProgressSection {...this.props} />
+            </>
+          )}
+        </Row>
+      </LayoutAuthorized>
+    );
+  }
+}
+
+export const EtoDashboard = compose<React.SFC>(
+  appConnect<any, IDispatchProps>({
+    stateToProps: s => ({
+      companyInformationProgress: selectFormFractionDone(
+        EtoCompanyInformationType.toYup(),
+        s.etoFlow.companyData,
+        etoFlowInitialState,
+      ),
+      etoTermsProgress: selectFormFractionDone(
+        EtoTermsType.toYup(),
+        s.etoFlow.etoData,
+        etoFlowInitialState,
+      ),
+      etoKeyIndividualsProgress: selectFormFractionDone(
+        EtoKeyIndividualsType.toYup(),
+        s.etoFlow.companyData,
+        etoFlowInitialState,
+      ),
+      legalInformationProgress: selectFormFractionDone(
+        EtoLegalInformationType.toYup(),
+        s.etoFlow.companyData,
+        etoFlowInitialState,
+      ),
+      productVisionProgress: selectFormFractionDone(
+        EtoProductVisionType.toYup(),
+        s.etoFlow.companyData,
+        etoFlowInitialState,
+      ),
+      etoMediaProgress: selectFormFractionDone(
+        EtoMediaType.toYup(),
+        s.etoFlow.companyData,
+        etoFlowInitialState,
+      ),
+      etoFormProgress: selectFormFractionDone(
+        TGeneralEtoDataType.toYup(),
+        { ...s.etoFlow.companyData, ...s.etoFlow.etoData },
+        etoFlowInitialState,
+      ),
+      loadingData: s.etoFlow.loading,
+      kycStatus: selectKycRequestStatus(s.kyc),
+      isEmailVerified: selectIsUserEmailVerified(s.auth),
+      businessRequestStateLoading: selectWidgetLoading(s.kyc),
+      isLightWallet: selectIsLightWallet(s.web3),
+      verifiedEmail: selectVerifiedUserEmail(s.auth),
+      backupCodesVerified: selectBackupCodesVerified(s.auth),
+      requestStatus: selectKycRequestStatus(s.kyc),
+      etoState: selectEtoState(s.etoFlow),
+    }),
+    dispatchToProps: dispatch => ({
+      loadDataStart: () => dispatch(actions.etoFlow.loadDataStart()),
+    }),
   }),
-})(EtoDashboardComponent);
+)(EtoDashboardComponent);
--- app/components/eto/dashboard/ETOFormsProgressSection.tsx
+++ app/components/eto/dashboard/ETOFormsProgressSection.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Col } from "reactstrap";
-import { compose } from "redux";
 import {
   EtoCompanyInformationType,
   EtoKeyIndividualsType,
@@ -10,16 +9,11 @@ import {
   EtoTermsType,
 } from "../../../lib/api/EtoApi.interfaces";
 import { TRequestStatus } from "../../../lib/api/KycApi.interfaces";
-import { actions } from "../../../modules/actions";
-import { selectIsUserEmailVerified } from "../../../modules/auth/selectors";
-import { etoFlowInitialState } from "../../../modules/eto-flow/reducer";
-import { selectFormFractionDone } from "../../../modules/eto-flow/selectors";
 import { selectKycRequestStatus, selectWidgetLoading } from "../../../modules/kyc/selectors";
-import { appConnect } from "../../../store";
 import { EtoFormProgressWidget } from "../../shared/EtoFormProgressWidget";
 import { etoRegisterRoutes } from "../registration/routes";
 
-interface IStateProps {
+interface IProps {
   companyInformationProgress: number;
   productVisionProgress: number;
   legalInformationProgress: number;
@@ -31,130 +25,69 @@ interface IStateProps {
   isEmailVerified: boolean;
 }
 
-interface IDispatchProps {
-  loadDataStart: () => void;
-}
+export const ETOFormsProgressSection: React.SFC<IProps> = ({
+  companyInformationProgress,
+  productVisionProgress,
+  etoTermsProgress,
+  loadingData,
+  etoKeyIndividualsProgress,
+  legalInformationProgress,
+  kycStatus,
+  isEmailVerified,
+}) => {
+  const shouldEtoDataLoad = kycStatus === "Accepted" && isEmailVerified;
 
-type IProps = IStateProps & IDispatchProps;
-
-class ETOFormsProgressSectionWidget extends React.Component<IProps> {
-  componentDidMount(): void {
-    const { kycStatus, isEmailVerified, loadDataStart } = this.props;
-
-    const shouldEtoDataLoad = kycStatus === "Accepted" && isEmailVerified;
-    if (shouldEtoDataLoad) loadDataStart();
-  }
-
-  render(): React.ReactNode {
-    const {
-      companyInformationProgress,
-      productVisionProgress,
-      etoTermsProgress,
-      loadingData,
-      etoKeyIndividualsProgress,
-      legalInformationProgress,
-      kycStatus,
-      isEmailVerified,
-    } = this.props;
-
-    const shouldEtoDataLoad = kycStatus === "Accepted" && isEmailVerified;
-
-    return (
-      <>
-        <Col lg={4} xs={12} md={6} className="mb-4">
-          <EtoFormProgressWidget
-            isLoading={loadingData}
-            to={etoRegisterRoutes.companyInformation}
-            progress={shouldEtoDataLoad ? companyInformationProgress : 0}
-            name="Company Information"
-          />
-        </Col>
-        <Col lg={4} xs={12} md={6} className="mb-4">
-          <EtoFormProgressWidget
-            isLoading={loadingData}
-            to={etoRegisterRoutes.etoTerms}
-            progress={shouldEtoDataLoad ? etoTermsProgress : 0}
-            name="ETO Terms"
-          />
-        </Col>
-        <Col lg={4} xs={12} md={6} className="mb-4">
-          <EtoFormProgressWidget
-            isLoading={loadingData}
-            to={etoRegisterRoutes.keyIndividuals}
-            progress={shouldEtoDataLoad ? etoKeyIndividualsProgress : 0}
-            name="Key Individuals"
-          />
-        </Col>
-        <Col lg={4} xs={12} md={6} className="mb-4">
-          <EtoFormProgressWidget
-            isLoading={loadingData}
-            to={etoRegisterRoutes.legalInformation}
-            progress={shouldEtoDataLoad ? legalInformationProgress : 0}
-            name="Legal Information"
-          />
-        </Col>
-        <Col lg={4} xs={12} md={6} className="mb-4">
-          <EtoFormProgressWidget
-            isLoading={loadingData}
-            to={etoRegisterRoutes.productVision}
-            progress={shouldEtoDataLoad ? productVisionProgress : 0}
-            name="Product Vision"
-          />
-        </Col>
-        <Col lg={4} xs={12} md={6} className="mb-4">
-          <EtoFormProgressWidget
-            isLoading={loadingData}
-            to={etoRegisterRoutes.etoMedia}
-            progress={shouldEtoDataLoad ? productVisionProgress : 0}
-            name="Media"
-          />
-        </Col>
-        {/* TODO: ADD TRANSLATIONS */}
-      </>
-    );
-  }
-}
-
-export const ETOFormsProgressSection = compose<React.SFC>(
-  appConnect<IStateProps, IDispatchProps>({
-    stateToProps: s => ({
-      companyInformationProgress: selectFormFractionDone(
-        EtoCompanyInformationType.toYup(),
-        s.etoFlow.companyData,
-        etoFlowInitialState,
-      ),
-      etoTermsProgress: selectFormFractionDone(
-        EtoTermsType.toYup(),
-        s.etoFlow.etoData,
-        etoFlowInitialState,
-      ),
-      etoKeyIndividualsProgress: selectFormFractionDone(
-        EtoKeyIndividualsType.toYup(),
-        s.etoFlow.companyData,
-        etoFlowInitialState,
-      ),
-      legalInformationProgress: selectFormFractionDone(
-        EtoLegalInformationType.toYup(),
-        s.etoFlow.companyData,
-        etoFlowInitialState,
-      ),
-      productVisionProgress: selectFormFractionDone(
-        EtoProductVisionType.toYup(),
-        s.etoFlow.companyData,
-        etoFlowInitialState,
-      ),
-      etoMediaProgress: selectFormFractionDone(
-        EtoMediaType.toYup(),
-        s.etoFlow.companyData,
-        etoFlowInitialState,
-      ),
-      loadingData: s.etoFlow.loading,
-      kycStatus: selectKycRequestStatus(s.kyc),
-      isEmailVerified: selectIsUserEmailVerified(s.auth),
-      businessRequestStateLoading: selectWidgetLoading(s.kyc),
-    }),
-    dispatchToProps: dispatch => ({
-      loadDataStart: () => dispatch(actions.etoFlow.loadDataStart()),
-    }),
-  }),
-)(ETOFormsProgressSectionWidget);
+  return (
+    <>
+      <Col lg={4} xs={12} md={6} className="mb-4">
+        <EtoFormProgressWidget
+          isLoading={loadingData}
+          to={etoRegisterRoutes.companyInformation}
+          progress={shouldEtoDataLoad ? companyInformationProgress : 0}
+          name="Company Information"
+        />
+      </Col>
+      <Col lg={4} xs={12} md={6} className="mb-4">
+        <EtoFormProgressWidget
+          isLoading={loadingData}
+          to={etoRegisterRoutes.etoTerms}
+          progress={shouldEtoDataLoad ? etoTermsProgress : 0}
+          name="ETO Terms"
+        />
+      </Col>
+      <Col lg={4} xs={12} md={6} className="mb-4">
+        <EtoFormProgressWidget
+          isLoading={loadingData}
+          to={etoRegisterRoutes.keyIndividuals}
+          progress={shouldEtoDataLoad ? etoKeyIndividualsProgress : 0}
+          name="Key Individuals"
+        />
+      </Col>
+      <Col lg={4} xs={12} md={6} className="mb-4">
+        <EtoFormProgressWidget
+          isLoading={loadingData}
+          to={etoRegisterRoutes.legalInformation}
+          progress={shouldEtoDataLoad ? legalInformationProgress : 0}
+          name="Legal Information"
+        />
+      </Col>
+      <Col lg={4} xs={12} md={6} className="mb-4">
+        <EtoFormProgressWidget
+          isLoading={loadingData}
+          to={etoRegisterRoutes.productVision}
+          progress={shouldEtoDataLoad ? productVisionProgress : 0}
+          name="Product Vision"
+        />
+      </Col>
+      <Col lg={4} xs={12} md={6} className="mb-4">
+        <EtoFormProgressWidget
+          isLoading={loadingData}
+          to={etoRegisterRoutes.etoMedia}
+          progress={shouldEtoDataLoad ? productVisionProgress : 0}
+          name="Media"
+        />
+      </Col>
+      {/* TODO: ADD TRANSLATIONS */}
+    </>
+  );
+};

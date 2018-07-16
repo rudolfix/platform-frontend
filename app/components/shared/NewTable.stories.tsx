b1538f49ca97a9ccec102296fffe827b474b24f4
Patryk Lizoń <patryk@hangover.studio>
2018-07-16 08:08:36 +0200

    Add NewTable component

--- app/components/shared/NewTable.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-
-import { NewTable } from './NewTable';
-
-const titles = ["Token", "Balance", "Value EUR", "Price EUR", "nEUR Reward", "ETO Status"];
-
-storiesOf('NewTable', module)
-  .add('default', () => (<NewTable titles={titles} />))
--- app/components/shared/NewTable.tsx
+++ app/components/shared/NewTable.tsx
@@ -1,32 +1,62 @@
-import * as React from 'react';
+import * as cn from "classnames";
+import { uniqueId } from "lodash";
+import * as React from "react";
 
-import { TTranslatedString } from '../../types';
-import * as styles from './NewTable.module.scss'
-import { Panel } from './Panel';
+import { TTranslatedString } from "../../types";
+import * as styles from "./NewTable.module.scss";
+import { Panel } from "./Panel";
 
 interface INewTableHeader {
-  titles: TTranslatedString[];
+  titles: (TTranslatedString | React.ReactNode)[];
 }
 
+interface INewTableRow {
+  children: React.ReactNode[];
+}
 
-export const NewTable:React.SFC<INewTableHeader> = ({titles}) => {
+interface INewTable {
+  children: React.ReactElement<INewTableRow> | React.ReactElement<INewTableRow>[];
+  className?: string;
+  keepRhythm?: boolean;
+}
+
+type TProps = INewTable & INewTableHeader;
+
+export class NewTableRow extends React.Component<INewTableRow> {
+  render(): React.ReactNode {
+    const { children } = this.props;
+
+    return (
+      <tr className={styles.row}>
+        {React.Children.map(children, child => {
+          return (
+            <th className={styles.cell} key={uniqueId("table-row")}>
+              {child}
+            </th>
+          );
+        })}
+      </tr>
+    );
+  }
+}
+
+export const NewTable: React.SFC<TProps> = ({ titles, children, className, keepRhythm }) => {
   return (
     <Panel>
-      <table>
-        <thead>
-          <tr>
-            {titles.map((title, index) => <th key={index}>{title}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>asdfasdfasdfasf</th>
-            <th>asdfasdfasdfasfasdfasdfasdfasfasdfasdfasdfasf</th>
-            <th>asdfasdfasdfasf</th>
-            <th>asdfasdfasdfasf</th>
-          </tr>
-        </tbody>
-      </table>
+      <div className={cn(styles.tableWrapper, className, keepRhythm && "keep-rhythm")}>
+        <table className={styles.table}>
+          <thead className={styles.header}>
+            <tr>
+              {titles.map((title, index) => (
+                <th className={styles.cell} key={index}>
+                  {title}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>{children}</tbody>
+        </table>
+      </div>
     </Panel>
-  )
-}
+  );
+};

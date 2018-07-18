8874a62cf6dc6446d17827bda0660501ef172881
Thomas Gorny <thomas.gorny@pm.me>
2018-07-18 15:12:16 +0200

    work on transaction state component

---
+++ app/components/shared/modals/TransactionState.stories.tsx
@@ -0,0 +1,22 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import { FormattedMessage } from 'react-intl-phraseapp'
+import { DocumentLink } from '../DocumentLink'
+import { TransactionState } from "./TransactionState";
+
+storiesOf("TransactionState", module)
+  .add("pending", () => (
+    <TransactionState state="pending" txHash="234523451234" blockNumber={12343456}>
+      <p>Your TX has been broadcast to the network and is waiting to be mined & confirmed. During ICOs, it may take 3+ hours to confirm. While this transaction is pending you will be blocked from making another transaction on the platform. <a>View your transaction</a>.</p>
+    </TransactionState>
+  ))
+  .add("reverted", () => (
+    <TransactionState state="reverted" txHash="23452345lkjlkjh123kj4h123lk4jh">
+
+    </TransactionState>
+  ))
+  .add("confirmed", () => (
+    <TransactionState state="confirmed" txHash="01234912341234kjlh1234kjh1234lkjh2134" title={<FormattedMessage id="investment-flow.investment-confirmed" />}>
+      <DocumentLink url="some.pdf" name={<FormattedMessage id="investment-flow.summary.download-agreement" />} />
+    </TransactionState>
+  ))

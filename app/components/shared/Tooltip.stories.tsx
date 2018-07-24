ebea61e96c82183bf59683383fff3111ed0729a5
Thomas Gorny <thomas.gorny@pm.me>
2018-07-24 15:02:50 +0200

    add space to Tooltip stories so that the tooltip is actually visible

--- app/components/shared/Tooltip.stories.tsx
+++ app/components/shared/Tooltip.stories.tsx
@@ -6,4 +6,12 @@ import { Tooltip } from "./Tooltip";
 const lorem =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, consequatur deserunt voluptatibus sapiente ducimus iusto culpa consectetur minus, voluptatum tempora nostrum quasi, rerum non facilis doloribus tempore ea obcaecati reprehenderit!";
 
-storiesOf("Tooltip", module).add("default", () => <Tooltip content={lorem} />);
+storiesOf("Tooltip", module).add("default", () =>
+  <div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <Tooltip content={lorem} />
+  </div>
+);

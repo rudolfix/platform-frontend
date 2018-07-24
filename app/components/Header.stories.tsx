e287c36bd3bedf1d42979cd364f31e06103b88f3
Thomas Gorny <thomas.gorny@pm.me>
2018-07-24 15:47:31 +0200

    update header with new logo image

---
+++ app/components/Header.stories.tsx
@@ -0,0 +1,17 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { HeaderComponent } from "./Header";
+
+storiesOf("Header", module)
+  .add("investor", () => <>
+    <HeaderComponent isAuthorized={false} logout={() => { }} location={"foo"} userType="investor" />
+    <br />
+    <HeaderComponent isAuthorized={true} logout={() => { }} location={"foo"} userType="investor" />
+  </>)
+  .add("issuer", () => <>
+    <HeaderComponent isAuthorized={false} logout={() => { }} location={"foo"} userType="issuer" />
+    <br />
+    <HeaderComponent isAuthorized={true} logout={() => { }} location={"foo"} userType="issuer" />
+  </>);
+

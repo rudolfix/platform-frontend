4afdb91384aebdc3d4d0fbe888f2c672942d0eed
Thomas Gorny <thomas.gorny@pm.me>
2018-07-24 15:55:44 +0200

    linting and formatting

--- app/components/Header.stories.tsx
+++ app/components/Header.stories.tsx
@@ -4,14 +4,22 @@ import * as React from "react";
 import { HeaderComponent } from "./Header";
 
 storiesOf("Header", module)
-  .add("investor", () => <>
-    <HeaderComponent isAuthorized={false} logout={() => { }} location={"foo"} userType="investor" />
-    <br />
-    <HeaderComponent isAuthorized={true} logout={() => { }} location={"foo"} userType="investor" />
-  </>)
-  .add("issuer", () => <>
-    <HeaderComponent isAuthorized={false} logout={() => { }} location={"foo"} userType="issuer" />
-    <br />
-    <HeaderComponent isAuthorized={true} logout={() => { }} location={"foo"} userType="issuer" />
-  </>);
-
+  .add("investor", () => (
+    <>
+      <HeaderComponent
+        isAuthorized={false}
+        logout={() => {}}
+        location={"foo"}
+        userType="investor"
+      />
+      <br />
+      <HeaderComponent isAuthorized={true} logout={() => {}} location={"foo"} userType="investor" />
+    </>
+  ))
+  .add("issuer", () => (
+    <>
+      <HeaderComponent isAuthorized={false} logout={() => {}} location={"foo"} userType="issuer" />
+      <br />
+      <HeaderComponent isAuthorized={true} logout={() => {}} location={"foo"} userType="issuer" />
+    </>
+  ));

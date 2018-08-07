8371972896c91c0f2dbc7e574f98f6a69bcd52c0
Krzysztof Kaczor <chris@kaczor.io>
2018-08-07 12:28:29 +0200

    add translations

--- app/components/modals/txSender/shared/TxPending.tsx
+++ app/components/modals/txSender/shared/TxPending.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { SpinningEthereum } from "../../../landing/parts/SpinningEthereum";
 
+import { FormattedMessage } from "react-intl-phraseapp";
 import * as styles from "./TxPending.module.scss";
 
 export interface ITxPendingProps {
@@ -13,20 +14,21 @@ export const TxPending: React.SFC<ITxPendingProps> = ({ blockId, txHash }) => (
   <div className="text-center">
     <SpinningEthereum className="mb-3" />
 
-    <h3 className={styles.title}>Pending transaction</h3>
+    <h3 className={styles.title}>
+      <FormattedMessage id="tx-sender.tx-pending.title" />
+    </h3>
 
     <p>
-      Your TX has been broadcast to the network and is waiting to be mined & confirmed. During ICOs,
-      it may take 3+ hours to confirm. While this transaction is pending you will be blocked from
-      making another transaction on the platform.
+      <FormattedMessage id="tx-sender.tx-pending.description" />
     </p>
 
     <a className={styles.txHash} href={`https://etherscan.io/tx/${txHash}`} target="_blank">
-      TX Hash: {txHash}
+      <FormattedMessage id="tx-sender.tx-pending.hash-label" /> {txHash}
     </a>
 
     <p>
-      Block number: <span className={styles.blockId}>{blockId}</span>
+      <FormattedMessage id="tx-sender.tx-pending.block-number-label" />
+      <span className={styles.blockId}>{blockId}</span>
     </p>
   </div>
 );

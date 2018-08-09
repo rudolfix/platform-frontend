1d43195ea9413e836a4e87516d7dd02cc594a429
Thomas Gorny <thomas.gorny@pm.me>
2018-08-09 19:46:50 +0200

    cleanup

--- app/components/eto/registration/pages/EtoVotingRights.tsx
+++ app/components/eto/registration/pages/EtoVotingRights.tsx
@@ -79,10 +79,9 @@ class EtoForm extends React.Component<FormikProps<TPartialEtoSpecData> & IProps>
         </div>
 
         <Col>
-          <Row className="justify-content-end">
+          <Row className="justify-content-center">
             <Button
               layout="primary"
-              className="mr-4"
               type="submit"
               onClick={() => {
                 this.props.saveData(this.props.values);

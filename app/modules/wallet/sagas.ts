c658d5aa578f3760887629f810475f687b6aa36c
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-08-25 02:03:39 +0200

    loads wallet state returning empty wallets if contract do not exist
    ICBM Commitment only - will be gone soon

--- app/modules/wallet/sagas.ts
+++ app/modules/wallet/sagas.ts
@@ -1,15 +1,16 @@
 import * as promiseAll from "promise-all";
 
-import BigNumber from "bignumber.js";
 import { fork, put, select } from "redux-saga/effects";
 import { TGlobalDependencies } from "../../di/setupBindings";
+import { ICBMLockedAccount } from "../../lib/contracts/ICBMLockedAccount";
+import { LockedAccount } from "../../lib/contracts/LockedAccount";
 import { IAppState } from "../../store";
 import { EthereumAddress } from "../../types";
 import { actions } from "../actions";
 import { numericValuesToString } from "../contracts/utils";
 import { neuCall, neuTakeEvery } from "../sagas";
-import { selectEthereumAddress } from "../web3/selectors";
-import { IWalletStateData } from "./reducer";
+import { selectEthereumAddressWithChecksum } from "../web3/selectors";
+import { ILockedWallet, IWalletStateData } from "./reducer";
 import { selectIsLoaded } from "./selectors";
 
 function* loadWalletDataSaga({ logger }: TGlobalDependencies): any {
@@ -17,7 +18,7 @@ function* loadWalletDataSaga({ logger }: TGlobalDependencies): any {
     const isLoaded = yield select((s: IAppState) => selectIsLoaded(s.wallet));
     if (isLoaded) return;
 
-    const ethAddress = yield select((s: IAppState) => selectEthereumAddress(s.web3));
+    const ethAddress = yield select((s: IAppState) => selectEthereumAddressWithChecksum(s.web3));
 
     const state: IWalletStateData = yield neuCall(loadWalletDataAsync, ethAddress);
 
@@ -28,36 +29,44 @@ function* loadWalletDataSaga({ logger }: TGlobalDependencies): any {
   }
 }
 
+async function loadICBMWallet(
+  ethAddress: EthereumAddress,
+  lockedAccount?: ICBMLockedAccount | LockedAccount,
+): Promise<ILockedWallet> {
+  if (lockedAccount) {
+    const balance = await lockedAccount.balanceOf(ethAddress);
+    return numericValuesToString({
+      LockedBalance: balance[0],
+      neumarksDue: balance[1],
+      unlockDate: balance[2],
+    });
+  } else {
+    // todo: may be removed when contracts deployed on production
+    return {
+      LockedBalance: "0",
+      neumarksDue: "0",
+      unlockDate: "0",
+    };
+  }
+}
+
 export async function loadWalletDataAsync(
   { contractsService, web3Manager }: TGlobalDependencies,
   ethAddress: EthereumAddress,
 ): Promise<IWalletStateData> {
   return {
-    ...(await promiseAll({
-      euroTokenLockedWallet: contractsService.euroLock.balanceOf(ethAddress).then(b =>
-        numericValuesToString({
-          ICBMLockedBalance: b[0],
-          neumarksDue: b[1],
-          unlockDate: b[2],
-        }),
-      ),
-      etherTokenLockedWallet: contractsService.etherLock.balanceOf(ethAddress).then(b =>
-        numericValuesToString({
-          ICBMLockedBalance: b[0],
-          neumarksDue: b[1],
-          unlockDate: b[2],
-        }),
-      ),
-    })),
+    ...{
+      euroTokenICBMLockedWallet: await loadICBMWallet(ethAddress, contractsService.icbmEuroLock),
+      etherTokenICBMLockedWallet: await loadICBMWallet(ethAddress, contractsService.icbmEtherLock),
+      euroTokenLockedWallet: await loadICBMWallet(ethAddress, contractsService.euroLock),
+      etherTokenLockedWallet: await loadICBMWallet(ethAddress, contractsService.etherLock),
+    },
     ...numericValuesToString(
       await promiseAll({
-        etherTokenBalance: contractsService.etherTokenContract.balanceOf(ethAddress),
-        euroTokenBalance: contractsService.euroTokenContract.balanceOf(ethAddress),
+        etherTokenBalance: contractsService.etherToken.balanceOf(ethAddress),
+        euroTokenBalance: contractsService.euroToken.balanceOf(ethAddress),
         etherBalance: web3Manager.internalWeb3Adapter.getBalance(ethAddress),
-        neuBalance: contractsService.neumarkContract.balanceOf(ethAddress),
-        //@TODO: Connect locked balance with locked wallet
-        euroTokenLockedBalance: Promise.resolve(new BigNumber("0")),
-        etherTokenLockedBalance: Promise.resolve(new BigNumber("0")),
+        neuBalance: contractsService.neumark.balanceOf(ethAddress),
       }),
     ),
   };

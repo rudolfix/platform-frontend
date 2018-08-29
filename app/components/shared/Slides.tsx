b55b7810d9db551df6a3abbdab009110bb49a6dc
Patryk Lizoń <patryk@hangover.studio>
2018-08-29 12:05:46 +0200

    Fix linter issues

--- app/components/shared/Slides.tsx
+++ app/components/shared/Slides.tsx
@@ -3,14 +3,14 @@ import * as React from "react";
 import { Proportion } from "./Proportion";
 
 interface IProps {
-  slideShareUrl: string;
+  slideShareUrl: string | undefined;
   className?: string;
 }
 
 export const Slides: React.SFC<IProps> = ({ slideShareUrl, className }) => {
-  return (
+  return slideShareUrl ? (
     <Proportion className={className}>
       <iframe src={slideShareUrl} allowFullScreen scrolling="no" />
     </Proportion>
-  );
+  ) : null;
 };

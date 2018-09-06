dcab9c35223f57d6655a393b49e1d7ee12fd18d3
Thomas Gorny <thomas.gorny@pm.me>
2018-09-06 15:14:41 +0200

    update storybook and fix visual regression

--- app/components/modals/txSender/investment-flow/Summary.stories.tsx
+++ app/components/modals/txSender/investment-flow/Summary.stories.tsx
@@ -5,12 +5,11 @@ import { InvestmentSummaryComponent } from "./Summary";
 
 const data = {
   companyName: "X company",
-  tokenPrice: 0.34,
   etoAddress: "0x123434562134asdf2412341234adf12341234",
   investmentEth: "12345678900000000000",
   investmentEur: "123456789000000000000000",
   gasCostEth: "2000000000000000",
-  equityTokens: "500.12345 Tokens",
+  equityTokens: "500",
   estimatedReward: "40000000000000000000",
   etherPriceEur: "200",
   agreementUrl: "somePDF.pdf",

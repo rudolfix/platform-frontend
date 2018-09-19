8511081623579d35a55c06e14237856cdcfdae2c
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-09-19 14:59:33 +0200

    Adds automatic wallet updates

--- app/modules/tx/sender/sagas.ts
+++ app/modules/tx/sender/sagas.ts
@@ -1,17 +1,21 @@
+import { BigNumber } from "bignumber.js";
+import { addHexPrefix } from "ethereumjs-util";
 import { END, eventChannel } from "redux-saga";
 import { call, fork, put, race, select, take } from "redux-saga/effects";
 import * as Web3 from "web3";
-
-import { addHexPrefix } from "ethereumjs-util";
 import { TGlobalDependencies } from "../../../di/setupBindings";
 import { TxWithMetadata } from "../../../lib/api/users/interfaces";
 import { IAppState } from "../../../store";
+import { compareBigNumbers } from "../../../utils/BigNumberUtils";
 import { connectWallet, signMessage } from "../../accessWallet/sagas";
 import { actions, TAction } from "../../actions";
 import { neuCall, neuTakeEvery } from "../../sagas";
+import { selectEthereumAddressWithChecksum } from "../../web3/selectors";
 import { updateTxs } from "../monitor/sagas";
 import { ITxData } from "./../../../lib/web3/Web3Manager";
+import { selectEtherTokenBalance } from "./../../wallet/selectors";
 import { TxSenderType } from "./reducer";
+import { selectTxDetails, selectTxType } from "./selectors";
 
 export function* withdrawSaga({ logger }: TGlobalDependencies): any {
   try {
@@ -42,7 +46,9 @@ export function* txSendSaga(_: TGlobalDependencies, type: TxSenderType): any {
   }
 
   // we need to wait for modal to close anyway
+
   yield take("TX_SENDER_HIDE_MODAL");
+  yield put(actions.wallet.startLoadingWalletData());
 
   return result;
 }
@@ -90,24 +96,75 @@ function* ensureNoPendingTx(
   yield put(actions.txSender.txSenderWatchPendingTxsDone(type));
 }
 
+function* generateEthWithrdawTransaction({ contractsService }: TGlobalDependencies): any {
+  const txStateDetails = yield select((s: IAppState) => selectTxDetails(s.txSender));
+
+  let txDetails: ITxData | undefined;
+
+  if (!txStateDetails) return;
+  const etherTokenBalance = yield select((s: IAppState) => selectEtherTokenBalance(s.wallet));
+  const from = yield select((s: IAppState) => selectEthereumAddressWithChecksum(s.web3));
+
+  // transaction can be fully covered by etherTokens
+  if (compareBigNumbers(etherTokenBalance, txStateDetails.value) >= 0) {
+    // TODO: Make value in tx detail forced
+
+    // need to call 3 args version of transfer method. See the abi in the contract.
+    // so we call the rawWeb3Contract directly
+    const txInput = contractsService.etherToken.rawWeb3Contract.transfer[
+      "address,uint256,bytes"
+    ].getData(txStateDetails.to, txStateDetails.value, "");
+
+    txDetails = {
+      to: contractsService.etherToken.address,
+      from,
+      data: txInput,
+      value: "0",
+      gas: txStateDetails.gas,
+      gasPrice: txStateDetails.gasPrice,
+    };
+
+    // fill up etherToken with ether from wallet
+  } else {
+    const ethVal = new BigNumber(txStateDetails.value);
+    const difference = ethVal.sub(etherTokenBalance);
+    const txCall = contractsService.etherToken.depositAndTransferTx(txStateDetails.to, ethVal, [
+      "",
+    ]);
+    txDetails = {
+      to: contractsService.etherToken.address,
+      from,
+      data: txCall.getData(),
+      value: difference.round().toString(),
+      gas: txStateDetails.gas,
+      gasPrice: txStateDetails.gasPrice,
+    };
+  }
+
+  if (txDetails) {
+    yield put(actions.txSender.txSenderAccept());
+  }
+}
+
 function* sendTxSubSaga({ web3Manager, apiUserService }: TGlobalDependencies): any {
-  const txData: ITxData | undefined = yield select((s: IAppState) => s.txSender.txDetails);
-  const type: TxSenderType = yield select((s: IAppState) => s.txSender.type);
+  const txData: ITxData = yield select((s: IAppState) => selectTxDetails(s.txSender));
+  const type = yield select((s: IAppState) => selectTxType(s.txSender));
 
   if (!txData) {
     throw new Error("Tx data is not defined");
   }
   try {
-    const txHash: string = yield web3Manager.sendTransaction(txData as any);
+    const txHash: string = yield web3Manager.sendTransaction(txData);
     yield put(actions.txSender.txSenderSigned(txHash));
     const txWithMetadata: TxWithMetadata = {
       transaction: {
-        from: txData.from!,
+        from: txData.from,
         gas: addHexPrefix(Number(txData.gas!).toString(16)),
         gasPrice: "0x" + Number(txData.gasPrice!).toString(16),
         hash: txHash,
         input: txData.data || "0x0",
-        nonce: "0x" + Number(txData.nonce!).toString(16),
+        // TODO:Connect nonce
+        nonce: "0x0",
         to: txData.to!,
         value: "0x" + Number(txData.value!).toString(16),
         blockHash: undefined,
@@ -220,6 +277,7 @@ function* sendDummyTx({ logger }: TGlobalDependencies, action: TAction): any {
 export const txSendingSagasWatcher = function*(): Iterator<any> {
   yield fork(neuTakeEvery, "TX_SENDER_START_WITHDRAW_ETH", withdrawSaga);
   yield fork(neuTakeEvery, "TX_SENDER_START_INVESTMENT", investSaga);
+  yield fork(neuTakeEvery, "TX_SENDER_GENERATE_TX", generateEthWithrdawTransaction);
 
   // Dev only
   yield fork(neuTakeEvery, "TX_SENDER_DEBUG_SIGN_DUMMY_MESSAGE", signDummyMessage);

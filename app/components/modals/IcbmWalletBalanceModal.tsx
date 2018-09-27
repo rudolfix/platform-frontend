ff7cbc2284a9f52720aab8f84ee800caebdcbbb3
Patryk Lizoń <patryk@hangover.studio>
2018-09-27 14:01:14 +0200

    Add migration flow to icbmwalletbalance modal

--- app/components/modals/IcbmWalletBalanceModal.tsx
+++ app/components/modals/IcbmWalletBalanceModal.tsx
@@ -1,3 +1,4 @@
+import * as cn from "classnames";
 import * as React from "react";
 import { FormattedMessage } from "react-intl-phraseapp";
 import { Modal } from "reactstrap";
@@ -10,6 +11,8 @@ import {
 } from "../../modules/icbmWalletBalanceModal/selectors";
 import { appConnect } from "../../store";
 import { TTranslatedString } from "../../types";
+import { Button, ButtonLink } from "../shared/buttons";
+import { CopyToClipboard } from "../shared/CopyToClipboard";
 import { InlineIcon } from "../shared/InlineIcon";
 import { Money } from "../shared/Money";
 import { SectionHeader } from "../shared/SectionHeader";
@@ -19,6 +22,9 @@ import * as iconEth from "../../assets/img/eth_icon.svg";
 import * as iconDownload from "../../assets/img/inline_icons/download.svg";
 import * as iconNeu from "../../assets/img/neu_icon.svg";
 import * as styles from "./IcbmWalletBalanceModal.module.scss";
+import { appRoutes } from "../appRoutes";
+import { SpinningEthereum } from "../landing/parts/SpinningEthereum";
+import { ConfettiEthereum } from "../landing/parts/ConfettiEthereum";
 
 interface IStateProps {
   isOpen: boolean;
@@ -40,9 +46,10 @@ interface IProps {
     title: TTranslatedString;
     url: string;
   };
+  withCopy?: boolean;
 }
 
-const HighlightedField: React.SFC<IProps> = ({ label, value, icon, link }) => {
+const HighlightedField: React.SFC<IProps> = ({ label, value, icon, link, withCopy }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.highlightedHeader}>
@@ -59,42 +66,161 @@ const HighlightedField: React.SFC<IProps> = ({ label, value, icon, link }) => {
           </div>
         )}
       </div>
-      <div className={styles.highlightedField}>{value}</div>
+      <div className={cn(styles.highlightedField, withCopy && "d-flex justify-content-between")}>
+        {withCopy && <CopyToClipboard value={value} />}
+        {value}
+      </div>
     </div>
   );
 };
 
-class IcbmWalletBalanceComponent extends React.Component<IStateProps & IDispatchProps> {
+const BalanceBody:React.SFC<IStateProps> = ({ethAddress, isLoading, neumarksDue, etherBalance}) => {
+  return (
+    <>
+      <HighlightedField
+        label={
+          <FormattedMessage id="settings.modal.icbm-wallet-balance.icbm-wallet-address.label" />
+        }
+        value={ethAddress || ""}
+      />
+      <HighlightedField
+        label={<FormattedMessage id="settings.modal.icbm-wallet-balance.neu-balance.label" />}
+        value={isLoading ? <>Loading</> : <Money currency="neu" value={neumarksDue || "0"} />}
+        icon={iconNeu}
+      />
+      <HighlightedField
+        label={<FormattedMessage id="settings.modal.icbm-wallet-balance.eth-balance.label" />}
+        value={
+          isLoading ? <>Loading</> : <Money value={etherBalance || "0"} currency="eth" />
+        }
+        icon={iconEth}
+      />
+    </>
+  )
+}
+
+const MigrateBody:React.SFC<{ step: number }> = ({ step }) => {
+  return (
+    <>
+      <p className={styles.description}>
+        <FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.description" />
+      </p>
+      <SectionHeader className={styles.header}>
+        {step === 1 && <FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.step.1.title" />}
+        {step === 2 && <FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.step.2.title" />}
+      </SectionHeader>
+      <p className={styles.description}>
+        {step === 1 &&<FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.step.1.description" />}
+        {step === 2 &&<FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.step.2description" />}
+      </p>
+      <HighlightedField
+        label={<FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.field.from-icbm-wallet" />}
+        value="test value 1"
+        withCopy
+      />
+      <HighlightedField
+        label={<FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.field.to-smart-contract" />}
+        value="test value 2"
+        withCopy
+      />
+      <HighlightedField
+        label={<FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.field.amount-to-sent" />}
+        value="test value 3"
+        withCopy
+      />
+      <HighlightedField
+        label={<FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.field.gas-limit" />}
+        value="test value 4"
+        withCopy
+      />
+      <HighlightedField
+        label={<FormattedMessage id="settings.modal.icbm-wallet-balance.body.migrate.field.data" />}
+        value="test value 5"
+        withCopy
+      />
+    </>
+  )
+}
+
+const BalanceFooter:React.SFC<{startMigration: ()=> void}> = ({startMigration}) => {
+  return (
+      <Button onClick={startMigration}>
+        <FormattedMessage id="settings.modal.icbm-wallet-balance.button" />
+      </Button>
+  )
+}
+
+type TTransactionStatus = "success" | "waiting"
+
+const MigrateFooter:React.SFC<{goToNextStep: ()=> void, step: number, transactionStatus: TTransactionStatus}> = ({goToNextStep, step, transactionStatus}) => {
+  return (
+    <>
+      {transactionStatus === "waiting" && <div className={styles.footerWaiting}>
+        <SpinningEthereum className={styles.animatedEthereum} />
+        <FormattedMessage id="settings.modal.icbm-wallet-balance.footer.waiting-for-transaction" />
+      </div>}
+      {transactionStatus === "success" && <div className={styles.footerSuccess}>
+        <ConfettiEthereum className={styles.animatedEthereum} />
+        <div>
+          <FormattedMessage id="settings.modal.icbm-wallet-balance.footer.transaction-successful" />
+          {
+            step === 1 && (
+            <Button layout="secondary" onClick={goToNextStep}>
+              <FormattedMessage id="settings.modal.icbm-wallet-balance.button.next-step" />
+            </Button>)
+          }
+          {
+            step === 2 && (
+              <ButtonLink to={appRoutes.portfolio} layout="secondary">
+                <FormattedMessage id="settings.modal.icbm-wallet-balance.button.portfolio" />
+              </ButtonLink>)
+          }
+        </div>
+      </div>}
+    </>
+  )
+}
+
+class IcbmWalletBalanceComponent extends React.Component<IStateProps & IDispatchProps, { isMigrating: boolean, migrationStep: number}> {
+  state = {
+    isMigrating: false,
+    migrationStep: 1,
+  }
+
+  private startMigration = () => {
+    this.setState({ isMigrating: true })
+  }
+
+  private goToNextStep = () => {
+    this.setState(prevState => ({migrationStep: prevState.migrationStep + 1}))
+  }
+
   render(): React.ReactNode {
-    const { isOpen, onCancel, ethAddress, etherBalance, neumarksDue, isLoading } = this.props;
+    const { isOpen, onCancel } = this.props;
+    const { isMigrating, migrationStep } = this.state;
+
     return (
       <Modal isOpen={isOpen} toggle={onCancel}>
         <ModalComponentBody onClose={onCancel}>
           <div className={styles.content}>
             <SectionHeader className={styles.header}>
-              <FormattedMessage id="settings.modal.icbm-wallet-balance.title" />
+
+              {isMigrating
+                ? <FormattedMessage id="settings.modal.icbm-wallet-balance.title.migrate" />
+                : <FormattedMessage id="settings.modal.icbm-wallet-balance.title.balance" />
+              }
+              
             </SectionHeader>
-            <HighlightedField
-              label={
-                <FormattedMessage id="settings.modal.icbm-wallet-balance.icbm-wallet-address.label" />
-              }
-              value={ethAddress || ""}
-            />
-            <HighlightedField
-              label={<FormattedMessage id="settings.modal.icbm-wallet-balance.neu-balance.label" />}
-              value={isLoading ? <>Loading</> : <Money currency="neu" value={neumarksDue || "0"} />}
-              icon={iconNeu}
-            />
-            <HighlightedField
-              label={<FormattedMessage id="settings.modal.icbm-wallet-balance.eth-balance.label" />}
-              value={
-                isLoading ? <>Loading</> : <Money value={etherBalance || "0"} currency="eth" />
-              }
-              icon={iconEth}
-            />
-            <p className={styles.footer}>
-              <FormattedMessage id="settings.modal.icbm-wallet-balance.coming-soon" />
-            </p>
+
+            {isMigrating
+              ? <MigrateBody step={migrationStep} />
+              : <BalanceBody {...this.props} />
+            }
+
+            {isMigrating
+              ? <MigrateFooter transactionStatus="success" step={migrationStep} goToNextStep={this.goToNextStep} />
+              : <BalanceFooter startMigration={this.startMigration} />
+            }
           </div>
         </ModalComponentBody>
       </Modal>

b4d40048e84ab6ab99e3ff9773aacf4413810552
David <dscharf@gmx.net>
2018-09-27 13:34:48 +0200

    add missing file

---
+++ app/test-e2e/issuerDashboard/LightWalletETODashboard.spec.e2e.ts
@@ -0,0 +1,38 @@
+import {
+  assertBackupSeedWidgetVisible,
+  assertEmailActivationWidgetVisible,
+  assertEtoDashboard,
+  convertToUniqueEmail,
+  goToDashboard,
+  registerWithLightWalletETO,
+  verifyLatestUserEmail,
+} from "../utils";
+import { backupLightWalletSeed } from "../register/BackupSeed.spec.e2e";
+
+const password = "strongpassword";
+
+describe("Light Wallet ETO Dashboard", () => {
+  it("should register verify an email and assert that the dashboard is changing", () => {
+    const testEmail = convertToUniqueEmail("moe@test.com");
+
+    registerWithLightWalletETO(testEmail, password);
+
+    assertEtoDashboard();
+    assertBackupSeedWidgetVisible();
+    assertEmailActivationWidgetVisible();
+
+    backupLightWalletSeed();
+    goToDashboard();
+
+    assertEtoDashboard();
+    assertBackupSeedWidgetVisible(true);
+    assertEmailActivationWidgetVisible();
+
+    verifyLatestUserEmail();
+    goToDashboard();
+
+    assertEtoDashboard();
+    assertEmailActivationWidgetVisible(true);
+    assertBackupSeedWidgetVisible(true);
+  });
+});

f580bde41c9befb5538b8489b111a74e62285202
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-10-02 12:40:48 +0200

    Refactors neuTakeUntil to handel starting and stopping by more than one action

--- app/modules/sagasUtils.ts
+++ app/modules/sagasUtils.ts
@@ -1,4 +1,4 @@
-import { cancel, take } from "redux-saga/effects";
+import { all, cancel, take } from "redux-saga/effects";
 import { TGlobalDependencies } from "../di/setupBindings";
 import { TActionType } from "./actions";
 import { neuFork } from "./sagas";
@@ -7,14 +7,17 @@ import { neuFork } from "./sagas";
  * Starts saga on `startAction`, cancels on `stopAction`, loops...
  */
 export function* neuTakeUntil(
-  startAction: TActionType,
-  stopAction: TActionType,
+  startAction: TActionType | [TActionType],
+  stopAction: TActionType | [TActionType],
   saga: (deps: TGlobalDependencies) => any,
 ): any {
-  while (yield take(startAction)) {
+  const startActionArray = Array.isArray(startAction) ? startAction : [startAction];
+  const stopActionArray = Array.isArray(stopAction) ? stopAction : [stopAction];
+
+  while (yield all(startActionArray.map((actionType: TActionType) => take(actionType)))) {
     const cancelForkToken = yield neuFork(saga);
 
-    yield take(stopAction);
+    yield all(stopActionArray.map((actionType: TActionType) => take(actionType)));
     yield cancel(cancelForkToken);
   }
 }

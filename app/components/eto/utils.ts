4f17b2c4d993715bf5d986872812a7194e5e109a
lexey <tblz@palavara.com>
2018-10-30 13:08:32 +0100

    #1520 reverted some sanitizer function changes

--- app/components/eto/utils.ts
+++ app/components/eto/utils.ts
@@ -2,9 +2,14 @@ export interface ICompoundField {
   [x: string]: string | number | undefined;
 }
 
-export const sanitizeKeyValueCompoundField = (data: ICompoundField[] | undefined) =>
-  data &&
-  data.filter(compoundField => {
-    const keys = Object.keys(compoundField);
-    return compoundField[keys[0]] !== undefined && compoundField[keys[1]] !== undefined;
-  });
+export const sanitizeKeyValueCompoundField = (data: ICompoundField[] | undefined) => {
+  if (data !== undefined && data !== null) {
+    const cleanData = data.filter(compoundField => {
+      const keys = Object.keys(compoundField);
+      return compoundField[keys[0]] !== undefined && compoundField[keys[1]] !== undefined;
+    });
+    return cleanData.length ? cleanData : undefined;
+  } else {
+    return undefined;
+  }
+};

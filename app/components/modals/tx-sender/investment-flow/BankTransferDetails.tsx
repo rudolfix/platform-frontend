1adfde8e6fdb2b034dfd4cf2314b8c1d8f633c3e
Thomas Gorny <thomas.gorny@pm.me>
2018-11-06 18:03:14 +0100

    Improve bank transfer (#1699)
    * remove gas cost from bank transfer
    
    * revert modal order on bank transfer flow
    
    * add copytoclipboard on bank transfer details
    
    * improve styling on clipboard button
    
    * implement printable bank transfer details
    
    * add whitelist flag to bank transfer reference code
    
    * update infoRow style
    
    * add working unit test for bank transfer reference code
    
    * cleanup
    
    * cleanup repeated markup
    
    * change bank transfer details to use env variables

--- app/components/modals/tx-sender/investment-flow/BankTransferDetails.tsx
+++ app/components/modals/tx-sender/investment-flow/BankTransferDetails.tsx
@@ -1,8 +1,10 @@
+import * as cn from "classnames";
 import * as React from "react";
 import { FormattedHTMLMessage, FormattedMessage } from "react-intl-phraseapp";
+import * as ReactToPrint from "react-to-print";
 import { Col, Container, Row } from "reactstrap";
+import { compose, withHandlers } from "recompose";
 
-import { MONEY_DECIMALS } from "../../../../config/constants";
 import { actions } from "../../../../modules/actions";
 import {
   selectBankTransferAmount,
@@ -10,16 +12,14 @@ import {
   selectIsBankTransferGasStipend,
 } from "../../../../modules/investment-flow/selectors";
 import { selectClientCountry, selectClientName } from "../../../../modules/kyc/selectors";
-import { ETxSenderType } from "../../../../modules/tx/interfaces";
 import { appConnect } from "../../../../store";
 import { IIntlProps, injectIntlHelpers } from "../../../../utils/injectIntlHelpers";
-import { formatMoney } from "../../../../utils/Money.utils";
 import { Button, EButtonLayout } from "../../../shared/buttons";
+import { CopyToClipboard } from "../../../shared/CopyToClipboard";
 import { CheckboxComponent } from "../../../shared/forms";
 import { Heading } from "../../../shared/modals/Heading";
 import { InfoList } from "../shared/InfoList";
 import { InfoRow } from "../shared/InfoRow";
-import { ITxSummaryDispatchProps } from "../TxSender";
 
 import * as styles from "./Summary.module.scss";
 
@@ -34,138 +34,168 @@ interface IStateProps {
   gasStipend?: boolean;
 }
 
-interface IDispatchProps extends ITxSummaryDispatchProps {
+interface IDispatchProps {
   onGasStipendChange: () => void;
 }
 
-type IProps = IStateProps & IDispatchProps;
+interface IHandlerProps {
+  handleCheckbox: (e: any) => void;
+}
+
+type IProps = IStateProps & IDispatchProps & IHandlerProps;
+
+const CopyToClipboardLabel: React.SFC<{ label: string }> = ({ label }) => (
+  <>
+    <CopyToClipboard className={cn(styles.copyToClipboard, styles.nonPrintable)} value={label} />{" "}
+    &nbsp; {label}
+  </>
+);
 
 const BankTransferDetailsComponent = injectIntlHelpers(
-  ({ onAccept, onChange, onGasStipendChange, ...data }: IProps & IIntlProps) => {
-    return (
-      <Container className={styles.container}>
-        <Row className="mt-0">
-          <Col>
-            <Heading>
-              <FormattedMessage id="investment-flow.bank-transfer.direct-bank-transfer" />
-            </Heading>
-          </Col>
-        </Row>
+  class BankTransferDetailsComponent extends React.Component<IProps & IIntlProps> {
+    ref!: HTMLDivElement | null;
+    render(): React.ReactNode {
+      const { handleCheckbox, ...data } = this.props;
+      return (
+        <div className={styles.printable} ref={el => (this.ref = el)}>
+          <Container className={styles.container}>
+            <Row className="mt-0">
+              <Col>
+                <Heading>
+                  <FormattedMessage id="investment-flow.bank-transfer.direct-bank-transfer" />
+                </Heading>
+              </Col>
+            </Row>
 
-        <Row>
-          <Col>
-            <p>
-              <FormattedMessage id="investment-flow.bank-transfer.general-instructions" />
-            </p>
-            <FormattedHTMLMessage
-              tagName="p"
-              id="investment-flow.bank-transfer.bank-details-instructions"
-            />
-            <p>
-              <CheckboxComponent
-                checked={data.gasStipend}
-                name="gas-stipend"
-                label={<FormattedMessage id="investment-flow.bank-transfer.gas-stipend" />}
-                onChange={onGasStipendChange}
-              />
-            </p>
-          </Col>
-        </Row>
+            <Row>
+              <Col>
+                <p>
+                  <FormattedMessage id="investment-flow.bank-transfer.general-instructions" />
+                </p>
+                <FormattedHTMLMessage
+                  tagName="p"
+                  id="investment-flow.bank-transfer.bank-details-instructions"
+                />
+                <p>
+                  <CheckboxComponent
+                    checked={data.gasStipend}
+                    name="gas-stipend"
+                    label={<FormattedMessage id="investment-flow.bank-transfer.gas-stipend" />}
+                    onChange={handleCheckbox}
+                  />
+                </p>
+              </Col>
+            </Row>
 
-        <Row>
-          <Col>
-            <Heading>
-              <FormattedMessage id="investment-flow.bank-transfer.from-account" />
-            </Heading>
-          </Col>
-        </Row>
+            <Row>
+              <Col>
+                <Heading>
+                  <FormattedMessage id="investment-flow.bank-transfer.from-account" />
+                </Heading>
+              </Col>
+            </Row>
 
-        <Row>
-          <Col>
-            <InfoList>
-              <InfoRow
-                caption={<FormattedMessage id="investment-flow.bank-transfer.account-name" />}
-                value={data.accountName}
-              />
-              <InfoRow
-                caption={<FormattedMessage id="investment-flow.bank-transfer.country" />}
-                value={data.country}
-              />
-            </InfoList>
-          </Col>
-        </Row>
+            <Row>
+              <Col>
+                <InfoList>
+                  <InfoRow
+                    caption={<FormattedMessage id="investment-flow.bank-transfer.account-name" />}
+                    value={data.accountName}
+                  />
+                  <InfoRow
+                    caption={<FormattedMessage id="investment-flow.bank-transfer.country" />}
+                    value={data.country}
+                  />
+                </InfoList>
+              </Col>
+            </Row>
 
-        <Row>
-          <Col>
-            <Heading>
-              <FormattedMessage id="investment-flow.bank-transfer.to-account" />
-            </Heading>
-          </Col>
-        </Row>
+            <Row>
+              <Col>
+                <Heading>
+                  <FormattedMessage id="investment-flow.bank-transfer.to-account" />
+                </Heading>
+              </Col>
+            </Row>
 
-        <Row>
-          <Col>
-            <InfoList>
-              <InfoRow
-                caption={<FormattedMessage id="investment-flow.bank-transfer.recipient" />}
-                value={data.recipient}
-              />
-              <InfoRow
-                caption={<FormattedMessage id="investment-flow.bank-transfer.iban" />}
-                value={data.iban}
-              />
-              <InfoRow
-                caption={<FormattedMessage id="investment-flow.bank-transfer.bic" />}
-                value={data.bic}
-              />
-              <InfoRow
-                caption={<FormattedMessage id="investment-flow.bank-transfer.reference-code" />}
-                value={data.referenceCode}
-              />
-              <InfoRow
-                caption={<FormattedMessage id="investment-flow.bank-transfer.amount" />}
-                value={`€ ${formatMoney(data.amount, MONEY_DECIMALS, 2)}`}
-              />
-            </InfoList>
-          </Col>
-        </Row>
+            <Row>
+              <Col>
+                <InfoList>
+                  <InfoRow
+                    caption={<FormattedMessage id="investment-flow.bank-transfer.recipient" />}
+                    value={<CopyToClipboardLabel label={data.recipient} />}
+                  />
+                  <InfoRow
+                    caption={<FormattedMessage id="investment-flow.bank-transfer.iban" />}
+                    value={<CopyToClipboardLabel label={data.iban} />}
+                  />
+                  <InfoRow
+                    caption={<FormattedMessage id="investment-flow.bank-transfer.bic" />}
+                    value={<CopyToClipboardLabel label={data.bic} />}
+                  />
+                  <InfoRow
+                    caption={<FormattedMessage id="investment-flow.bank-transfer.reference-code" />}
+                    value={<CopyToClipboardLabel label={data.referenceCode} />}
+                  />
+                  <InfoRow
+                    caption={<FormattedMessage id="investment-flow.bank-transfer.amount" />}
+                    value={<CopyToClipboardLabel label={data.amount} />}
+                  />
+                </InfoList>
+              </Col>
+            </Row>
 
-        <Row className="justify-content-center mb-0 mt-0">
-          <Button layout={EButtonLayout.PRIMARY} type="button" onClick={onAccept}>
-            <FormattedMessage id="investment-flow.confirm" />
-          </Button>
-          <Button
-            layout={EButtonLayout.SECONDARY}
-            type="button"
-            onClick={onChange}
-            data-test-id="invest-modal-summary-change-button"
-          >
-            <FormattedMessage id="investment-flow.change" />
-          </Button>
-        </Row>
-      </Container>
-    );
+            <Row className="justify-content-center mb-0 mt-0">
+              <ReactToPrint
+                trigger={() => (
+                  <Button
+                    layout={EButtonLayout.PRIMARY}
+                    type="button"
+                    className={styles.nonPrintable}
+                  >
+                    <FormattedMessage id="investment-flow.bank-transfer.print" />
+                  </Button>
+                )}
+                content={() => this.ref}
+              />
+            </Row>
+          </Container>
+        </div>
+      );
+    }
   },
 );
 
-const BankTransferDetails = appConnect<IStateProps, IDispatchProps>({
-  stateToProps: state => {
-    return {
-      accountName: selectClientName(state.kyc),
-      country: selectClientCountry(state.kyc),
-      recipient: "Fifth Force GMBH",
-      iban: "DE1250094039446384529400565",
-      bic: "TLXXXXXXXXX",
-      referenceCode: selectBankTransferReferenceCode(state),
-      amount: selectBankTransferAmount(state),
-      gasStipend: selectIsBankTransferGasStipend(state),
-    };
-  },
-  dispatchToProps: d => ({
-    onAccept: () => d(actions.investmentFlow.showBankTransferSummary()),
-    onChange: () => d(actions.investmentFlow.changeBankTransfer(ETxSenderType.INVEST)),
-    onGasStipendChange: () => d(actions.investmentFlow.toggleBankTransferGasStipend()),
+const BankTransferDetails = compose<IProps, {}>(
+  appConnect<IStateProps, IDispatchProps>({
+    stateToProps: state => {
+      return {
+        accountName: selectClientName(state.kyc),
+        country: selectClientCountry(state.kyc),
+        recipient: process.env.NF_BANK_TRANSFER_RECIPIENT!,
+        iban: process.env.NF_BANK_TRANSFER_IBAN!,
+        bic: process.env.NF_BANK_TRANSFER_BIC!,
+        referenceCode: selectBankTransferReferenceCode(state),
+        amount: selectBankTransferAmount(state),
+        gasStipend: selectIsBankTransferGasStipend(state),
+      };
+    },
+    dispatchToProps: d => ({
+      onGasStipendChange: () => d(actions.investmentFlow.toggleBankTransferGasStipend()),
+    }),
   }),
-})(BankTransferDetailsComponent);
+  withHandlers<IProps, IHandlerProps>({
+    handleCheckbox: ({ onGasStipendChange }) => (e: any) => {
+      const domEl: HTMLInputElement = e.target;
+      // This needs to be done so that the checkbox appears in the print view!
+      if (domEl.checked) {
+        domEl.setAttribute("checked", "checked");
+      } else {
+        domEl.removeAttribute("checked");
+      }
+      onGasStipendChange();
+    },
+  }),
+)(BankTransferDetailsComponent);
 
 export { BankTransferDetailsComponent, BankTransferDetails };

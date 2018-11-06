6dec82c9ce5ae62062f9c5e667e7541c9164217c
Paweł Lula <pavloblack@hotmail.com>
2018-11-06 17:55:29 +0100

    Change e2e test to use contract id

--- app/test-e2e/eto-view/EtoViewForUnapprovedInvestor.spec.e2e.ts
+++ app/test-e2e/eto-view/EtoViewForUnapprovedInvestor.spec.e2e.ts
@@ -4,13 +4,13 @@ import { withParams } from "../../utils/withParams";
 import { createAndLoginNewUser } from "../utils/userHelpers";
 import { assertEtoView } from "./EtoViewUtils";
 
-const ETO_PPREVIEW_CODE = "4c701f6c-fa03-468c-9ffe-dd9b24f3ab84";
+const ETO_ID = "0xef2260A8e516393F313e0E659b1A3571e98D73eE";
 
 describe("Eto Investor View", () => {
   beforeEach(() => createAndLoginNewUser({ type: "investor" }));
 
   it("should show investment notification when kyc is not done", () => {
-    cy.visit(withParams(appRoutes.etoPublicView, { previewCode: ETO_PPREVIEW_CODE }));
+    cy.visit(withParams(appRoutes.etoPublicViewById, { etoId: ETO_ID }));
     assertEtoView();
 
     cy.get(tid("eto-overview-settings-update-required-to-invest")).should("exist");

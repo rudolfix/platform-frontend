58f96a4092de2878e904b2a638dc04cb2ca95d65
Paweł Lula <pavloblack@hotmail.com>
2018-11-07 19:39:19 +0100

    Update backend sha

--- e2e/setup-e2e.sh
+++ e2e/setup-e2e.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname "$0")"
 
-BACKEND_SHA=73beb84445fa9aa02ee9c612e99b05b50a004fc8
+BACKEND_SHA=0a24fbab36b2bad47e31cb975d8cf9af549bf24d
 
 # we tag images with shorter SHA
 BACKEND_SHORT_SHA=${BACKEND_SHA:0:7}

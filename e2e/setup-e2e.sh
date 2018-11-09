d860962410efca4018d2ca17fbfad833b7390847
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-11-09 12:24:43 +0100

    fixes backend hash

--- e2e/setup-e2e.sh
+++ e2e/setup-e2e.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname "$0")"
 
-BACKEND_SHA=c68a1b266fe2f069ee74a15a73951af9050ddd52
+BACKEND_SHA=6f6e6e3c6cf25b62ae0477eb77f3ea021ada72ab
 
 # we tag images with shorter SHA
 BACKEND_SHORT_SHA=${BACKEND_SHA:0:7}

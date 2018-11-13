ab4cdc995301cf51d771fd1c205292cca50e52db
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-11-13 17:19:23 +0100

    Fixes Issue with Gas API Saga (#1777)
    * Some changes in GasApi
    
    * keeps gasPrice incase of error
    
    * Keeps Gas Price in state even during errors

--- app/modules/gas/sagas.ts
+++ app/modules/gas/sagas.ts
@@ -1,20 +1,11 @@
-import { fork, put, select } from "redux-saga/effects";
+import { fork, put } from "redux-saga/effects";
 import { TGlobalDependencies } from "../../di/setupBindings";
 import { IHttpResponse } from "../../lib/api/client/IHttpClient";
 import { GasModelShape } from "../../lib/api/GasApi";
 import { actions } from "../actions";
 import { neuTakeEvery } from "../sagasUtils";
-import { selectGasPrice, selectIsGasPriceAlreadyLoaded } from "./selectors";
 
 function* ensureGasApiDataSaga({ gasApi, logger }: TGlobalDependencies): any {
-  const isAlreadyLoaded: boolean = yield select(selectIsGasPriceAlreadyLoaded);
-
-  if (isAlreadyLoaded) {
-    const gasPrice: GasModelShape = yield select(selectGasPrice);
-    yield put(actions.gas.gasApiLoaded({ data: gasPrice }));
-    return;
-  }
-
   try {
     const gasValue: IHttpResponse<GasModelShape> = yield gasApi.getGas();
 

a9e1f1c5d33f60723e6fa7634a674377094685ed
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-11-13 15:34:02 +0100

    Migration tool enhancement (#1720)

--- app/modules/icbm-wallet-balance-modal/selectors.ts
+++ app/modules/icbm-wallet-balance-modal/selectors.ts
@@ -1,9 +1,10 @@
 import { addBigNumbers } from "../../utils/BigNumberUtils";
 import { ILockedWallet } from "../wallet/reducer";
-import { IIcbmWalletBalanceModal, IWalletMigrationData } from "./reducer";
+import { IAppState } from "./../../store";
+import { IIcbmWalletBalanceModal, IWalletMigrationData, TWalletMigrationSteps } from "./reducer";
 
 export const selectIcbmWalletEthAddress = (state: IIcbmWalletBalanceModal): string | undefined =>
-  state.ethAddress;
+  state.icbmWalletEthAddress;
 
 export const selectIcbmMigrationWallet = (
   state: IIcbmWalletBalanceModal,
@@ -26,4 +27,16 @@ export const selectIcbmWalletConnectedIcbmModal = (state: IIcbmWalletBalanceModa
 
 export const selectWalletMigrationData = (
   state: IIcbmWalletBalanceModal,
-): IWalletMigrationData | undefined => state.walletMigrationData;
+): IWalletMigrationData[] | undefined => state.walletMigrationData;
+
+export const selectWalletMigrationCurrentStep = (state: IAppState): TWalletMigrationSteps =>
+  state.icbmWalletBalanceModal && state.icbmWalletBalanceModal.currentMigrationStep;
+
+export const selectIcbmModalIsMigrating = (state: IAppState): boolean =>
+  state.icbmWalletBalanceModal && state.icbmWalletBalanceModal.isMigrating;
+
+export const selectIcbmModalIsFirstTransactionDone = (state: IAppState): boolean =>
+  state.icbmWalletBalanceModal && state.icbmWalletBalanceModal.firstTransactionDone;
+
+export const selectIcbmModalIsSecondTransactionDone = (state: IAppState): boolean =>
+  state.icbmWalletBalanceModal && state.icbmWalletBalanceModal.secondTransactionDone;

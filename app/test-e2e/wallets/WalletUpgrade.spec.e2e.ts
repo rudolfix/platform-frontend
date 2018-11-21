98864e9cb4e51d9662105653355e3625a81a3069
Thomas Gorny <thomas.gorny@pm.me>
2018-11-21 17:08:01 +0100

    work on upgrade test

--- app/test-e2e/wallets/WalletUpgrade.spec.e2e.ts
+++ app/test-e2e/wallets/WalletUpgrade.spec.e2e.ts
@@ -1,11 +1,10 @@
 import { INV_EUR_ICBM_HAS_KYC_SEED } from "../constants";
-import { etoFixtureAddressByName } from "../utils";
+import { closeModal, confirmAccessModal, etoFixtureAddressByName } from "../utils";
 import { tid } from "../utils/selectors";
 import { createAndLoginNewUser } from "../utils/userHelpers";
 
 describe("Upgrade icbm wallet", () => {
   it("do", () => {
-    const PUBLIC_ETO_ID = etoFixtureAddressByName("ETOInPublicState");
     createAndLoginNewUser({
       type: "investor",
       kyc: "business",
@@ -14,6 +13,11 @@ describe("Upgrade icbm wallet", () => {
     }).then(() => {
       cy.visit("/wallet");
       cy.get(tid("icbmNeuroWallet.shared-component.upgrade.button")).click();
+      cy.get(tid("modals.tx-sender.withdraw-flow.summery.withdrawSummery.accept")).click();
+      confirmAccessModal();
+      cy.get(tid("modals.shared.signing-message.modal"));
+      cy.get(tid("modals.tx-sender.withdraw-flow.success"));
+      closeModal();
     });
   });
 });

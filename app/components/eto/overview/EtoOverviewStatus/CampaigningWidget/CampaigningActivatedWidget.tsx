bf556a324cab47a4b512dac3fffd0f45f873a7b8
Thomas Gorny <thomas.gorny@pm.me>
2018-11-22 17:14:43 +0100

    add profile update button to bookbuilding if not kyc approved

--- app/components/eto/overview/EtoOverviewStatus/CampaigningWidget/CampaigningActivatedWidget.tsx
+++ app/components/eto/overview/EtoOverviewStatus/CampaigningWidget/CampaigningActivatedWidget.tsx
@@ -16,8 +16,10 @@ import { onEnterAction } from "../../../../../utils/OnEnterAction";
 import { ECurrencySymbol, EMoneyFormat, Money } from "../../../../shared/Money";
 import { CounterWidget } from "../index";
 import { Message } from "../Message";
-import { CampaigningActivatedInvestorWidget } from "./CampaigningActivatedInvestorWidget";
+import { CampaigningActivatedInvestorApprovedWidget } from "./CampaigningActivatedInvestorApprovedWidget";
 
+import { appRoutes } from "../../../../appRoutes";
+import { ButtonLink } from "../../../../shared/buttons";
 import * as styles from "../EtoOverviewStatus.module.scss";
 
 export interface IExternalProps {
@@ -65,38 +67,62 @@ const CampaigningActivatedWidgetComponent: React.SFC<IProps> = ({
 }) => {
   if (isActive && !isInvestorsLimitReached) {
     return (
-      <div className={styles.groupWrapper}>
-        <div className={styles.group}>
-          <span className={styles.label}>
-            <FormattedMessage id="shared-component.eto-overview.amount-backed" />
-          </span>
-          <span className={styles.value} data-test-id="eto-bookbuilding-amount-backed">
-            <Money
-              value={pledgedAmount}
-              currency="eur"
-              format={EMoneyFormat.FLOAT}
-              currencySymbol={ECurrencySymbol.SYMBOL}
-            />
-          </span>
+      <>
+        <div className={styles.groupWrapper}>
+          <div className={styles.group}>
+            <span className={styles.label}>
+              <FormattedMessage id="eto-overview.campaigning.whitelist-status" />
+            </span>
+            <span className={styles.value} data-test-id="eto-bookbuilding-amount-backed">
+              {pledgedAmount ? (
+                <FormattedMessage id="eto-overview.campaigning.whitelist-status.label-subscribed" />
+              ) : (
+                <FormattedMessage id="eto-overview.campaigning.whitelist-status.label-not-subscribed" />
+              )}
+            </span>
+          </div>
+          <div className={styles.group}>
+            <span className={styles.label}>
+              <FormattedMessage id="shared-component.eto-overview.amount-backed" />
+            </span>
+            <span className={styles.value} data-test-id="eto-bookbuilding-amount-backed">
+              <Money
+                value={pledgedAmount}
+                currency="eur"
+                format={EMoneyFormat.FLOAT}
+                currencySymbol={ECurrencySymbol.SYMBOL}
+              />
+            </span>
+          </div>
+          <div className={styles.group}>
+            <span className={styles.label}>
+              <FormattedMessage id="shared-component.eto-overview.investors-backed" />
+            </span>
+            <span className={styles.value} data-test-id="eto-bookbuilding-investors-backed">
+              {investorsCount !== null ? investorsCount : "-"} out of {investorsLimit} whitelisted
+            </span>
+          </div>
+          {isInvestor &&
+            isVerifiedInvestor && (
+              <CampaigningActivatedInvestorApprovedWidget
+                etoId={etoId}
+                minPledge={minPledge}
+                maxPledge={maxPledge}
+                pledge={pledge}
+              />
+            )}
         </div>
-        <div className={styles.group}>
-          <span className={styles.label}>
-            <FormattedMessage id="shared-component.eto-overview.investors-backed" />
-          </span>
-          <span className={styles.value} data-test-id="eto-bookbuilding-investors-backed">
-            {investorsCount !== null ? investorsCount : "-"} out of {investorsLimit} whitelisted
-          </span>
-        </div>
-        {isInvestor && (
-          <CampaigningActivatedInvestorWidget
-            etoId={etoId}
-            minPledge={minPledge}
-            maxPledge={maxPledge}
-            pledge={pledge}
-            isVerifiedInvestor={isVerifiedInvestor}
-          />
-        )}
-      </div>
+        {isInvestor &&
+          !isVerifiedInvestor && (
+            <ButtonLink
+              className="mb-3"
+              to={appRoutes.settings}
+              data-test-id="eto-overview-settings-update-required-to-invest"
+            >
+              <FormattedMessage id="shared-component.eto-overview.settings-update-required" />
+            </ButtonLink>
+          )}
+      </>
     );
   }
 

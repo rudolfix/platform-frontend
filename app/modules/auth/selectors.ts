b9ac024744de6ce50435a128d1ee396502d67790
David <dscharf@gmx.net>
2018-11-27 11:08:06 +0100

    fixes selector naming

--- app/modules/auth/selectors.ts
+++ app/modules/auth/selectors.ts
@@ -51,5 +51,8 @@ export const selectIsLatestAgreementAccepted = (state: IAppState): boolean =>
   !!(
     state.auth.user &&
     state.auth.currentAgreementHash &&
-    state.auth.user.latestAcceptedTosIpfs !== state.auth.currentAgreementHash
+    state.auth.user.latestAcceptedTosIpfs === state.auth.currentAgreementHash
   );
+
+export const selectIsLatestAgreementLoaded = (state: IAppState) =>
+  !!state.auth.currentAgreementHash;

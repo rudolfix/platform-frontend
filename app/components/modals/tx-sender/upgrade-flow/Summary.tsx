ee10dd28c8f6b7c2ed505a0051ae3f3e16af98cb
Moe Elsharif <mohammed.elsharif@gmail.com>
2018-12-07 16:32:37 +0100

    Adds UpgradeSummary Component (#2173)

--- app/components/modals/tx-sender/upgrade-flow/Summary.tsx
+++ app/components/modals/tx-sender/upgrade-flow/Summary.tsx
@@ -9,22 +9,24 @@ import {
 } from "../../../../modules/tx/sender/selectors";
 import { appConnect } from "../../../../store";
 import { Button } from "../../../shared/buttons";
+import { DocumentTemplateButton } from "../../../shared/DocumentLink";
 import { Heading } from "../../../shared/modals/Heading";
 import { Money } from "../../../shared/Money";
 import { InfoList } from "../shared/InfoList";
 import { InfoRow } from "../shared/InfoRow";
 import { ITxSummaryDispatchProps, ITxSummaryStateProps, TSummaryComponentProps } from "../TxSender";
 
-export const WithdrawSummaryComponent: React.SFC<TSummaryComponentProps> = ({
+export const UpgradeSummaryComponent: React.SFC<TSummaryComponentProps> = ({
   txData,
   txCost,
   onAccept,
+  downloadICBMAgreement,
 }) => (
   <Container>
     <Row className="mb-4">
       <Col>
         <Heading>
-          <FormattedMessage id="withdraw-flow.summary" />
+          <FormattedMessage id="upgrade-flow.summary" />
         </Heading>
       </Col>
     </Row>
@@ -32,20 +34,30 @@ export const WithdrawSummaryComponent: React.SFC<TSummaryComponentProps> = ({
     <Row>
       <Col>
         <InfoList>
-          <InfoRow caption={<FormattedMessage id="withdraw-flow.to" />} value={txData.to} />
+          <InfoRow caption={<FormattedMessage id="upgrade-flow.to" />} value={txData.to} />
 
           <InfoRow
-            caption={<FormattedMessage id="withdraw-flow.value" />}
+            caption={<FormattedMessage id="upgrade-flow.value" />}
             value={<Money currency="eth" value={txData.value!} />}
           />
 
           <InfoRow
-            caption={<FormattedMessage id="withdraw-flow.transaction-cost" />}
+            caption={<FormattedMessage id="upgrade-flow.transaction-cost" />}
             value={<Money currency="eth" value={txCost} />}
           />
         </InfoList>
       </Col>
     </Row>
+    {downloadICBMAgreement && (
+      <Row>
+        <Col className="my-3 text-center">
+          <DocumentTemplateButton
+            onClick={() => downloadICBMAgreement()}
+            title={<FormattedMessage id="wallet.icbm.reservation-agreement" />}
+          />
+        </Col>
+      </Row>
+    )}
     <Row>
       <Col className="text-center">
         <Button
@@ -60,12 +72,13 @@ export const WithdrawSummaryComponent: React.SFC<TSummaryComponentProps> = ({
   </Container>
 );
 
-export const WithdrawSummary = appConnect<ITxSummaryStateProps, ITxSummaryDispatchProps>({
+export const UpgradeSummary = appConnect<ITxSummaryStateProps, ITxSummaryDispatchProps>({
   stateToProps: state => ({
     txData: selectTxSummaryData(state)!,
     txCost: selectTxGasCostEthUlps(state),
   }),
   dispatchToProps: d => ({
     onAccept: () => d(actions.txSender.txSenderAccept()),
+    downloadICBMAgreement: () => d(actions.icbmWalletBalanceModal.downloadICBMWalletAgreement()),
   }),
-})(WithdrawSummaryComponent);
+})(UpgradeSummaryComponent);

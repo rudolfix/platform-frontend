402d7bec3b9dd0b2e8cd588f235142e97b673c5a
Paweł Lula <pavloblack@hotmail.com>
2018-12-12 14:08:12 +0100

    Remove webpack-hot-middleware (#2245)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const paths = require("../webpack/paths");
 const devConfig = require("../webpack/webpack.config.dev");
-const webpack = require("webpack");
 
 module.exports = (baseConfig, env, config) => {
   const pathToStyleLoader = path.join(__dirname, "./setup-styles.ts");
@@ -117,20 +116,6 @@ module.exports = (baseConfig, env, config) => {
   ];
 
   config.resolve.extensions = devConfig.resolve.extensions;
-
-  const isMakingScreenshots = process.env.npm_lifecycle_event === "storybook:screenshots";
-  if (isMakingScreenshots) {
-    config.mode = "none";
-    config.devServer = {
-      hot: false,
-    };
-    config.entry.iframe = config.entry.iframe.filter(
-      x => x.indexOf("webpack-hot-middleware") === -1,
-    );
-    config.plugins = config.plugins.filter(
-      plugin => plugin.constructor.name !== "HotModuleReplacementPlugin",
-    );
-  }
 
   return config;
 };

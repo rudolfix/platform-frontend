e90b3c6f7d8b63e43cd733a8dd0741ad2bfe2f1e
Alexey Tuboltsev <tblz@palavara.com>
2018-12-14 16:33:52 +0100

    #2209 add DocumentLayout to storybook (#2246)
    * #2209
    add DocumentLayout to storybook
    fix documentTitle translations

---
+++ app/components/documents/Documents.stories.tsx
@@ -0,0 +1,24 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { etoDocuments, etoFilesData, etoTemplates } from "../../../test/fixtures";
+import { EEtoState } from "../../lib/api/eto/EtoApi.interfaces";
+import { DocumentsLayout } from "./Documents";
+import { getDocumentTitles } from "./utils";
+
+const props = {
+  etoFilesData: etoFilesData,
+  loadingData: false,
+  etoFileLoading: false,
+  etoState: EEtoState.ON_CHAIN,
+  etoTemplates: etoTemplates,
+  etoDocuments: etoDocuments,
+  documentTitles: getDocumentTitles(false),
+  isRetailEto: false,
+  generateTemplate: () => {},
+  downloadDocumentByType: () => {},
+};
+
+storiesOf("ETO/Documents", module).add("default", () => {
+  return <DocumentsLayout {...props} />;
+});

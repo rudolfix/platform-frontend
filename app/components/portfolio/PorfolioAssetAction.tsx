53c4a74b0598997c28a4b13d84c391e47ed8f9aa
Moe Elsharif <mohammed.elsharif@gmail.com>
2019-01-07 15:45:18 +0100

    connects portfolioAssetAction with claim

--- app/components/portfolio/PorfolioAssetAction.tsx
+++ app/components/portfolio/PorfolioAssetAction.tsx
@@ -1,24 +1,32 @@
 import * as React from "react";
 import { FormattedMessage } from "react-intl-phraseapp";
 
+import { actions } from "../../modules/actions";
 import { EETOStateOnChain } from "../../modules/public-etos/types";
-import { Button, EButtonLayout } from "../shared/buttons";
+import { appConnect } from "../../store";
+import { Button, ButtonArrowRight, EButtonLayout } from "../shared/buttons";
 
 type TExternalProps = {
   state: EETOStateOnChain;
   etoId: string;
 };
 
-const PortfolioAssetAction: React.SFC<TExternalProps> = ({ state }) => {
+interface IDispatchProps {
+  onClaim: (etoId: string) => void;
+}
+const PortfolioAssetActionComponent: React.SFC<TExternalProps & IDispatchProps> = ({
+  state,
+  etoId,
+  onClaim,
+}) => {
   switch (state) {
-    // TODO: Connect Claim and Payout with refactored transaction functionality
-    // case ETOStateOnChain.Claim:
-    // case ETOStateOnChain.Payout:
-    //   return (
-    //     <Button onClick={claim} layout="simple" svgIcon={arrowIcon} iconPosition="icon-after">
-    //       <FormattedMessage id="portfolio.section.reserved-assets.claim-tokens" />
-    //     </Button>
-    //   );
+    case EETOStateOnChain.Claim:
+    case EETOStateOnChain.Payout:
+      return (
+        <ButtonArrowRight onClick={() => onClaim(etoId)}>
+          <FormattedMessage id="portfolio.section.reserved-assets.claim-tokens" />
+        </ButtonArrowRight>
+      );
     // case ETOStateOnChain.Refund:
     //   return (
     //     <Button layout="simple" svgIcon={arrowIcon} iconPosition="icon-after">
@@ -36,5 +44,11 @@ const PortfolioAssetAction: React.SFC<TExternalProps> = ({ state }) => {
       return null;
   }
 };
+
+const PortfolioAssetAction = appConnect<{}, IDispatchProps, TExternalProps>({
+  dispatchToProps: dispatch => ({
+    onClaim: (etoId: string) => dispatch(actions.txTransactions.startUserClaim(etoId)),
+  }),
+})(PortfolioAssetActionComponent);
 
 export { PortfolioAssetAction };

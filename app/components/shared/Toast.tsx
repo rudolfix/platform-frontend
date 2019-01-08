e80e1212082caf1611a04b7967a5c8f51e65a740
lexey <tblz@palavara.com>
2019-01-08 13:44:48 +0100

    change Toast typings to accomodate for TMessage

--- app/components/shared/Toast.tsx
+++ app/components/shared/Toast.tsx
@@ -4,14 +4,15 @@ import * as React from "react";
 import { toast, ToastContainer as ToastifyContainer } from "react-toastify";
 
 import { TOAST_COMPONENT_DELAY } from "../../config/constants";
+import { TTranslatedString } from "../../types";
 
 const ToastContainer = () => <ToastifyContainer autoClose={TOAST_COMPONENT_DELAY} />;
 
-const showErrorToast = (content: React.ReactNode) => {
+const showErrorToast = (content: TTranslatedString) => {
   toast.error(content);
 };
 
-const showInfoToast = (content: React.ReactNode) => {
+const showInfoToast = (content: TTranslatedString) => {
   toast.info(content);
 };
 

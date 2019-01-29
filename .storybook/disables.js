108673e1878b0314af78977855acf42fd5bc773d
Wiktor Furman <wiktor.furman@outlook.com>
2019-01-29 20:10:47 +0100

    Merge vis reg disable files to single one (#2373)

--- .storybook/disables.js
+++ .storybook/disables.js
@@ -1,7 +1,17 @@
 (() => {
   const $iframe = document.getElementById("storybook-preview-iframe");
   const $doc = $iframe.contentDocument;
+  const $style = $doc.createElement("style");
 
+  // disable animations
+  $style.innerHTML = `* {
+    transition: none !important;
+    animation: none !important;
+  }`;
+
+  $doc.body.appendChild($style);
+
+  // disable video play
   const videos = $doc.getElementsByTagName("video");
 
   for (video of videos) {

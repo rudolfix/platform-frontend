9cb0034cc8bcdf9ead2ec10cde9b52b9b51235e4
Paweł Lula <pavloblack@hotmail.com>
2019-02-06 11:29:20 +0000

    #1733 Add redistribute summary component

---
+++ app/components/modals/tx-sender/investor-payout/RedistributeSummary.tsx
@@ -0,0 +1,87 @@
+import * as cn from "classnames";
+import * as React from "react";
+import { FormattedMessage } from "react-intl-phraseapp";
+import { Container } from "reactstrap";
+
+import { externalRoutes } from "../../../../config/externalRoutes";
+import { actions } from "../../../../modules/actions";
+import { ITokenDisbursal } from "../../../../modules/investor-portfolio/types";
+import { selectTxSummaryAdditionalData } from "../../../../modules/tx/sender/selectors";
+import { selectEthereumAddressWithChecksum } from "../../../../modules/web3/selectors";
+import { appConnect } from "../../../../store";
+import { EthereumAddressWithChecksum } from "../../../../types";
+import { withParams } from "../../../../utils/withParams";
+import { Button } from "../../../shared/buttons";
+import { Heading } from "../../../shared/modals/Heading";
+import { Money } from "../../../shared/Money";
+import { InfoList } from "../shared/InfoList";
+import { InfoRow } from "../shared/InfoRow";
+
+import * as styles from "./RedistributeSummary.module.scss";
+
+interface IStateProps {
+  tokensDisbursal: ITokenDisbursal[];
+  walletAddress: EthereumAddressWithChecksum;
+}
+
+interface IDispatchProps {
+  onAccept: () => any;
+}
+
+type TComponentProps = IStateProps & IDispatchProps;
+
+const InvestorRedistributePayoutSummaryLayout: React.FunctionComponent<TComponentProps> = ({
+  walletAddress,
+  tokensDisbursal,
+  onAccept,
+}) => {
+  return (
+    <Container>
+      <Heading className="mb-4">
+        <FormattedMessage id="investor-payout.redistribute.summary.title" />
+      </Heading>
+
+      <p className="mb-3">
+        <FormattedMessage id="investor-payout.redistribute.summary.description" />
+      </p>
+      <InfoList className="mb-4">
+        {tokensDisbursal.map(disbursal => (
+          <InfoRow
+            key={disbursal.token}
+            caption={
+              <FormattedMessage id="investor-payout.redistribute.summary.total-redistributed" />
+            }
+            value={<Money value={disbursal.amountToBeClaimed} currency={disbursal.token} />}
+          />
+        ))}
+      </InfoList>
+      <section className="text-center">
+        <a
+          className="d-inline-block mb-3"
+          href={withParams(externalRoutes.commitmentStatus, { walletAddress })}
+        >
+          <FormattedMessage id="investor-payout.summary.neu-tokenholder-agreement" />
+        </a>
+        <p className={cn(styles.hint)}>
+          To process this payout we will need to trigger and ethereum transaction which will incure
+          gas costs. This will be deducted from your ETH wallet.
+        </p>
+        <Button onClick={onAccept}>
+          <FormattedMessage id="investor-payout.redistribute.summary.accept" />
+        </Button>
+      </section>
+    </Container>
+  );
+};
+
+const InvestorRedistributePayoutSummary = appConnect<IStateProps, IDispatchProps, {}>({
+  stateToProps: state => ({
+    walletAddress: selectEthereumAddressWithChecksum(state),
+    tokensDisbursal: selectTxSummaryAdditionalData(state),
+  }),
+  dispatchToProps: d => ({
+    onAccept: () => d(actions.txSender.txSenderAccept()),
+  }),
+})(InvestorRedistributePayoutSummaryLayout);
+
+export { InvestorRedistributePayoutSummary, InvestorRedistributePayoutSummaryLayout };

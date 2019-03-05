7bec361850876995683e3474b2d265b0074734cc
Wiktor Furman <wiktor.furman@outlook.com>
2019-03-05 08:40:17 +0100

    #974 Logout different user on email verification (#2542)

--- app/modules/auth/actions.ts
+++ app/modules/auth/actions.ts
@@ -4,6 +4,7 @@ import { createAction, createSimpleAction } from "../actionsUtils";
 export const authActions = {
   loadJWT: (jwt: string) => createAction("AUTH_LOAD_JWT", { jwt }),
   setUser: (user: IUser) => createAction("AUTH_SET_USER", { user }),
-  logout: (userType?: EUserType) => createAction("AUTH_LOGOUT", { userType }),
+  logout: (userType?: EUserType, disableRedirect?: boolean) =>
+    createAction("AUTH_LOGOUT", { userType, disableRedirect }),
   verifyEmail: () => createSimpleAction("AUTH_VERIFY_EMAIL"),
 };

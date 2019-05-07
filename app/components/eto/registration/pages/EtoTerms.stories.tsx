48e3948b6935ba8687e2aea193abdec7524cd903
Alexey Tuboltsev <tblz@palavara.com>
2019-05-07 23:02:14 +0200

    #2866 - fix form justification  (#2867)
    * fix panel justification issue in forms
    adjust storybook tests to show the justification of form in panel
    
    * Reorders tests
    
    * reduces circlce test machines to 3

--- app/components/eto/registration/pages/EtoTerms.stories.tsx
+++ app/components/eto/registration/pages/EtoTerms.stories.tsx
@@ -6,23 +6,26 @@ import * as React from "react";
 import { testEto, testProduct } from "../../../../../test/fixtures";
 import { TEtoProducts } from "../../../../lib/api/eto/EtoProductsApi.interfaces";
 import { noopLogger } from "../../../../lib/dependencies/logger";
+import { Panel } from "../../../shared/Panel";
 import { EtoRegistrationTermsLayout } from "./EtoTerms";
 
 const availableProducts: TEtoProducts = [testProduct];
 
-storiesOf("EtoTerms", module).add("default", () => (
-  <Formik initialValues={testEto} onSubmit={action("onSubmit")}>
-    {props => (
-      <EtoRegistrationTermsLayout
-        availableProducts={availableProducts}
-        changeProductType={action("changeProductType")}
-        eto={testEto}
-        savingData={false}
-        readonly={false}
-        saveData={action("saveData")}
-        logger={noopLogger}
-        {...props}
-      />
-    )}
-  </Formik>
-));
+storiesOf("EtoTerms", module)
+  .addDecorator(story => <Panel>{story()}</Panel>)
+  .add("default", () => (
+    <Formik initialValues={testEto} onSubmit={action("onSubmit")}>
+      {props => (
+        <EtoRegistrationTermsLayout
+          availableProducts={availableProducts}
+          changeProductType={action("changeProductType")}
+          eto={testEto}
+          savingData={false}
+          readonly={false}
+          saveData={action("saveData")}
+          logger={noopLogger}
+          {...props}
+        />
+      )}
+    </Formik>
+  ));

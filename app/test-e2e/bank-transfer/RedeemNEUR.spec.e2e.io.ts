9eed0a1eaf47ee9d8f88c1a9a7666c20faf31740
Moe Elsharif <mohammed.elsharif@gmail.com>
2019-06-06 11:49:26 +0200

    Adds retry to nRedeem (#3088)

--- app/test-e2e/bank-transfer/RedeemNEUR.spec.e2e.io.ts
+++ app/test-e2e/bank-transfer/RedeemNEUR.spec.e2e.io.ts
@@ -11,7 +11,8 @@ import { formField, tid } from "../utils/selectors";
 import { loginFixtureAccount } from "../utils/userHelpers";
 import { assertBankAccountDetails } from "./assertions";
 
-describe("Redeem NEUR", () => {
+describe("Redeem NEUR", function(): void {
+  this.retries(2);
   beforeEach(() => {
     loginFixtureAccount("INV_HAS_EUR_HAS_KYC", {
       kyc: "business",

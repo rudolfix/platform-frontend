011fb39aad2bb7fa4fc3e848d621ced88d7598a6
rudolfix <marcin.rudolf@neufund.org>
2019-08-15 12:02:50 +0200

    removes memorandum template from hidden autogenerated templates (#3318)

--- packages/web/app/lib/api/eto/EtoFileUtils.ts
+++ packages/web/app/lib/api/eto/EtoFileUtils.ts
@@ -1,9 +1,5 @@
 // Templates to not be presented
-export const ignoredTemplates: string[] = [
-  "pamphletTemplate",
-  "prospectusTemplate",
-  "investmentMemorandumTemplate",
-];
+export const ignoredTemplates: string[] = ["pamphletTemplate", "prospectusTemplate"];
 // Termsheet template was enabled
 // @see https://github.com/Neufund/platform-frontend/issues/2744
 // Documents to not be publicly presented

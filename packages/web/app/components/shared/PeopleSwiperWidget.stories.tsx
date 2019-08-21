809e9a7fc3a0b335a0b4bf5d05f25da427138255
Paweł Lula <pavloblack@hotmail.com>
2019-08-21 16:43:17 +0300

    Fix eto layout issue on mobile (#3343)

--- packages/web/app/components/shared/PeopleSwiperWidget.stories.tsx
+++ packages/web/app/components/shared/PeopleSwiperWidget.stories.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import Container from "reactstrap/lib/Container";
 
 import { aga, marcin, panor, sergiej, ula, zoe } from "../../../test/fixtures/fixturesPersons";
-import { PeopleSwiperWidgeLayout } from "./PeopleSwiperWidget.unsafe";
+import { PeopleSwiperWidgeLayout } from "./PeopleSwiperWidget";
 
 storiesOf("PeopleSwiperWidget", module)
   .add("2 persons", () => {

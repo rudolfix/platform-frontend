a9f35533806c7aef57afe67a311224e2d97760fc
Moe Elsharif <mohammed.elsharif@gmail.com>
2019-09-18 15:14:17 +0200

    Reduces Broadcast Channel Protocols (#3466)
    * Reduces Broadcast Channel Protocols
    
    * uses "native" protocol for tests that use electron
    
    * cancels only idb

--- packages/web/app/lib/dependencies/broadcast-channel/broadcastChannel.ts
+++ packages/web/app/lib/dependencies/broadcast-channel/broadcastChannel.ts
@@ -1,6 +1,10 @@
 import BroadcastChannel from "broadcast-channel";
 
 export const createNewBroadcastChannel = <T>(key: string): BroadcastChannel<T> =>
-  new BroadcastChannel(key);
+  // Currently we will only force localstorage until issue 29 is solved
+  //@SEE https://github.com/pubkey/broadcast-channel/issues/29
+  new BroadcastChannel(key, {
+    webWorkerSupport: false,
+  });
 
 export type BroadcastChannel<T> = BroadcastChannel<T>;

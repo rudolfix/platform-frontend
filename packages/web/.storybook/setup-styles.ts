abe34e77256d33842828b8edbe07f7f844bf2f19
Paweł Lula <pavloblack@hotmail.com>
2019-11-21 11:00:54 +0100

    Load custom fonts properly (#3765)

--- packages/web/.storybook/setup-styles.ts
+++ packages/web/.storybook/setup-styles.ts
@@ -1,9 +1,2 @@
-import "font-awesome/scss/font-awesome.scss";
-import "react-select/dist/react-select.css";
-import "react-virtualized-select/styles.css";
-import "react-virtualized/styles.css";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
-import "../app/styles/bootstrap.scss";
-import "../app/styles/overrides.scss";
+import "../app/index.scss";
 import "./storybook-overrides.scss";

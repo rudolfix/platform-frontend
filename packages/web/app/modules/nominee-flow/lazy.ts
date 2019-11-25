192f1318beb7e99df1aa7e09d80b677d2bbf4458
lexey <tblz@palavara.com>
2019-11-25 14:19:07 +0100

    add a conditional lazy-laoding/import of 'ipfs-only-hash'

--- packages/web/app/modules/nominee-flow/lazy.ts
+++ packages/web/app/modules/nominee-flow/lazy.ts
@@ -1,2 +1,15 @@
-export const generateHash = (buffer: Buffer): Promise<string> =>
-  import("ipfs-only-hash").then((Hash: IpfsHasher) => Hash.of(buffer));
+export const lazyLoadIpfsOnlyHash = (): Promise<any> => {
+  return new Promise((res, reject) => {
+    let importedModule;
+    try {
+      if (process.env.NF_CYPRESS_RUN === "0") {
+        importedModule = import(/* webpackChunkName: "ipfs-only-hash" */"ipfs-only-hash");
+      } else {
+        importedModule = require("ipfs-only-hash")
+      }
+      res(importedModule)
+    } catch(e) {
+      reject(`could not load module: "ipfs-only-hash". Error: ${e}`)
+    }
+  })
+};

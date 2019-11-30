9af78af3f60487b44cf7177d67b7b26044cddca9
Moe Elsharif <mohammed.elsharif@gmail.com>
2019-11-30 11:40:45 +0100

    Optimizes EtoLinkStep (#3805)
    * changes chrome script
    
    * Initial change of EtoLinkNomineeStep
    
    * updates the cache
    
    * linter
    
    * Review fixes
    Adds retry to flaky test
    Removes Cypress from Package
    
    * removes chrome

--- .circleci/install-chrome.sh
+++ .circleci/install-chrome.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 source ~/.bashrc
 
 echo "Installing Chrome Browser"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt update && sudo apt install google-chrome-stable
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt update && sudo google-chrome-stable_current_amd64.deb
+

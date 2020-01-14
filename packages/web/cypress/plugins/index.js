53072dd107c2b9d72e579239572a1a47d54abf53
Drakkein <wiktor@furman.me>
2020-01-14 18:26:48 +0100

    Show only errors

--- packages/web/cypress/plugins/index.js
+++ packages/web/cypress/plugins/index.js
@@ -8,5 +8,13 @@ module.exports = on => {
     webpackOptions: require("../../webpack/webpack.config.cypress"),
   };
   on("file:preprocessor", webpack(options));
-  require("cypress-log-to-output").install(on);
+
+  require("cypress-log-to-output").install(on, (type, event) => {
+    // log only errors
+    if (event.level === "error" || event.type === "error") {
+      return true;
+    }
+
+    return false;
+  });
 };

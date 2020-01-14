e9727a5421cb0feaba6ba4b518a033b54bf786ac
Drakkein <wiktor@furman.me>
2020-01-14 18:37:03 +0100

    Show only errors

--- packages/web/cypress/plugins/index.js
+++ packages/web/cypress/plugins/index.js
@@ -4,7 +4,10 @@ const webpack = require("@cypress/webpack-preprocessor");
 require("dotenv").config();
 
 module.exports = on => {
-  require("cypress-log-to-output").install(on);
+  require("cypress-log-to-output").install(on, (type, event) => {
+    // log only errors
+    return event.level === "error" || event.type === "error";
+  });
 
   const options = {
     webpackOptions: require("../../webpack/webpack.config.cypress"),

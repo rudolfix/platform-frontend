ceaeb31aa792677c4588ebf968e4004401d5fc19
Drakkein <wiktor@furman.me>
2020-01-15 11:33:44 +0100

    Update chrome install script

--- .circleci/install-chrome.sh
+++ .circleci/install-chrome.sh
@@ -5,6 +5,10 @@ cd "$(dirname "$0")"
 source ~/.bashrc
 
 echo "Installing Chrome Browser"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt update && sudo google-chrome-stable_current_amd64.deb
+curl --silent --show-error --location --fail --retry 3 --output /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
+    && (sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb || sudo apt-get -fy install)  \
+    && rm -rf /tmp/google-chrome-stable_current_amd64.deb \
+    && sudo sed -i 's|HERE/chrome"|HERE/chrome" --disable-setuid-sandbox --no-sandbox|g' \
+        "/opt/google/chrome/google-chrome" \
+    && google-chrome --version
 

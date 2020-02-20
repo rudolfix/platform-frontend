d1a61e6373f6c73cff61f5637dad513cef9410e0
lexey <tblz@palavara.com>
2020-02-20 13:04:40 +0100

    refactor wallet selector layout

--- packages/web/app/components/wallet-selector/WalletSelectorLayout.tsx
+++ packages/web/app/components/wallet-selector/WalletSelectorLayout.tsx
@@ -1,188 +1,50 @@
-import { ButtonInline } from "@neufund/design-system";
-import * as cn from "classnames";
+import { Location } from "history";
 import * as React from "react";
-import { FormattedMessage } from "react-intl-phraseapp";
-import { StaticContext } from "react-router";
-import { Link, RouteComponentProps } from "react-router-dom";
-import { Col, Row } from "reactstrap";
+import { FormattedHTMLMessage, FormattedMessage } from "react-intl-phraseapp";
 
-import { externalRoutes } from "../../config/externalRoutes";
-import { EUserType } from "../../lib/api/users/interfaces";
 import { ELogoutReason } from "../../modules/auth/types";
-import { ENotificationType } from "../../modules/notifications/types";
-import { TLoginRouterState } from "../../modules/routing/types";
-import { appRoutes } from "../appRoutes";
-import { ButtonLink } from "../shared/buttons";
-import { ExternalLink } from "../shared/links";
-import { Notification } from "../shared/notification-widget/Notification";
 import { WalletRouter } from "./WalletRouter";
-import { WalletSelectorContainer } from "./WalletSelectorContainer";
 
 import * as styles from "./WalletSelectorLayout.module.scss";
-
-type TRouteLoginProps = RouteComponentProps<unknown, StaticContext, TLoginRouterState>;
 
 interface IExternalProps {
   rootPath: string;
   isLoginRoute: boolean;
-  oppositeRoute: string;
-  userType: string;
-  openICBMModal: () => void;
-  isSecretProtected: boolean;
-  logoutReason: ELogoutReason | undefined;
-  hideLogoutReason: () => Partial<{ logoutReason: ELogoutReason | undefined }> | undefined;
-}
-
-interface ISelectTitleProps {
-  isLoginRoute: boolean;
   walletSelectionDisabled: boolean;
+  openICBMModal: () => void;
+  logoutReason: ELogoutReason | undefined;
+  redirectLocation: Location;
 }
 
-const SelectTitle: React.FunctionComponent<ISelectTitleProps> = ({
+export const WalletSelectorLayout: React.FunctionComponent<IExternalProps> = ({
+  rootPath,
+  redirectLocation,
   isLoginRoute,
   walletSelectionDisabled,
-}) => {
-  if (walletSelectionDisabled) {
-    return <FormattedMessage id="wallet-selector.tabs.register.title.no-wallet-selection" />;
-  } else if (isLoginRoute) {
-    return <FormattedMessage id="wallet-selector.tabs.login.title" />;
-  } else {
-    return <FormattedMessage id="wallet-selector.tabs.register.title" />;
-  }
-};
-
-export const WalletSelectorLayout: React.FunctionComponent<IExternalProps & TRouteLoginProps> = ({
-  rootPath,
-  isLoginRoute,
-  oppositeRoute,
-  userType,
-  openICBMModal,
-  isSecretProtected,
   logoutReason,
-  hideLogoutReason,
-  location,
-}) => {
-  const walletSelectionDisabled =
-    userType === EUserType.NOMINEE ||
-    (userType === EUserType.ISSUER && process.env.NF_ISSUERS_CAN_LOGIN_WITH_ANY_WALLET !== "1");
-
-  return (
-    <WalletSelectorContainer data-test-id="wallet-selector">
-      {logoutReason === ELogoutReason.SESSION_TIMEOUT && (
-        <Notification
+}) => (
+  <>
+    {logoutReason === ELogoutReason.SESSION_TIMEOUT && (
+        <div
           data-test-id="wallet-selector-session-timeout-notification"
-          text={<FormattedMessage id="notifications.auth-session-timeout" />}
-          type={ENotificationType.WARNING}
-          onClick={hideLogoutReason}
-        />
+          className={styles.logoutNotification}
+        >
+          <FormattedHTMLMessage tagName="span" id="notifications.auth-session-timeout" />
+        </div>
       )}
-      <Row>
-        <Col tag="section" md={{ size: 10, offset: 1 }} lg={{ size: 8, offset: 2 }}>
-          <h1 className={cn(styles.walletChooserTitle, "my-4", "text-center")}>
-            <SelectTitle
-              walletSelectionDisabled={walletSelectionDisabled}
-              isLoginRoute={isLoginRoute}
-            />
-          </h1>
-
-          {!walletSelectionDisabled && (
-            <div className={styles.walletChooserButtons}>
-              <div className="m-2">
-                <ButtonLink data-test-id="wallet-selector-light" to={`${rootPath}/light`}>
-                  {isLoginRoute ? (
-                    <FormattedMessage id="wallet-selector.tabs.neuwallet-login" />
-                  ) : (
-                    <FormattedMessage id="wallet-selector.tabs.neuwallet-register" />
-                  )}
-                </ButtonLink>
-              </div>
-
-              <div className="m-2">
-                <ButtonLink data-test-id="wallet-selector-browser" to={`${rootPath}/browser`}>
-                  {isLoginRoute ? (
-                    <FormattedMessage id="wallet-selector.tabs.browser-wallet-login" />
-                  ) : (
-                    <FormattedMessage id="wallet-selector.tabs.browser-wallet-register" />
-                  )}
-                </ButtonLink>
-              </div>
-
-              <div className="m-2">
-                <ButtonLink data-test-id="wallet-selector-ledger" to={`${rootPath}/ledger`}>
-                  {isLoginRoute ? (
-                    <FormattedMessage id="wallet-selector.tabs.ledger-login" />
-                  ) : (
-                    <FormattedMessage id="wallet-selector.tabs.ledger-register" />
-                  )}
-                </ButtonLink>
-              </div>
-            </div>
-          )}
-
-          {userType === EUserType.INVESTOR && (
-            <p className="text-center mt-4">
-              <FormattedMessage
-                id="wallet-selector.tabs.icbm-help-text"
-                values={{
-                  here: (
-                    <ButtonInline onClick={openICBMModal}>
-                      <strong>
-                        <FormattedMessage id="wallet-selector.tabs.icbm-help-text.here" />
-                      </strong>
-                    </ButtonInline>
-                  ),
-                }}
-              />
-            </p>
-          )}
-        </Col>
-      </Row>
-
-      <section className="mt-4">
-        <WalletRouter rootPath={rootPath} locationState={location.state} />
-      </section>
-
-      <Row className="mt-5">
-        <Col sm={12} md={6} className="text-center text-md-left">
-          {isLoginRoute ? (
-            <>
-              <FormattedMessage id="wallet-selector.login.help-link" />{" "}
-              <Link to={appRoutes.restore}>
-                <strong>
-                  <FormattedMessage id="wallet-selector.help-link.login.label" />
-                </strong>
-              </Link>
-            </>
-          ) : (
-            <>
-              <FormattedMessage id="wallet-selector.register.help-link" />{" "}
-              <ExternalLink href={externalRoutes.neufundSupportHome}>
-                <strong>
-                  <FormattedMessage id="wallet-selector.help-link.register.label" />
-                </strong>
-              </ExternalLink>
-            </>
-          )}
-        </Col>
-        {!isSecretProtected && (
-          <Col sm={12} md={6} className="text-center text-md-right mt-3 mt-md-0">
-            {isLoginRoute ? (
-              <FormattedMessage id="wallet-selector.neuwallet.register-link-text" />
-            ) : (
-              <FormattedMessage id="wallet-selector.neuwallet.login-link-text" />
-            )}{" "}
-            <Link to={oppositeRoute} data-test-id="wallet-selector-opposite-route-link">
-              <strong>
-                {isLoginRoute ? (
-                  <FormattedMessage id="wallet-selector.register" />
-                ) : (
-                  <FormattedMessage id="wallet-selector.login" />
-                )}
-              </strong>
-            </Link>
-          </Col>
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>
+        {isLoginRoute ? (
+          <FormattedMessage id="wallet-selector.log-in" />
+        ) : (
+          <FormattedMessage id="wallet-selector.sign-up" />
         )}
-      </Row>
-    </WalletSelectorContainer>
-  );
-};
+      </h1>
+      <WalletRouter
+        rootPath={rootPath}
+        redirectLocation={redirectLocation}
+        walletSelectionDisabled={walletSelectionDisabled}
+      />
+    </div>
+  </>
+);

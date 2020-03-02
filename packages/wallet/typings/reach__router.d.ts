81f9f2a309456c98aea2daecc2a51905d21a2e21
Paweł Lula <pavloblack@hotmail.com>
2020-03-02 19:16:21 +0800

    #4069 connect mobile app with CI (#4105)
    * #4069 Fix tsc for app
    
    * #4069 Add tsc check for e2e and unit tests
    
    * #4069 Add basic wallet job to circle ci config
    
    * #4069 Update jobs names to match circleci regex
    
    * #4069 Add android e2e tests
    
    * #4069 Fix typo in circle yml
    
    * #4069 Install packages before spinning e2e tests
    
    * #4069 Install packages before spinning e2e tests
    
    * #4069 Update android API
    
    * #4069 Rename jobs to include project
    
    * #4069 Add small comment
    
    * #4069 Remove unused job
    
    * #4069 Update hash

---
+++ packages/wallet/typings/reach__router.d.ts
@@ -0,0 +1,7 @@
+import "@reach/router";
+
+declare module "@reach/router" {
+  // Given that we don't have in global `Window` object we should mock it to any
+  // for storybook to work properly
+  type Window = any;
+}

d7766cd498e28f63e5386c2660f34f8163f985fa
DmitriyNoa <dmytro.zharkov@gmail.com>
2020-03-10 20:38:39 +0100

    Add device info comments

--- packages/wallet/app/modules/device-information/DeviceInformation.ts
+++ packages/wallet/app/modules/device-information/DeviceInformation.ts
@@ -4,14 +4,31 @@ import { injectable } from "inversify";
 
 @injectable()
 export class DeviceInformation {
+
+  /**
+   * Get unique device ID
+   * On Android devices that have multiple users, each user appears as a completely separate device, so the ANDROID_ID value is unique to each user.
+   * On iOS it uses the DeviceUID uid identifier.
+   * iOS: "FCDBD8EF-62FC-4ECB-B2F5-92C9E79AC7F9"
+   * Android: "dd96dec43fb81c97"
+   * @returns {string} Unique device identifier
+   */
   getUniqueId(): string {
     return getUniqueId();
   }
 
+  /**
+   * Checks if app is running on emulator
+   * @returns {Promise<boolean>} if emulator
+   */
   async isEmulator(): Promise<boolean> {
     return isEmulator();
   }
 
+  /**
+   * Returns device platform
+   * @returns {string} ios or android
+   */
   getPlatform(): string {
     return Platform.OS;
   }

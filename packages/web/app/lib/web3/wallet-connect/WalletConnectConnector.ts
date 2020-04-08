12c77d43d464da6388384d5bee364d45a42d34b5
AlexeyTuboltsev <tblz@palavara.com>
2020-04-08 13:13:58 +0200

    fix logout

--- packages/web/app/lib/web3/wallet-connect/WalletConnectConnector.ts
+++ packages/web/app/lib/web3/wallet-connect/WalletConnectConnector.ts
@@ -41,6 +41,7 @@ export enum EWalletConnectEventTypes {
   SESSION_REQUEST_TIMEOUT = "sessionRequestTimeout",
 }
 
+
 @injectable()
 export class WalletConnectConnector extends EventEmitter {
   private web3?: Web3;

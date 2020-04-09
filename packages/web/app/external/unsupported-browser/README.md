eb800c3043aca8f5153bf5457b329ae20134e557
Arjun Umesha <arjunu@users.noreply.github.com>
2020-04-09 12:47:52 +0200

    prefix & minify

---
+++ packages/web/app/external/unsupported-browser/README.md
@@ -0,0 +1,4 @@
+## Browser Support Check
+
+`check-browser-support.min.js` and `unsupported-browser-styles.min.css` are manually minified versions of their source files and the CSS is prefixed using `Autoprefixer` online.
+These files have been committed because they aren't regenerated in the build step because we don't expect them to change often.
--- packages/web/app/external/unsupported-browser/check-browser-support.js
+++ packages/web/app/external/unsupported-browser/check-browser-support.js
@@ -1,15 +1,17 @@
-function isBrowserSupported() {
-  for (var feature in Modernizr) {
-    var test = Modernizr[feature];
-    // we don't want cssgridlegacy but it's included with cssgrid so ignore it
-    if (typeof test === "boolean" && test === false && feature !== "cssgridlegacy") {
-      return false;
+(function() {
+  function isBrowserSupported() {
+    for (var feature in Modernizr) {
+      var test = Modernizr[feature];
+      // we don't want cssgridlegacy but it's included with cssgrid so ignore it
+      if (typeof test === "boolean" && test === false && feature !== "cssgridlegacy") {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
-}
 
-if (!isBrowserSupported()) {
-  document.getElementById("app").style.display = "none";
-  document.getElementById("unsupported-browser").style.display = "flex";
-}
+  if (!isBrowserSupported()) {
+    document.getElementById("app").style.display = "none";
+    document.getElementById("unsupported-browser").style.display = "flex";
+  }
+})();

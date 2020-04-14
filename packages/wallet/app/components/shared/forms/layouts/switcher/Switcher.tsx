d9f221aa570b73bcc3ad08ac56d581bbdd9f9ffe
Paweł Lula <pavloblack@hotmail.com>
2020-04-14 15:45:14 +0200

    #4226 Fix switcher border radius shadow on android

--- packages/wallet/app/components/shared/forms/layouts/switcher/Switcher.tsx
+++ packages/wallet/app/components/shared/forms/layouts/switcher/Switcher.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View, Platform } from "react-native";
 
 import { roundness, shadowStyles } from "../../../../../styles/common";
 import { SwitcherItem } from "./SwitcherItem";
@@ -20,6 +20,10 @@ type TExternalProps = {
   onChangeItem: (itemId: string) => void;
 } & TViewProps;
 
+/**
+ * A switcher that aligns with our design system.
+ * @note `disabled` and `invalid` modes not yet implemented
+ */
 const Switcher = React.forwardRef<{}, TExternalProps>(
   ({ selectedItemId, items, onChangeItem, style, ...props }, ref) => {
     // TODO: Expose consistent `ref`s for all inputs ('focus', 'blur', etc)
@@ -42,13 +46,29 @@ const Switcher = React.forwardRef<{}, TExternalProps>(
   },
 );
 
-const styles = StyleSheet.create({
-  container: {
-    ...shadowStyles.s2,
-  },
-  list: {
-    borderRadius: roundness,
-  },
-});
+const styles = StyleSheet.create(
+  // given that shadow is handled differently on android
+  // we also need to apply border-radius in a different
+  // otherwise corner shadow is cut by the item background
+  Platform.select({
+    default: {
+      container: {
+        ...shadowStyles.s2,
+      },
+      list: {
+        borderRadius: roundness,
+      },
+    },
+    android: {
+      container: {
+        ...shadowStyles.s2,
+
+        overflow: "hidden",
+        borderRadius: roundness,
+      },
+      list: {},
+    },
+  }),
+);
 
 export { Switcher };

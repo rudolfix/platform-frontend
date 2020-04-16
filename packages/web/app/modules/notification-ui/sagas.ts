6484f5777290cb791d37ce047dd9c1e1936ecfba
Arjun Umesha <arjunu@users.noreply.github.com>
2020-04-16 14:02:14 +0200

    include show toast methods

--- packages/web/app/modules/notification-ui/sagas.ts
+++ packages/web/app/modules/notification-ui/sagas.ts
@@ -1,17 +1,25 @@
 import { fork, neuTakeLatest, TActionFromCreator } from "@neufund/sagas";
 import { notificationUIActions } from "@neufund/shared-modules";
+import { showErrorToast, showInfoToast } from "../../components/shared/Toast";
+import { getMessageTranslation } from "../../components/translatedMessages/messages";
 
 import { TGlobalDependencies } from "../../di/setupBindings";
 
 function* showInfo(
   _: TGlobalDependencies,
   action: TActionFromCreator<typeof notificationUIActions, typeof notificationUIActions.showInfo>,
-): Generator<unknown, void> {}
+): Generator<unknown, void> {
+  const {message, options} = action.payload;
+  showInfoToast(getMessageTranslation(message), options);
+}
 
 function* showError(
   _: TGlobalDependencies,
   action: TActionFromCreator<typeof notificationUIActions, typeof notificationUIActions.showError>,
-): Generator<unknown, void> {}
+): Generator<unknown, void> {
+  const {message, options} = action.payload;
+  showErrorToast(getMessageTranslation(message), options);
+}
 
 export function* notificationUISaga(): Generator<unknown, void> {
   yield fork(neuTakeLatest, notificationUIActions.showInfo, showInfo);

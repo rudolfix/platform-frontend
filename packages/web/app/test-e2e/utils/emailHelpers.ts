7ab651800832158b34a1f6ee8e8000717f26f84e
AlexeyTuboltsev <tblz@palavara.com>
2020-04-20 10:30:45 +0200

    Change email template to fix e2e test

--- packages/web/app/test-e2e/utils/emailHelpers.ts
+++ packages/web/app/test-e2e/utils/emailHelpers.ts
@@ -12,6 +12,7 @@ export const getLatestVerifyUserEmailLink = (
     .then(r => {
       const latestEmailByUser = getLatestEmailByUser(r, email);
       const activationLink = get(latestEmailByUser, "template_vars.activation_link");
+
       if (activationLink) {
         // we need to replace the loginlink pointing to a remote destination
         // with one pointing to our local instance
@@ -28,13 +29,15 @@ export const assertWaitForLatestEmailSentWithSalt = (
   timeout: number = 20000,
 ) => {
   expect(timeout, `Email not received in ${timeout} ms`).to.be.gt(0);
+
   cy.wait(1000);
+
   cy.request({
     url: MOCK_API_URL + `sendgrid/session/mails?to=${userEmail}`,
     method: "GET",
   }).then(r => {
     if (r.status === 200 && getLatestEmailByUser(r, userEmail)) {
-      const loginLink = get(getLatestEmailByUser(r, userEmail), "template_vars.login_link");
+      const loginLink = get(getLatestEmailByUser(r, userEmail), "template_vars.activation_link");
       expect(loginLink).to.contain("salt");
       return;
     }

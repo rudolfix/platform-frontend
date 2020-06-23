528fc903385a27f9e52dc30ecfb5678e02e424db
Alexey Tuboltsev <tblz@palavara.com>
2020-06-23 16:39:06 +0200

    #4439 new landing page (#4445)

--- packages/web/app/components/landing/Landing.tsx
+++ packages/web/app/components/landing/Landing.tsx
@@ -1,6 +1,5 @@
-import { ButtonGroup, EButtonLayout, EButtonSize, EIconPosition } from "@neufund/design-system";
+import { EButtonLayout, EButtonSize } from "@neufund/design-system";
 import { withContainer } from "@neufund/shared-utils";
-import * as cn from "classnames";
 import * as React from "react";
 import { FormattedMessage } from "react-intl-phraseapp";
 import { compose, withProps } from "recompose";
@@ -17,74 +16,55 @@ import { ButtonLink } from "../shared/buttons/ButtonLink";
 import { createErrorBoundary } from "../shared/errorBoundary/ErrorBoundary.unsafe";
 import { ErrorBoundaryLayout } from "../shared/errorBoundary/ErrorBoundaryLayout";
 
-import arrowRight from "../../assets/img/inline_icons/arrow_right.svg";
+import fish from "../../assets/img/neufund_landing_visual.png";
 import * as styles from "./Landing.module.scss";
 
 const LandingLayout: React.FunctionComponent = () => (
-  <>
-    <section className={styles.landing} data-test-id="landing-page">
-      <Content>
-        <div className={styles.neon} />
-
-        <WidgetGrid>
-          <Container columnSpan={EColumnSpan.ONE_AND_HALF_COL} type={EContainerType.GRID}>
-            <h1 className={styles.header}>
-              <FormattedMessage id="platform.landing.featured.header" />
-            </h1>
-            <p className={styles.description}>
-              <FormattedMessage id="platform.landing.featured.description" />
-            </p>
-          </Container>
-
-          <Container
-            className={styles.featuresContainer}
-            columnSpan={EColumnSpan.ONE_AND_HALF_COL}
-            type={EContainerType.GRID}
-          >
-            <ul className={cn(styles.features)}>
-              <li className={cn(styles.featuresItem, styles.featuresItemInvest)}>
-                <FormattedMessage id="platform.landing.list.first" />
-              </li>
-              <li className={cn(styles.featuresItem, styles.featuresItemMinTicket)}>
-                <FormattedMessage id="platform.landing.list.second" />
-              </li>
-              <li className={cn(styles.featuresItem, styles.featuresItemTradable)}>
-                <FormattedMessage id="platform.landing.list.third" />
-              </li>
-            </ul>
-          </Container>
-        </WidgetGrid>
-      </Content>
-    </section>
-
+  <section className={styles.landing} data-test-id="landing-page">
     <Content>
+      <WidgetGrid>
+        <Container columnSpan={EColumnSpan.ONE_AND_HALF_COL} type={EContainerType.GRID}>
+          <div className={styles.neon} />
+          <h1 className={styles.header}>
+            <FormattedMessage id="platform.landing.featured.header" />
+          </h1>
+          <p className={styles.description}>
+            <FormattedMessage id="platform.landing.description" />
+          </p>
+
+          <div className={styles.buttonGroup}>
+            <ButtonLink
+              layout={EButtonLayout.PRIMARY}
+              size={EButtonSize.NORMAL}
+              to={externalRoutes.neufundBlogChangeIsComing}
+              className={styles.button}
+            >
+              <FormattedMessage id="common.text.read-more" />
+            </ButtonLink>
+            <ButtonLink
+              layout={EButtonLayout.SECONDARY}
+              size={EButtonSize.NORMAL}
+              to={appRoutes.register}
+            >
+              <FormattedMessage id="wallet-selector.register" />
+            </ButtonLink>
+          </div>
+        </Container>
+
+        <Container
+          className={styles.image}
+          columnSpan={EColumnSpan.ONE_AND_HALF_COL}
+          type={EContainerType.GRID}
+        >
+          <img src={fish} alt="" />
+        </Container>
+      </WidgetGrid>
+
       <section className={styles.investmentOpportunities}>
-        <h2 className={cn(styles.investmentOpportunitiesHeader)}>Investment opportunities</h2>
-
         <EtoList mockedEtos={etoMockCompanies} shouldOpenInNewWindow={true} />
-
-        <ButtonGroup className={styles.investmentOpportunitiesCallToAction}>
-          <ButtonLink
-            layout={EButtonLayout.PRIMARY}
-            size={EButtonSize.HUGE}
-            to={appRoutes.register}
-          >
-            <FormattedMessage id="wallet-selector.register" />
-          </ButtonLink>
-          <ButtonLink
-            iconPosition={EIconPosition.ICON_AFTER}
-            layout={EButtonLayout.LINK}
-            size={EButtonSize.HUGE}
-            svgIcon={arrowRight}
-            target={"_blank"}
-            to={externalRoutes.neufundInvest}
-          >
-            <FormattedMessage id="common.text.read-more" />
-          </ButtonLink>
-        </ButtonGroup>
       </section>
     </Content>
-  </>
+  </section>
 );
 
 const Landing = compose(
--- packages/web/app/data/etoCompanies.ts
+++ packages/web/app/data/etoCompanies.ts
@@ -1,9 +1,6 @@
 import { Q18 } from "@neufund/shared-utils";
 
-import emfluxThumbnail from "../assets/img/eto_offers/investment_thumbnails_emflux_motors.jpg";
 import icbmThumbnail from "../assets/img/eto_offers/investment_thumbnails_icbm_capital_raise.png";
-import mySwooopThumbnail from "../assets/img/eto_offers/investment_thumbnails_my_swooop.jpg";
-import ngraveThumbnail from "../assets/img/eto_offers/investment_thumbnails_ngrave.jpg";
 
 export type TMockEto = {
   brandName: string;
@@ -25,32 +22,5 @@ export const etoMockCompanies: TMockEto[] = [
     categories: ["Technology", "Blockchain"],
     keyQuoteFounder:
       "More than a thousand investors participated in Neufund’s first fundraise in 2017.",
-  },
-  {
-    brandName: "NGRAVE",
-    url: "https://www.ngrave.io/",
-    companyPreviewCardBanner: ngraveThumbnail,
-    id: "ngrave",
-    categories: ["Blockchain", "Belgium"],
-    keyQuoteFounder:
-      "Next gen cryptocurrency hardware wallet developed with one sole focus: no compromise on security. Create, manage and initiate transactions with you Ngrave wallet fully offline without the need to expose it to a physical or network connection.",
-  },
-  {
-    brandName: "Emflux Motors",
-    url: "http://www.emfluxmotors.com/",
-    companyPreviewCardBanner: emfluxThumbnail,
-    id: "emflux",
-    categories: ["Mobility", "India"],
-    keyQuoteFounder:
-      "Electric superbike changing the landscape of transportation & mobility. India-based venture backed by both crypto and traditional investors with a mission to power 10 million electric bikes in India by 2027.",
-  },
-  {
-    brandName: "mySWOOOP",
-    url: "https://www.myswooop.de/",
-    companyPreviewCardBanner: mySwooopThumbnail,
-    id: "my-swooop",
-    categories: ["Re-Commerce", "Germany"],
-    keyQuoteFounder:
-      "Omni-channel re-commerce platform buying and selling new and used electronics. With tailored software technology, mySWOOOP automatically determines current market prices in real-time assuring attractive resale-margins.",
   },
 ];

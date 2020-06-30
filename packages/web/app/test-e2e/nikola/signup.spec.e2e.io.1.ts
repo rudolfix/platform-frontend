bc6571ac85f17e08c4f7d8b9418416a53c346803
Nikola Sporczyk <nsporczyk@gmail.com>
2020-06-30 10:40:32 +0200

    updates

--- packages/web/app/test-e2e/nikola/signup.spec.e2e.io.1.ts
+++ packages/web/app/test-e2e/nikola/signup.spec.e2e.io.1.ts
@@ -1,26 +1,23 @@
 import { tid } from "../utils/index";
 import { cyan } from "color-name";
+import { generateRandomEmailAddress } from "../utils/userHelpers";
 
 describe("Signup", () => {
   it("should sign up with light wallet", () => {
-    cy.visit("https://localhost:9090");
+    const email = generateRandomEmailAddress();
+    const password = "myStrongPassword";
+    cy.visit("/");
 
     cy.get(tid("Header-register")).click();
-    //QUESTION - how do I toggle random input, to avoid email reuse?
     //how do I click on eye symbol?
 
-    cy.get("#email").type("email@test.com");
-    cy.get("#password").type("dupadupa");
-
-    //how do I make this line reuse the previously used password instead of typing?
-    cy.get("#repeatPassword").should("be.visible");
-    cy.get("#repeatPassword").type("dupadupa");
-    //this is wrong and I can't find the right command: cy.get("#repeatPassword").should("have.length", 8)
+    cy.get("#email").type(email);
+    cy.get("#password").type(password);
+    cy.get("#repeatPassword").type(password);
 
     //why the tid?
     cy.get(tid("wallet-selector-register-tos")).click();
 
-    cy.get(tid("wallet-selector-register-button")).should("be.enabled");
     cy.get(tid("wallet-selector-register-button")).click();
   });
 });

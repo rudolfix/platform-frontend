c0864478cdd9451105eb63f3d8c80b3ed99f61fe
Nikola Sporczyk <nsporczyk@gmail.com>
2020-07-14 14:12:59 +0200

    fix linting

--- packages/web/app/test-e2e/nikola/signup.spec.e2e.io.1.ts
+++ packages/web/app/test-e2e/nikola/signup.spec.e2e.io.1.ts
@@ -14,17 +14,24 @@ describe("Signup", () => {
     cy.get("#password").type(password);
     cy.get("#repeatPassword").type(password);
 
-    cy.get(tid("wallet-selector-register-password")).find("[type='password']").should('be.visible') 
-    cy.get(tid("wallet-selector-register-password")).find("[type='text']").should('not.be.visible')
+    cy.get(tid("wallet-selector-register-password"))
+      .find("[type='password']")
+      .should("be.visible");
+    cy.get(tid("wallet-selector-register-password"))
+      .find("[type='text']")
+      .should("not.be.visible");
 
     cy.get(tid("wallet-selector-register-password.adornment")).click();
-    
-    cy.get(tid("wallet-selector-register-password")).find("[type='password']").should('not.be.visible')
-    cy.get(tid("wallet-selector-register-password")).find("[type='text']").should('be.visible') 
+
+    cy.get(tid("wallet-selector-register-password"))
+      .find("[type='password']")
+      .should("not.be.visible");
+    cy.get(tid("wallet-selector-register-password"))
+      .find("[type='text']")
+      .should("be.visible");
 
     cy.get(tid("wallet-selector-register-tos")).click();
 
     cy.get(tid("wallet-selector-register-button")).click();
   });
 });
-//should I group the test into smaller pieces and name them separately?

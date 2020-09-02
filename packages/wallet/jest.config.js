0ed3c936131b60e9c9e2a435acac08c14477ef3c
Paweł Lula <pavloblack@hotmail.com>
2020-09-02 22:04:33 +0200

    #4483 Allow jest to work with `export type`

--- packages/wallet/jest.config.js
+++ packages/wallet/jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   preset: "react-native",
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  transform: {
+    "^.+\\.(js)$": "babel-jest",
+    "\\.(ts|tsx)$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsConfig: "./tsconfig.unit.json",
+      babelConfig: true,
+    },
+  },
   setupFiles: ["./jest-setup-file.specUtils.ts"],
   modulePathIgnorePatterns: ["<rootDir>/tests/e2e/"],
   setupFilesAfterEnv: ["@testing-library/react-native/cleanup-after-each"],
@@ -7,4 +18,5 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/app/__mocks__/fileMock.ts",
   },
+  cacheDirectory: ".jest/cache",
 };
